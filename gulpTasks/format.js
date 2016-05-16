982c4cddd647684fb45bcd2e7e841fe0ae71ac5d
Dominique Begin <dbegin@coveo.com>
2016-05-16 12:55:59 -0400

    Start adding tslint and tsfmt to project

---
+++ gulpTasks/format.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const tslint = require('gulp-tslint');
+const tsfmt = require('gulp-tsfmt');
+const path = require('path');
+
+const sourceFiles = ['src/**/*.ts', '!src/**/*.d.ts'];
+
+gulp.task('lint', function () {
+  gulp.src(sourceFiles)
+    .pipe(tslint())
+    .pipe(tslint.report('prose'));
+})
+
+gulp.task('format', function () {
+  gulp.src(sourceFiles)
+    .pipe(tsfmt())
+    .pipe(gulp.dest(file => path.dirname(file.path)));
+})
