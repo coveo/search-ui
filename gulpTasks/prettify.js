f0d243fc5d313697722145f0c0ee471b95906b15
Dominique Begin <dbegin@coveo.com>
2016-05-18 12:48:26 -0400

    Rename 'format' task to 'prettify' ❤️

--- gulpTasks/prettify.js
+++ gulpTasks/prettify.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const sourceFiles = ['src/**/*.ts', '!src/**/*.d.ts', '!src/strings/**/*.ts'];
 
-gulp.task('format', function () {
+gulp.task('prettify', function () {
   gulp.src(sourceFiles)
     .pipe(prettyTypescript())
     .pipe(gulp.dest('src'));
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ requireDir('./gulpTasks');
 gulp.task('default', ['build', 'buildLegacy']);
 
 gulp.task('build', function (done) {
-  runsequence('clean', ['css', 'fileTypes', 'sprites', 'strings', 'setup'], 'lint', 'compile', 'definitions', done);
+  runsequence('clean', ['css', 'fileTypes', 'sprites', 'strings', 'setup'], 'prettify', 'compile', 'definitions', done);
 });
 
 gulp.task('buildLegacy', function (done) {
