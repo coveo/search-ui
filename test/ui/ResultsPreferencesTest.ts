d20e2a3065eacc92e3f46f007eaf41f497ddda26
Mathieu Carpentier <mcarpentier@coveo.com>
2016-05-27 17:03:12 -0400

    Fix typo in ResultsPreferences tests name

--- test/ui/ResultsPreferencesTest.ts
+++ test/ui/ResultsPreferencesTest.ts
@@ -20,7 +20,7 @@ module Coveo {
       expect($$(test.cmp.element).find('input[value="' + l("OpenInOutlookWhenPossible") + '"]')).not.toBeNull();
     });
     
-    it('will build will build a open in new window if it\'s wanted', () => {
+    it('will build a open in new window option if it\'s wanted', () => {
       test = Mock.advancedComponentSetup<ResultsPreferences>(ResultsPreferences, new Mock.AdvancedComponentSetupOptions(element.el, {enableOpenInNewWindow: true}));
       expect($$(test.cmp.element).find('input[value="' + l("AlwaysOpenInNewWindow") + '"]')).not.toBeNull();
     });
@@ -30,7 +30,7 @@ module Coveo {
       expect($$(test.cmp.element).find('input[value="' + l("OpenInOutlookWhenPossible") + '"]')).toBeNull();
     });
     
-    it('will not build will build a open in new window if it\'s not wanted', () => {
+    it('will not build a open in new window option if it\'s not wanted', () => {
       test = Mock.advancedComponentSetup<ResultsPreferences>(ResultsPreferences, new Mock.AdvancedComponentSetupOptions(element.el, {enableOpenInNewWindow: false}));
       expect($$(test.cmp.element).find('input[value="' + l("AlwaysOpenInNewWindow") + '"]')).toBeNull();
     });
