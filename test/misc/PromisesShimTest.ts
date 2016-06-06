258d87e00a40f84471cf149ccbd3ff29fa4bf3b0
olamothe <olamothe@coveo.com>
2016-06-06 17:37:12 -0400

    Pull requests comments https://coveord.atlassian.net/browse/JSUI-818

--- test/misc/PromisesShimTest.ts
+++ test/misc/PromisesShimTest.ts
@@ -2,33 +2,33 @@
 
 module Coveo {
   describe('PromisesShim', function () {
-    it('should shim finally', (done)=> {
-      new Promise((resolve, reject)=> {
+    it('should shim finally', (done) => {
+      new Promise((resolve, reject) => {
         resolve(true);
-      }).finally(()=> {
+      }).finally(() => {
         // dummy check, what we really need to see is that done was called
         expect(true).toBe(true);
         done();
       });
     })
 
-    it('should shim done', (done)=> {
-      var p = new Promise((resolve, reject)=> {
+    it('should shim done', (done) => {
+      var p = new Promise((resolve, reject) => {
         resolve(true);
       })
 
-      p.done((value)=> {
+      p.done((value) => {
         expect(value).toBe(true);
         done();
       });
     })
 
-    it('should shim fail', (done)=> {
-      var p = new Promise((resolve, reject)=> {
+    it('should shim fail', (done) => {
+      var p = new Promise((resolve, reject) => {
         reject(false);
       })
 
-      p.fail((value)=> {
+      p.fail((value) => {
         expect(value).toBe(false);
         done();
       });
