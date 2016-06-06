d6b5752fa6b95ddd731dfab2c4f11ecc08307d06
Dominique Begin <dbegin@coveo.com>
2016-06-06 14:54:06 -0400

    The prettify task now lints test files https://coveord.atlassian.net/browse/JSUI-833

--- gulpTasks/prettify.js
+++ gulpTasks/prettify.js
@@ -2,10 +2,11 @@ const gulp = require('gulp');
 const prettyTypescript = require('pretty-typescript');
 const path = require('path');
 
-const sourceFiles = ['src/**/*.ts', '!src/strings/**/*.ts'];
-
 gulp.task('prettify', function () {
-  gulp.src(sourceFiles)
+  gulp.src(['src/**/*.ts', '!src/strings/**/*.ts'])
     .pipe(prettyTypescript())
     .pipe(gulp.dest('src'));
+  gulp.src(['test/**/*.ts', '!test/lib/**/*.ts'])
+    .pipe(prettyTypescript())
+    .pipe(gulp.dest('test'));
 });
