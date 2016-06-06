41164cfcbdbb23cd78e6b2cc5b0fa24c86f23d82
Dominique Begin <dbegin@coveo.com>
2016-06-06 15:14:29 -0400

    Update pretty-typescript + fix not returning gulp stream in prettify https://coveord.atlassian.net/browse/JSUI-833

--- gulpTasks/prettify.js
+++ gulpTasks/prettify.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const prettyTypescript = require('pretty-typescript');
+const merge = require('merge-stream');
 const path = require('path');
 
 gulp.task('prettify', function () {
-  gulp.src(['src/**/*.ts', '!src/strings/**/*.ts'])
+  var src = gulp.src(['src/**/*.ts', '!src/strings/**/*.ts'])
     .pipe(prettyTypescript())
     .pipe(gulp.dest('src'));
-  gulp.src(['test/**/*.ts', '!test/lib/**/*.ts'])
+  var test = gulp.src(['test/**/*.ts', '!test/lib/**/*.ts'])
     .pipe(prettyTypescript())
     .pipe(gulp.dest('test'));
+  return merge(src, test);
 });
