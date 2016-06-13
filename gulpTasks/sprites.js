34b46266541adbd66d598438af2e932212c1ee85
Dominique Begin <dbegin@coveo.com>
2016-06-13 15:28:10 -0400

    Change 'BuildSpritesList' to 'BuildSpriteList' Grammar nazi :) https://coveord.atlassian.net/browse/JSUI-839

--- gulpTasks/sprites.js
+++ gulpTasks/sprites.js
@@ -3,7 +3,7 @@ const glob = require('glob');
 const _ = require('underscore');
 const pngSprite = require('png-sprite');
 const fs = require('fs');
-const buildSpriteList = require('../BuildSpritesList');
+const buildSpriteList = require('../BuildSpriteList');
 
 gulp.task('sprites', ['regularSprites', 'retinaSprites', 'regularSpriteList', 'retinaSpriteList', 'validateRetinaSprites']);
 gulp.task('spritesLegacy', ['regularSpritesLegacy', 'retinaSpritesLegacy']);
