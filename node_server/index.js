51d134e85f6999dbb3093d7770cd1801554e3034
Dario Martins Silva <dmsilva@coveo.com>
2016-06-16 13:27:11 -0400

    fix base path html

--- node_server/index.js
+++ node_server/index.js
@@ -6,12 +6,12 @@ app.use('/', express.static(__dirname + '/../bin'))
 
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '../bin/Index.html');
+  response.sendFile(__dirname + '/../bin/Index.html');
 })
 
 app.get(/^(.+)$/, function(req, res){ 
      res.sendfile( __dirname + req.params[0]); 
- });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
