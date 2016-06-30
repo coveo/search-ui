818294cadeacdfdfe07eeebffd3e073ac6844bf8
olamothe <olamothe@coveo.com>
2016-06-30 15:09:17 -0400

    Build always gets minimized https://coveord.atlassian.net/browse/JSUI-868

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const webpack = require('webpack');
 const minimize = process.argv.indexOf('--minimize') !== -1;
 const colors = require('colors');
 const failPlugin = require('webpack-fail-plugin');
-if (minimize) {
-  console.log('Building minified version of the library'.bgGreen.red);
-} else {
-  console.log('Building non minified version of the library'.bgGreen.red);
-}
 
 // Fail plugin will allow the webpack ts-loader to fail correctly when the TS compilation fails
 var plugins = [failPlugin];
