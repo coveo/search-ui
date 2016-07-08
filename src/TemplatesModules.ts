c23dd67621abf5c6e859f856ad721278157ae30d
olamothe <olamothe@coveo.com>
2016-07-08 10:35:14 -0400

    Exclude src/*.ts for definitions task https://coveord.atlassian.net/browse/JSUI-903

--- src/TemplatesModules.ts
+++ src/TemplatesModules.ts
@@ -2,4 +2,3 @@ export {TemplateHelpers} from './ui/Templates/TemplateHelpers';
 export {TemplateCache} from './ui/Templates/TemplateCache';
 export {HtmlTemplate} from './ui/Templates/HtmlTemplate';
 export {UnderscoreTemplate} from './ui/Templates/UnderscoreTemplate';
-
