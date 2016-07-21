c47f0dc0bd8499d09c0be1c9639032bd89e9e874
Dario Martins Silva <dmsilva@coveo.com>
2016-07-21 17:08:02 -0400

    remove unused function https://coveord.atlassian.net/browse/JSUI-945

--- src/ui/ResponsiveComponents/ResponsiveFacets.ts
+++ src/ui/ResponsiveComponents/ResponsiveFacets.ts
@@ -230,15 +230,4 @@ export class ResponsiveFacets implements IResponsiveComponent {
   private drawFacetSliderGraphs() {
     _.each(this.facetSliders, facetSlider => facetSlider.drawDelayedGraphData());
   }
-
-  private isFacetSearchScrolledIntoView(facetSearchElement: HTMLElement) {
-    let facetTop = facetSearchElement.getBoundingClientRect().top;
-    let facetBottom = facetSearchElement.getBoundingClientRect().bottom;
-    let dropdownTop = this.dropdownContent.el.getBoundingClientRect().top;
-    let dropdownBottom = this.dropdownContent.el.getBoundingClientRect().bottom;
-
-    dropdownTop = dropdownTop >= 0 ? dropdownTop : 0;
-
-    return (facetTop >= dropdownTop) && (facetBottom <= dropdownBottom);
-  }
 }
