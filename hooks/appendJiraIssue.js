e8f1ccf3d9d77e7ec92e307f39560340047eb620
Dario Martins Silva <dmsilva@coveo.com>
2016-07-25 14:33:40 -0400

    get commit message filename https://coveord.atlassian.net/browse/JSUI-888

--- hooks/appendJiraIssue.js
+++ hooks/appendJiraIssue.js
@@ -5,5 +5,7 @@
 const childProcess = require('child_process');
 
 let branchName = childProcess.execSync('git symbolic-ref --short HEAD', {encoding: 'utf8'});
-branchName = branchName.trim().replace(/\r\n$/, '');
-console.log(branchName);
+branchName = branchName.trim();
+
+let commitMessageFilename = process.argv[1];
+console.log(commitMessageFilename);
