7e46bc7f1374790190324988b9b98f0eed489539
Dario Martins Silva <dmsilva@coveo.com>
2016-07-25 15:08:15 -0400

    append jira issue https://coveord.atlassian.net/browse/JSUI-888

--- hooks/appendJiraIssue.js
+++ hooks/appendJiraIssue.js
@@ -3,9 +3,15 @@
 'use strict'
 
 const childProcess = require('child_process');
+const fs = require('fs');
 
 let branchName = childProcess.execSync('git symbolic-ref --short HEAD', {encoding: 'utf8'});
 branchName = branchName.trim();
 
+let issueRegex = new RegExp(/JSUI-[\d]+/, 'm');
+
+
 let commitMessageFilename = process.argv[1];
-console.log(commitMessageFilename);
+
+let commitMessage = fs.readFileSync(commitMessageFilename);
+
