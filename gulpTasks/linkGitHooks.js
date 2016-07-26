c999d364994eccda02ad73272f47c7f0d6ea6eb7
Dario Martins Silva <dmsilva@coveo.com>
2016-07-26 15:55:20 -0400

    add copy if symlink fails
    https://coveord.atlassian.net/browse/JSUI-888

--- gulpTasks/linkGitHooks.js
+++ gulpTasks/linkGitHooks.js
@@ -1,34 +1,38 @@
 'use strict';
 
 const gulp = require('gulp');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const _ = require('underscore');
 const Q = require('Q');
 const colors = require('colors');
 
 const lstat = Q.denodeify(fs.stat);
-const symlink = Q.denodeify(fs.symlink);
+const symlink = Q.denodeify(fs.ensureSymlink);
+const copy = Q.denodeify(fs.copy);
+const readdir = Q.denodeify(fs.readdir);
 
 gulp.task('linkGitHooks', function() {
   let noSuchFile = -2;
   let gitHooksDir = '.git/hooks/';
   let gitHooksSourceDir = 'hooks/';
-  
-  if (!fs.existsSync(gitHooksDir)) {
-    fs.mkdirSync(gitHooksDir);
-  }
 
-  fs.readdir(gitHooksSourceDir, (err, files) => {
+  readdir(gitHooksSourceDir).then((err, files) => {
     _.each(files, filename => {
       let symname = path.resolve(process.cwd(), gitHooksDir + filename);
       let source = path.resolve(process.cwd(), gitHooksSourceDir + filename);
       let cwd = process.cwd();
       process.chdir(gitHooksDir);
       symlink(source, symname, 'file')
-        .catch(err => {
-          console.log(colors.red(err));
-        })
+          .catch(err => {
+            //Need to be admin on windows to create a symlink (╯°□°）╯︵ ┻━┻
+            console.log('Couldn\'t create a symlink, trying to copy...'.yellow);
+            copy(source, symname)
+                .catch(err => {
+                  console.log(colors.red(err));
+                });
+
+          })
         .finally(() => {
           process.chdir(cwd);
         });
