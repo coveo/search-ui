489e5cecd7785a87e77d45c537a4f9f1f3b6c696
olamothe <olamothe@coveo.com>
2016-07-28 11:09:09 -0400

    use lstat to check if it's a symbolic link VS a real directory Debounce the call so that it's not called multiple time in a row on start https://coveord.atlassian.net/browse/JSUI-922

--- gulpTasks/dev.js
+++ gulpTasks/dev.js
@@ -1,29 +1,33 @@
+'use strict';
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 const colors = require('colors');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const buildUtilities = require('../gulpTasks/buildUtilities.js');
+const _ = require('underscore');
 
-var webpackConfig = require('../webpack.config.js');
+let webpackConfig = require('../webpack.config.js');
 webpackConfig.entry['CoveoJsSearch'].unshift('webpack-dev-server/client?http://localhost:8080/');
 const compiler = webpack(webpackConfig);
 
-compiler.plugin('done', function () {
-  setTimeout(function () {
-    console.log('... Compiler done ... Linking external projects'.black.bgGreen);
-    buildUtilities.exec('node', ['./environments/link.externally.js'], undefined, function () {
-    })
-  }, 1000)
+let debounced = _.debounce(()=> {
+  console.log('... Compiler done ... Linking external projects'.black.bgGreen);
+  buildUtilities.exec('node', ['./environments/link.externally.js'], undefined, function () {
+  })
+}, 1000)
+
+compiler.plugin('done', ()=> {
+  debounced();
 })
 
-gulp.task('dev', ['setup', 'prepareSass'], function (done) {
-  var server = new WebpackDevServer(compiler, {
+gulp.task('dev', ['setup', 'prepareSass'], (done)=> {
+  let server = new WebpackDevServer(compiler, {
     contentBase: 'bin/',
     publicPath: '/js/',
     compress: true
   });
-  server.listen(8080, 'localhost', function () {
+  server.listen(8080, 'localhost', ()=> {
   });
   done();
 })
