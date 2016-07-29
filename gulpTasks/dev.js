bb01a69dad7073fc59ce00dc644e8ed5fc0cb305
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2016-07-29 14:13:56 -0400

    No more error on gulp dev when the link directory exists (#115)
    * use lstat to check if it's a symbolic link VS a real directory
    Debounce the call so that it's not called multiple time in a row on start
    https://coveord.atlassian.net/browse/JSUI-922
    
    * bad merge
    https://coveord.atlassian.net/browse/JSUI-922

--- gulpTasks/dev.js
+++ gulpTasks/dev.js
@@ -5,30 +5,33 @@ const colors = require('colors');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const buildUtilities = require('../gulpTasks/buildUtilities.js');
+const _ = require('underscore');
 
-let webpackConfigSrc = require('../webpack.config.js');
-webpackConfigSrc.entry['CoveoJsSearch'].unshift('webpack-dev-server/client?http://localhost:8080/');
-const compilerSrc = webpack(webpackConfigSrc);
+let webpackConfig = require('../webpack.config.js');
+webpackConfig.entry['CoveoJsSearch'].unshift('webpack-dev-server/client?http://localhost:8080/');
+const compiler = webpack(webpackConfig);
 
 let webpackConfigTest = require('../webpack.test.config');
-webpackConfigSrc.entry['tests'].unshift('webpack-dev-server/client?http://localhost:8081/');
+webpackConfigTest.entry['tests'].unshift('webpack-dev-server/client?http://localhost:8081/');
 const compilerTest = webpack(webpackConfigTest);
 
-compilerSrc.plugin('done', function () {
-  setTimeout(function () {
-    console.log('... Compiler done ... Linking external projects'.black.bgGreen);
-    buildUtilities.exec('node', ['./environments/link.externally.js'], undefined, function () {
-    })
-  }, 1000)
+let debounced = _.debounce(()=> {
+  console.log('... Compiler done ... Linking external projects'.black.bgGreen);
+  buildUtilities.exec('node', ['./environments/link.externally.js'], undefined, function () {
+  })
+}, 1000)
+
+compiler.plugin('done', ()=> {
+  debounced();
 })
 
-gulp.task('dev', ['setup', 'prepareSass', 'setupTests'], function (done) {
-  var serverSrc = new WebpackDevServer(compilerSrc, {
+gulp.task('dev', ['setup', 'prepareSass'], (done)=> {
+  let server = new WebpackDevServer(compiler, {
     contentBase: 'bin/',
     publicPath: '/js/',
     compress: true
   });
-  serverSrc.listen(8080, 'localhost', ()=> {
+  server.listen(8080, 'localhost', ()=> {
   });
   done();
 })
