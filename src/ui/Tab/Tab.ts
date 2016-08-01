9b630f31c16774abda21b1df0a55ad7cc78b2dc5
Dominique Bégin <dominique.begin.0@gmail.com>
2016-08-01 15:23:22 -0400

    Fix styling on Tab and Sort
    https://coveord.atlassian.net/browse/JSUI-869

--- src/ui/Tab/Tab.ts
+++ src/ui/Tab/Tab.ts
@@ -11,6 +11,7 @@ import {Initialization} from '../Base/Initialization';
 import {Utils} from '../../utils/Utils';
 import {Assert} from '../../misc/Assert';
 import {$$} from '../../utils/Dom';
+import {KeyboardUtils, KEYBOARD} from '../../utils/KeyboardUtils';
 
 export interface ITabOptions {
   expression?: string;
@@ -114,7 +115,9 @@ export class Tab extends Component {
 
     this.bind.onRootElement(QueryEvents.buildingQuery, (args: IBuildingQueryEventArgs) => this.handleBuildingQuery(args));
     this.bind.onQueryState(MODEL_EVENTS.CHANGE_ONE, QUERY_STATE_ATTRIBUTES.T, (args: IAttributeChangedEventArg) => this.handleQueryStateChanged(args));
-    this.bind.on(element, 'click', (e: Event) => this.handleClick());
+    const clickAction = () => this.handleClick();
+    this.bind.on(element, 'click', clickAction);
+    this.bind.on(element, 'keyup', KeyboardUtils.keypressAction(KEYBOARD.ENTER, clickAction))
     this.render();
     ResponsiveTabs.init(this.root, Tab.ID, this);
   }
