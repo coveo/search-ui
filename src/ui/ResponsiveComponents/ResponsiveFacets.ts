47615b47123224b2ecc5aa6257af9616b5daccfc
Dario Martins Silva <dmsilva@coveo.com>
2016-09-09 10:06:57 -0400

    find coveo-tab-section everytime it is needed, instead of one time at init
    https://coveord.atlassian.net/browse/JSUI-1104

--- src/ui/ResponsiveComponents/ResponsiveFacets.ts
+++ src/ui/ResponsiveComponents/ResponsiveFacets.ts
@@ -27,7 +27,6 @@ export class ResponsiveFacets implements IResponsiveComponent {
   private previousSibling: Dom;
   private parent: Dom;
   private dropdownHeader: Dom;
-  private tabSection: Dom;
   private popupBackground: Dom;
   private popupBackgroundClickListener: EventListener;
   private facets: Facet[] = [];
@@ -40,10 +39,6 @@ export class ResponsiveFacets implements IResponsiveComponent {
       this.logger.info('No element with class coveo-facet-column. Responsive facets cannot be enabled');
       return;
     }
-    if (!$$(root).find('.coveo-tab-section')) {
-      this.logger.info('No element with class coveo-tab-section. Responsive facets cannot be enabled');
-      return;
-    }
     ResponsiveComponentsManager.register(ResponsiveFacets, $$(root), Facet.ID, component);
   }
 
@@ -51,7 +46,6 @@ export class ResponsiveFacets implements IResponsiveComponent {
     this.ID = ID;
     this.coveoRoot = root;
     this.searchInterface = <SearchInterface>Component.get(root.el, SearchInterface, false);
-    this.tabSection = $$(this.coveoRoot.find('.coveo-tab-section'));
     this.buildDropdownContent();
     this.buildDropdownHeader();
     this.bindDropdownContentEvents();
@@ -66,10 +60,14 @@ export class ResponsiveFacets implements IResponsiveComponent {
   }
 
   public changeToSmallMode() {
+    if (!$$(this.coveoRoot).find('.coveo-tab-section')) {
+      ResponsiveFacets.logger.info('No element with class coveo-tab-section. Responsive facets cannot be enabled');
+      return;
+    }
     this.positionPopup();
     this.closeDropdown();
     this.disableFacetPreservePosition();
-    this.tabSection.el.appendChild(this.dropdownHeader.el);
+    $$(this.coveoRoot.find('.coveo-tab-section')).el.appendChild(this.dropdownHeader.el);
     this.dropdownContent.el.style.display = 'none';
   }
 
@@ -185,7 +183,7 @@ export class ResponsiveFacets implements IResponsiveComponent {
     }
     this.dropdownContent.el.style.width = width.toString() + 'px';
 
-    PopupUtils.positionPopup(this.dropdownContent.el, this.tabSection.el, this.coveoRoot.el,
+    PopupUtils.positionPopup(this.dropdownContent.el, $$(this.coveoRoot.find('.coveo-tab-section')).el, this.coveoRoot.el,
       { horizontal: HorizontalAlignment.INNERRIGHT, vertical: VerticalAlignment.BOTTOM }, this.coveoRoot.el);
   }
 
