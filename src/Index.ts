7962067a93c007345876de4852681a825ea7b392
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2016-09-22 09:39:12 -0400

    Remove jQuery from ambient typings so that if we reference it, it won't compile (#176)
    Remove jQuery usage from StreamHightUtils
    Remove jQuery usage from some test files
    Re-import StreamHighlightUtils test from old repository, and fix them
    https://coveord.atlassian.net/browse/JSUI-1125

--- src/Index.ts
+++ src/Index.ts
@@ -111,12 +111,13 @@ export {ChatterPostedBy} from './ui/ChatterPostedBy/ChatterPostedBy';
 export {ChatterTopic} from './ui/ChatterTopic/ChatterTopic';
 export {ChatterUtils} from './utils/ChatterUtils';
 export { Logo } from './ui/Logo/Logo';
+import {CoreHelpers} from './ui/Templates/CoreHelpers';
 
 // Webpack output a library target with a temporary name.
 // This is to allow end user to put CoveoJsSearch.Dependencie.js before or after the main CoveoJsSearch.js, without breaking
 // This code swap the current module to the "real" Coveo variable.
-
 let swapVar = () => {
+
   if (window['Coveo'] == undefined) {
     window['Coveo'] = this;
   } else {
@@ -124,6 +125,7 @@ let swapVar = () => {
       window['Coveo'][k] = this[k];
     });
   }
+  CoreHelpers.exportAllHelpersGlobally(window['Coveo']);
   if (window['__extends'] == undefined) {
     var __extends = function (d, b) {
       for (var p in b) {
@@ -139,5 +141,6 @@ let swapVar = () => {
     };
     window['__extends'] = __extends;
   }
+
 };
 swapVar();
