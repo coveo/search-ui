960ed66ca06709434963823b6dcfddc1c3e87266
Dario Martins Silva <dmsilva@coveo.com>
2016-10-05 16:28:06 -0400

    start test for quickview
    https://coveord.atlassian.net/browse/JSUI-1167

---
+++ test/ui/QuickviewTest.ts
@@ -0,0 +1,11 @@
+import * as Mock from '../MockEnvironment';
+import {Quickview} from '../../src/ui/Quickview/Quickview';
+
+export function QuickviewTest() {
+  describe('Quickview', () => {
+    let test: Mock.IBasicComponentSetup<Quickview>;
+    it('closes the modal box on close', () => {
+
+    });
+  });
+}
