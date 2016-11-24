86b06c6880d943e51523ab2169cd4a1758b6986d
Dario Martins Silva <dmsilva@coveo.com>
2016-11-24 17:08:25 -0500

    re-add empty file....?
    https://coveord.atlassian.net/browse/JSUI-828

--- src/ui/HierarchicalFacet/HierarchicalFacetValuesList.ts
+++ src/ui/HierarchicalFacet/HierarchicalFacetValuesList.ts
@@ -1,1 +1,18 @@
+/// <reference path="HierarchicalFacet.ts" />
 
+import {FacetValuesList} from '../Facet/FacetValuesList';
+import {FacetValue} from '../Facet/FacetValues';
+import {HierarchicalFacet} from './HierarchicalFacet';
+import {IFacetValueElementKlass} from '../Facet/FacetValueElement';
+
+export class HierarchicalFacetValuesList extends FacetValuesList {
+  public hierarchyFacetValues: FacetValue[];
+
+  constructor(public facet: HierarchicalFacet, public facetValueElementKlass: IFacetValueElementKlass) {
+    super(facet, facetValueElementKlass);
+  }
+
+  protected getValuesToBuildWith() {
+    return this.hierarchyFacetValues;
+  }
+}
