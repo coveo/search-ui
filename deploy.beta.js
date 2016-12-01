300aeeb3c8cd38bc977e252463259adca0442add
olamothe <olamothe@coveo.com>
2016-12-01 16:06:56 -0500

    Deploy beta script
    https://coveord.atlassian.net/browse/JSUI-1183

--- deploy.beta.js
+++ deploy.beta.js
@@ -1,6 +1,6 @@
 'use strict';
 const login = require('npm-cli-login');
-
+const exec = require('child_process').exec;
 login('coveo', process.env.NPM_PW, 'sandbox_JSUI@coveo.com');
 
 console.log('executing deploy script beta');
