d6c41d6dda6e1b2edc7bcc2f3e74c54219ccd613
olamothe <olamothe@coveo.com>
2016-12-01 16:19:03 -0500

    Deploy beta script
    https://coveord.atlassian.net/browse/JSUI-1183

--- deploy.beta.js
+++ deploy.beta.js
@@ -4,6 +4,6 @@ const exec = require('child_process').exec;
 login('coveo', process.env.NPM_PW, 'sandbox_JSUI@coveo.com');
 
 console.log('executing deploy script beta');
-exec('npm publish --tag beta', function (error, stdout, stderr) {
+setTimeout(exec('npm publish --tag beta', function (error, stdout, stderr) {
   console.log(error, stdout, stderr);
-});
+}), 2000);
