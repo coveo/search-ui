a467d91af0efcbe80fb3c0cc5a6375b18c276783
Dario Martins Silva <dario@live.ca>
2016-12-16 15:00:31 -0500

    Parse the q URI parameter as a string (#246)
    Force the q parameter to be parsed as a string.
    When trying to parse a value, use a try catch and log an error when it fails.
    Add tests for HashUtils.
    https://coveord.atlassian.net/browse/JSUI-1313

--- test/controllers/HistoryControllerTest.ts
+++ test/controllers/HistoryControllerTest.ts
@@ -1,4 +1,5 @@
 import {HistoryController} from '../../src/controllers/HistoryController';
+import {InitializationEvents} from '../../src/events/InitializationEvents';
 import * as Mock from '../MockEnvironment';
 import {$$} from '../../src/utils/Dom';
 import {Defer} from '../../src/misc/Defer';
@@ -40,6 +41,28 @@ export function HistoryControllerTest() {
       $$(historyController.element).trigger('state:all');
       Defer.flush();
       expect(historyController.windoh.location.hash).toBe('#c=notDefault&d=[1,2,3]');
+    });
+
+    it('keeps parsing hash values after one fails to parse', () => {
+      let threwError = false;
+      let hashUtils = jasmine.createSpyObj('hashUtils', ['getValue', 'getHash']);
+      env.queryStateModel.attributes = {
+        a: 1,
+        b: 2,
+        c: 3
+      };
+
+      hashUtils.getValue.and.callFake(() => {
+        if (!threwError) {
+          threwError = true;
+          throw new Error();
+        }
+      });
+      historyController = new HistoryController(env.root, Mock.mockWindow(), env.queryStateModel, env.queryController, hashUtils);
+
+      $$(env.root).trigger(InitializationEvents.restoreHistoryState);
+
+      expect(hashUtils.getValue).toHaveBeenCalledTimes(_.size(env.queryStateModel.attributes));
     });
   });
 }
