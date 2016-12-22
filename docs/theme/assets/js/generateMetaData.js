e7b3755367b2349367b8c63e1c83328d3353647f
fbeaudoin <fbeaudoin@coveo.com>
2016-12-22 08:48:38 -0500

    Added page-topic meta data Modified meta data script Modified page title rendering

--- docs/theme/assets/js/generateMetaData.js
+++ docs/theme/assets/js/generateMetaData.js
@@ -1,59 +1,64 @@
 /**
- * Generates the <meta> tags which can be used to correctly index page content.
+ * Generates the <meta> tags which can be used to correctly index the page content.
  */
 function generatePageMetaData() {
 
-  var pageTitle = getPageTitle();
+  var contextPipe = getContextPipe();
+  var optionsPipe;
 
-  generateAllIndexElementsMetaData();
-  generateSeparateMetaData();
+  generatePageTopicMetaData();
+  generateIndexContentMetaData();
 
   /**
-   * Get the page title from the breadcrumb. Omit page title if it is "Globals".
-   * @returns {string} The page title followed by a dot ('.') if page title was not "Globals". Empty string otherwise.
+   * Gets the contextPipe from the breadcrumb. The contextPipe is the page topic followed by a dot ('.').
+   * For instance, if the current page topic is "Facet", the contextPipe will be "Facet."
+   * No contextPipe is generated if page topic is "Globals".
+   * @returns {string} The page topic followed by a dot ('.') if page topic was not "Globals". Empty string otherwise.
    */
-  function getPageTitle() {
-    var retVal = $('ul.tsd-breadcrumb li:last-of-type a')[0].textContent + ".";
-    if (retVal == "Globals.") {
-      retVal = "";
-    }
+  function getContextPipe() {
+    var temp = $('ul.tsd-breadcrumb li:last-of-type a')[0].textContent + ".";
 
-    return retVal;
+    return temp == ".Globals" ? "" : temp;
   }
 
   /**
-   * Generates the <meta name="all-index-elements" content="..."> tag in page head.
-   * This tag contains all elements from all sections of the page index.
+   * Generates the <meta name='page-topic' content='[pageTopic]'> tag in the page head.
    */
-  function generateAllIndexElementsMetaData() {
+  function generatePageTopicMetaData() {
 
-    // Get all index elements in an array and replace the elements by their respective text value.
-    // e.g., a "a.tsd-kind-icon" element in the array could become a string such as "AdvancedSearch".
-    var indexElements = $('section.tsd-index-section:not(.tsd-is-external) a').toArray();
+    var pageTopic = $('.tsd-page-title h1')[0].textContent;
 
-    indexElements.forEach(function(element, index, array) {
-      array.splice(index, 1, pageTitle + element.text);
+    $('head title').before('<meta name=\'page-topic\' content=\'' + toTitleCase(pageTopic) + '\'>')
+  }
+
+  /**
+   * Transforms a string to match title case.
+   * @param text The string to transform.
+   * @returns {*} The string formatted in title case.
+   */
+  function toTitleCase(text)
+  {
+    return text.replace(/\w\S*/g, function(text) {
+      return text.charAt(0).toUpperCase() + text.substr(1);
     });
-
-    // Generate the meta tag in the page head if indexElements is not empty. Content will be formatted thus:
-    // "pageTitle.element1;pageTitle.element2;...;pageTitle.elementN;".
-    if (indexElements.length > 0)
-    {
-      $('head title').before('<meta name="all-index-elements" content="' + indexElements.join(';') + '">');
-    }
   }
 
   /**
-   * Generates the <meta name="[section-name]" content="..."> tags in the page head.
+   * Generates each <meta name="[section-name]" content="..."> tag in the page head.
    * Each of these tags contains elements from a distinct section of the page index.
+   * Also generates the <meta name='all-index-elements' content='...'> tag in the page head, which contains all
+   * elements from the index.
    */
-  function generateSeparateMetaData() {
+  function generateIndexContentMetaData() {
 
     // Get all index header elements in a array.
     var categories = $('section.tsd-index-section:not(.tsd-is-external) h3').toArray();
 
-    // This will be an array of arrays.
+    // This will be an array of arrays containing all index elements sorted by categories.
     var sortedElements = [];
+
+    // This array will contain all index elements (with no regard to their respective category).
+    var allIndexElements = [];
 
     // Push the header title in the first position of each sortedElements array.
     categories.forEach(function(header, index) {
@@ -61,7 +66,7 @@ function generatePageMetaData() {
       var headerContent = header.textContent;
       contentArray.push(headerContent);
 
-      // Push all header content in the second position of its respective array.
+      // Push all header content in the second position of its array.
       var selector = "h3:contains("+ headerContent +")";
       var toPush = $(".tsd-index-content " + selector +"+ul a").toArray();
       contentArray.push(toPush);
@@ -75,20 +80,33 @@ function generatePageMetaData() {
       // spaces are replaced by dashes. Thus, "Coveo components" becomes "coveo-components".
       var categoryTitle = category[0].toLowerCase().split(" ").join("-");
 
-      // This will contain the elements under a specific category (section name).
+      // This array will contain the elements under the corresponding specific category (section name).
       var categoryElements = [];
 
-      // Push all corresponding element text in the array. Pushed texte is formatted thus: pageTitle.element.
-      // For instance, the includeDate option from the AdvancedSearch page would formatted as
-      // "AdvancedSearch.includeDate", unless the page title is "Globals", in which case the page title is omitted.
+      // The optionsPipe will be empty unless the categoryTitle is "component-options".
+      optionsPipe = categoryTitle == "component-options" ? "options." : "";
+
+      // Push all corresponding element text in the categoryElements array and in the allIndexElements array.
+      // Pushed element text will be formatted in one of the following ways:
+      // 1) contextPipe.optionsPipe.elementName if element is an option.
+      // 2) contextPipe.elementName if element is not an option and contextPipe is not "Globals.".
+      // 3) elementName if pageTopicPipe is "Globals.".
       category[1].forEach(function(element) {
-        categoryElements.push(pageTitle + element.text);
+        var toPush = contextPipe + optionsPipe + element.text;
+        categoryElements.push(toPush);
+        allIndexElements.push(toPush)
       });
 
-      // Generate the meta tag in the page head. Content will be formatted thus:
-      // "pageTitle.element1;pageTitle.element2;...;pageTitle.elementN;".
+      // Generate the corresponding meta tag in the page head. Content will be formatted thus:
+      // "[pageTopicPipe.][optionsPipe.]elementName1;...;[pageTopicPipe.][optionsPipe.]elementNameN".
       $('head title').before('<meta name=\'' + categoryTitle + '\' content=\'' + categoryElements.join(';') + '\'>');
     });
+
+    // Generate the "all-index-elements" meta tag in the page head if allIndexElements is not empty.
+    if (allIndexElements)
+    {
+      $('head title').before('<meta name=\'' + "all-index-elements" + '\' content=\'' + allIndexElements.join(';') + '\'>');
+    }
   }
 }
 
