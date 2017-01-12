e77b4af50483521d0061ee3fd055b56e02628d0b
olamothe <olamothe@coveo.com>
2017-01-12 17:43:01 -0500

    use branch name from travis when available to tag beta releases.
    https://coveord.atlassian.net/browse/JSUI-36

--- deploy.beta.js
+++ deploy.beta.js
@@ -3,9 +3,11 @@ const login = require('npm-cli-login');
 const exec = require('child_process').exec;
 login('coveo', process.env.NPM_PW, 'sandbox_JSUI@coveo.com');
 
+const travisBranchName = process.env.TRAVIS_BRANCH || '';
+
 console.log('executing deploy script beta');
 setTimeout(function () {
-  exec('npm publish --tag beta', function (error, stdout, stderr) {
+  exec(`npm publish --tag ${travisBranchName}-beta`, function (error, stdout, stderr) {
     console.log(error, stdout, stderr);
   })
 }, 2000);
