0d3b5d88955667e52d692298e9484c5eec3f0b77
Dominique Bégin <dominique.begin.0@gmail.com>
2017-01-16 12:46:57 -0500

    Promisified templatesParser
    https://coveord.atlassian.net/browse/JSUI-1361

--- gulpTasks/templatesParser.js
+++ gulpTasks/templatesParser.js
@@ -1,9 +1,6 @@
-var fs = require('fs');
 var path = require('path');
-var Q = require('q');
-var fsReaddir = Q.denodeify(fs.readdir);
-var fsWriteFile = Q.denodeify(fs.writeFile);
-var fsStat = Q.denodeify(fs.stat);
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 
 function createDirectory(directory) {
   if (!fs.existsSync(directory)) {
@@ -15,57 +12,48 @@ function createDirectory(directory) {
   }
 }
 
-function findTemplates(directory, callback) {
-  fsReaddir(directory).then(files => {
-    Q.all(files.map(file => {
-      var filePath = path.join(directory, file);
-      var extname = path.extname(file);
-      var deferred = Q.defer();
-      fsStat(filePath).then(stat => {
-        if (stat.isDirectory()) {
-          findTemplates(filePath, subTemplates => {
-            deferred.resolve(subTemplates);
-          });
-        } else if (stat.isFile() && (extname == '.ejs' || extname == '.html')) {
-          deferred.resolve([filePath]);
-        } else {
-          deferred.resolve([]);
-        }
-      });
-      return deferred.promise;
-    })).then(results => callback(results.reduce((r1, r2) => r1.concat(r2))));
-  });
+function isTemplateFile(file) {
+  const extname = path.extname(file);
+  return (extname == '.ejs' || extname == '.html');
 }
 
-function parseDirectory(directory, conditions, callback) {
-  findTemplates(directory, templates => {
-    callback(templates.map(template => {
-      var extname = path.extname(template);
-      var name = path.basename(template, extname);
-      var subtemplate = (name.indexOf("_") == 0);
-      name = subtemplate ? name.substr(1) : name;
-      var condition = conditions[name];
-      var content = fs.readFileSync(template).toString();
-      var templateObj = {
-        name: name,
-        type: (extname == '.html' ? 'HtmlTemplate' : 'UnderscoreTemplate'),
-        condition: condition,
-        subtemplate: subtemplate,
-        path: path.dirname(path.relative(directory, template)),
-        content: content,
-        priority: condition != null ? condition.priority || 0 : -1
-      };
-      buildRegisterTemplate(templateObj);
-      buildTemplateHtml(templateObj);
-      return templateObj;
-    }).sort((a, b) => a.priority < b.priority ? 1 : -1));
-  });
+function readDirectory(directory) {
+  return fs.readdirAsync(directory).map(fileName => {
+    const fullPath = path.join(directory, fileName);
+    return fs.statAsync(fullPath).then(stat => stat.isDirectory() ? readDirectory(fullPath) : fullPath);
+  })
+    .reduce((a, b) => a.concat(b), [])
+    .filter(path => isTemplateFile(path));
+}
+
+function parseDirectory(directory, conditions) {
+  return readDirectory(directory).map(template => {
+    var extname = path.extname(template);
+    var name = path.basename(template, extname);
+    var subtemplate = (name.indexOf("_") == 0);
+    name = subtemplate ? name.substr(1) : name;
+    var condition = conditions[name];
+    var content = fs.readFileSync(template).toString();
+    var templateObj = {
+      name: name,
+      type: (extname == '.html' ? 'HtmlTemplate' : 'UnderscoreTemplate'),
+      condition: condition || {},
+      subtemplate: subtemplate,
+      path: path.dirname(path.relative(directory, template)),
+      content: content,
+      priority: condition != null ? condition.priority || 0 : -1
+    };
+    buildRegisterTemplate(templateObj);
+    buildTemplateHtml(templateObj);
+    return templateObj;
+  }).then(templates => templates.sort((a, b) => a.priority < b.priority ? 1 : -1));
 }
 
 function buildRegisterTemplate(template) {
   template.js = 'Coveo.TemplateCache.registerTemplate(' + [
     JSON.stringify(template.name),
     'Coveo.' + template.type + '.fromString(' + JSON.stringify(template.content) + (template.condition != null ? ', ' + JSON.stringify(template.condition.value) : '') + ')',
+    template.condition.layout ? `"${template.condition.layout}"` : 'undefined',
     (!template.subtemplate).toString(),
     (!template.subtemplate).toString()
   ].join(', ') + ')';
@@ -73,15 +61,15 @@ function buildRegisterTemplate(template) {
 
 function buildTemplateHtml(template) {
   template.html = '<script id=' + JSON.stringify(template.name) +
-      ' class="result-template" type="' + ( template.type == 'HtmlTemplate' ? 'text/html' : 'text/underscore' ) + '" ' +
-      (template.condition != null ? 'data-condition=' + JSON.stringify(template.condition.value) : '') + ' >' +
-      template.content + '</script>';
+    ' class="result-template" type="' + ( template.type == 'HtmlTemplate' ? 'text/html' : 'text/underscore' ) + '" ' +
+    (template.condition != null ? 'data-condition=' + JSON.stringify(template.condition.value) : '') + ' >' +
+    template.content + '</script>';
 }
 
 function compileTemplates(directory, destination, fileName, conditions, done) {
   createDirectory(destination);
-  parseDirectory(directory, conditions, templates => {
-    var groupedTemplates = {};
+  parseDirectory(directory, conditions).then(templates => {
+    let groupedTemplates = {};
     groupedTemplates[fileName] = [];
     templates.forEach(template => {
       if (groupedTemplates[template.path] == null) {
@@ -90,16 +78,12 @@ function compileTemplates(directory, destination, fileName, conditions, done) {
       groupedTemplates[template.path].push(template.js);
       groupedTemplates[fileName].push(template.js);
     });
-    Q.all(Object.keys(groupedTemplates).map(key => {
-      return fsWriteFile(path.join(destination, key + '.js'), groupedTemplates[key].join('\n'));
-    }))
-    .catch(e => {
-      done(e)
+    return groupedTemplates;
+  }).then(groupedTemplates => {
+    Object.keys(groupedTemplates).forEach(key => {
+      fs.writeFileAsync(path.join(destination, key + '.js'), groupedTemplates[key].join('\n'));
     })
-    .then(() => {
-      done()
-    });
-  })
+  }).catch(e => done(e));
 }
 
 module.exports = {
