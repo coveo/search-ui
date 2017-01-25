f13f7eec4055fa5d3dc0e238e1db926a16b4f77d
Frederic Beaudoin <fbeaudoin@wks-000564.corp.coveo.com>
2017-01-25 12:22:53 -0500

    Exposed class in public documentation (see JSUI-862).

--- src/events/CardOverlayEvents.ts
+++ src/events/CardOverlayEvents.ts
@@ -1,4 +1,23 @@
+/**
+ *The CardOverlayEvents class contains string definitions for all events related to the {@link CardOverlay} component.
+ */
 export class CardOverlayEvents {
+
+  /**
+   * Opening a {@link CardOverlay} component triggers this event (see {@link CardOverlay.openOverlay}).
+   *
+   * Handlers receive the HTML element corresponding to the current CardOverlay.
+   *
+   * @type {string}
+   */
   public static openCardOverlay = 'openCardOverlay';
+
+  /**
+   * Closing a {@link CardOverlay} component triggers this event (see {@link CardOverlay.closeOverlay}).
+   *
+   * Handlers receive the HTML element corresponding to the current CardOverlay.
+   *
+   * @type {string}
+   */
   public static closeCardOverlay = 'closeCardOverlay';
 }
