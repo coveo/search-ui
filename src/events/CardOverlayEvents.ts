ff0af5e25ceacde5d0ba80e3efefb8d9717851f3
Frederic Beaudoin <fbeaudoin@wks-000564.corp.coveo.com>
2017-01-30 16:18:06 -0500

    Removed some misleading information from the CardOverlayEvents public documentation.

--- src/events/CardOverlayEvents.ts
+++ src/events/CardOverlayEvents.ts
@@ -6,16 +6,12 @@ export class CardOverlayEvents {
   /**
    * Opening a {@link CardOverlay} component triggers this event (see {@link CardOverlay.openOverlay}).
    *
-   * Handlers receive the HTML element corresponding to the current CardOverlay.
-   *
    * @type {string}
    */
   public static openCardOverlay = 'openCardOverlay';
 
   /**
    * Closing a {@link CardOverlay} component triggers this event (see {@link CardOverlay.closeOverlay}).
-   *
-   * Handlers receive the HTML element corresponding to the current CardOverlay.
    *
    * @type {string}
    */
