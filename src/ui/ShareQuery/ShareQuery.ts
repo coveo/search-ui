e8b07fc8314cb9fdf984bbf9109e809ceb42c5fb
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-02-07 14:24:54 -0500

    DOC-458 ShareQuery documentation revision

--- src/ui/ShareQuery/ShareQuery.ts
+++ src/ui/ShareQuery/ShareQuery.ts
@@ -13,9 +13,9 @@ export interface IShareQueryOptions {
 }
 
 /**
- * ShareQuery shows 2 text boxes, one with a shareable link and the
- * other with the complete query expression of the currently performed query.<br/>
- * It populates the {@link Settings} components menu for easy access.
+ * The ShareQuery component populates the {@link Settings} popup menu with the **Share Query** menu item. When the end
+ * user clicks this item, it displays a panel containing two input boxes: one containing a shareable link and the other
+ * containing the complete current query expression.
  */
 export class ShareQuery extends Component {
   static ID = 'ShareQuery';
@@ -26,10 +26,11 @@ export class ShareQuery extends Component {
   private completeQuery: HTMLInputElement;
 
   /**
-   * Create a new ShareQuery component.
-   * @param element
-   * @param options
-   * @param bindings
+   * Creates a new ShareQuery component.
+   * @param element The HTMLElement on which to instantiate the component.
+   * @param options The options for the ShareQuery component.
+   * @param bindings The bindings that the component requires to function normally. If not set, these will be
+   * automatically resolved (with a slower execution time).
    */
   constructor(public element: HTMLElement, public options: IShareQueryOptions, bindings?: IComponentBindings) {
     super(element, ShareQuery.ID, bindings);
@@ -48,42 +49,42 @@ export class ShareQuery extends Component {
   }
 
   /**
-   * Show the ShareQuery
+   * Shows the **Share Query** panel.
    */
   public open() {
     $$(this.element).addClass('coveo-share-query-opened');
   }
 
   /**
-   * Hide the ShareQuery
+   * Hides the **Share Query** panel.
    */
   public close() {
     $$(this.element).removeClass('coveo-share-query-opened');
   }
 
   /**
-   * Get the link to the current query
+   * Gets the link to the current query.
    */
   public getLinkToThisQuery(): string {
     return this.linkToThisQuery.value;
   }
 
   /**
-   * Set the link to the current query
+   * Sets the link to the current query.
    */
   public setLinkToThisQuery(link: string): void {
     this.linkToThisQuery.value = link;
   }
 
   /**
-   * Get the complete query string
+   * Gets the complete query expression string
    */
   public getCompleteQuery(): string {
     return this.completeQuery.value;
   }
 
   /**
-   * Set the complete query string
+   * Set the complete query expression string.
    */
   public setCompleteQuery(completeQuery: string) {
     this.completeQuery.value = completeQuery;
