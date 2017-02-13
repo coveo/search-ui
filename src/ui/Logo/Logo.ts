dcf55c14c6d2e16efa23bb453ea323cd3c675299
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-02-13 16:33:19 -0500

    DOC-458 Logo - Exposed constructor in public doc

--- src/ui/Logo/Logo.ts
+++ src/ui/Logo/Logo.ts
@@ -17,6 +17,13 @@ export class Logo extends Component {
 
   static options: ILogoOptions = {};
 
+  /**
+   * Creates a new Logo component.
+   * @param element The HTMLElement on which to instantiate the component.
+   * @param options The options for the Logo component.
+   * @param bindings The bindings that the component requires to function normally. If not set, these will be
+   * automatically resolved (with a slower execution time).
+   */
   constructor(public element: HTMLElement, public options?: ILogoOptions, bindings?: IComponentBindings) {
     super(element, Logo.ID, bindings);
     this.options = ComponentOptions.initComponentOptions(element, Logo, options);
