9b17ccfcc77c13bc732c2292c1219b146dc1d342
André Thériault <atheriault2@coveo.com>
2017-02-14 14:25:24 -0500

    New option in the EndpointCaller to support the XmlHttpRequest [JSUI-1433]
    https://coveord.atlassian.net/browse/JSUI-1433

--- test/rest/EndpointCallerTest.ts
+++ test/rest/EndpointCallerTest.ts
@@ -28,6 +28,27 @@ export function EndpointCallerTest() {
         expect(jasmine.Ajax.requests.mostRecent().url).toBe('this is an XMLHTTPRequest');
       });
 
+      it('should use the provided XMLHTTPRequest', function () {
+        var xmlHttpRequest = XMLHttpRequest;
+        xmlHttpRequest.prototype.send = function () {
+          throw new Error('My working XmlHttpRequest implementation');
+        };
+
+        var endpointCaller = new EndpointCaller({ xmlHttpRequest: xmlHttpRequest });
+        try {
+          endpointCaller.call({
+            method: 'POST',
+            requestData: {},
+            url: 'this is an XMLHTTPRequest',
+            queryString: [],
+            responseType: 'text',
+            errorsAsSuccess: false
+          });
+        } catch (e) {
+          expect(e.message).toBe('My working XmlHttpRequest implementation');
+        }
+      });
+
       it('should set the auth if provided', function () {
         var endpointCaller = new EndpointCaller({
           accessToken: 'myToken'
