1eabdd37c5427adc1bdfd587c286faa802a2e43a
Dominique Bégin <dominique.begin.0@gmail.com>
2017-02-20 09:58:49 -0500

    Used merged options instead of base options in TemplateList
    https://coveord.atlassian.net/browse/JSUI-1391

--- src/ui/Templates/TemplateList.ts
+++ src/ui/Templates/TemplateList.ts
@@ -12,12 +12,12 @@ export class TemplateList extends Template {
   instantiateToString(object: IQueryResult | ITemplateMetaFields, instantiateOptions: IInstantiateTemplateOptions = {}): string {
     let merged = new DefaultInstantiateTemplateOptions().merge(instantiateOptions);
 
-    if (instantiateOptions.role != null) {
-      const roledTemplate = _.find(this.templates, t => t.role === instantiateOptions.role);
+    if (merged.role != null) {
+      const roledTemplate = _.find(this.templates, t => t.role === merged.role);
       if (roledTemplate) {
-        return roledTemplate.instantiateToString(object, instantiateOptions);
+        return roledTemplate.instantiateToString(object, merged);
       } else {
-        return new DefaultResultTemplate().instantiateToString(object, instantiateOptions);
+        return new DefaultResultTemplate().instantiateToString(object, merged);
       }
     }
     const filteredTemplates = _.reject(this.templates, t => t.role != null);
@@ -33,12 +33,12 @@ export class TemplateList extends Template {
   instantiateToElement(object: IQueryResult, instantiateOptions: IInstantiateTemplateOptions = {}): HTMLElement {
     let merged = new DefaultInstantiateTemplateOptions().merge(instantiateOptions);
 
-    if (instantiateOptions.role != null) {
-      const roledTemplate = _.find(this.templates, t => t.role === instantiateOptions.role);
+    if (merged.role != null) {
+      const roledTemplate = _.find(this.templates, t => t.role === merged.role);
       if (roledTemplate) {
-        return roledTemplate.instantiateToElement(object, instantiateOptions);
+        return roledTemplate.instantiateToElement(object, merged);
       } else {
-        return new DefaultResultTemplate().instantiateToElement(object, instantiateOptions);
+        return new DefaultResultTemplate().instantiateToElement(object, merged);
       }
     }
     const filteredTemplates = _.reject(this.templates, t => t.role != null);
