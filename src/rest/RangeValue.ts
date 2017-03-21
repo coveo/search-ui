da61164df8c9c58b678cd75468c90a3b0d439ac9
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-03-21 10:35:20 -0400

    DOC-635 - Added some info about FacetRange VS query function generated fields - Also revised the IRangeValue documentation

--- src/rest/RangeValue.ts
+++ src/rest/RangeValue.ts
@@ -1,21 +1,31 @@
 /**
- * Describe a single rangle value in a group by
+ * The IRangeValue interface describes a single range of values in a group by clause.
  */
 export interface IRangeValue {
+
   /**
-   * Start of the range.
+   * Specifies the start of the range.
+   *
+   * E.g., `0`
    */
   start?: any;
+
   /**
-   * End of the range
+   * Specifies the end of the range.
+   *
+   * E.g., `500`
    */
   end?: any;
+
   /**
-   * Label to generate for this range
+   * Specifies the label to generate for this range.
+   *
+   * E.g., `0 - 500`
    */
   label?: string;
+
   /**
-   * Is the end parameter included or excluded in this range.
+   * Specifies whether to include the value of the [end]{@link IRangeValue.end} property in this range.
    */
   endInclusive?: boolean;
 }
