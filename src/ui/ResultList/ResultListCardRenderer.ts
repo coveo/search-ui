0d9b86f9d903aa43d7c27a323936e1bc5c8e665d
Dominique Bégin <dominique.begin.0@gmail.com>
2017-04-18 09:52:38 -0400

    Whitespace
    https://coveord.atlassian.net/browse/JSUI-1391

--- src/ui/ResultList/ResultListCardRenderer.ts
+++ src/ui/ResultList/ResultListCardRenderer.ts
@@ -7,7 +7,7 @@ export class ResultListCardRenderer extends ResultListRenderer {
     if (!_.isEmpty(resultElements)) {
       if (!this.resultListOptions.enableInfiniteScroll) {
         // Used to prevent last card from spanning the grid's whole width
-        _.times(3, () => this.resultListOptions.resultContainer.appendChild($$('div', { className: 'coveo-card-layout'}).el));
+        _.times(3, () => this.resultListOptions.resultContainer.appendChild($$('div', { className: 'coveo-card-layout' }).el));
       }
     }
   }
