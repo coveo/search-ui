13b3fbf6bf144c62b549572d87d4bfc209df6fae
dms1lva <dario@live.ca>
2017-04-25 15:41:32 -0400

    set public path
    https://coveord.atlassian.net/browse/JSUI-1559

--- src/setPublicPath.ts
+++ src/setPublicPath.ts
@@ -1,6 +1,19 @@
+import { Utils } from './utils/Utils';
+
 // Dynamically set the public path to load the chunks relative to the Coveo script
+// Fallback on last parsed script of document.currentScript is not available.
 export function setPublicPath() {
-  let scripts = document.getElementsByTagName('script');
-  let path = scripts[scripts.length - 1].src.replace(/[\w.]*.\.js/, '');
+  let path;
+  if (Utils.isNullOrUndefined(document.currentScript)) {
+    let scripts = document.getElementsByTagName('script');
+    path = parseScriptDirectoryPath(scripts[scripts.length - 1]);
+  } else {
+    let script = <HTMLScriptElement>document.currentScript;
+    path = parseScriptDirectoryPath(script);
+  }
   __webpack_public_path__ = path;
+}
+
+function parseScriptDirectoryPath(script: HTMLScriptElement) {
+  return script.src.replace(/[\w.]*.\.js/, '');
 }
