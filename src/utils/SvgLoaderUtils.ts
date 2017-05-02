62404d634080bae30f5b3bc05e213e9ffe864f88
dms1lva <dario@live.ca>
2017-05-02 09:33:11 -0400

    stuff
    https://coveord.atlassian.net/browse/JSUI-1505

--- src/utils/SvgLoaderUtils.ts
+++ src/utils/SvgLoaderUtils.ts
@@ -5,10 +5,11 @@ export class SVGLoaderUtils {
   public static buildSVG(id: string, container: HTMLElement): Dom {
     let svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
     
-    let use = $$('use', { 'xlink:href': 'http://localhost:8080/doctype.svg#' + id });
-    svg.appendChild(use.el);
+    let use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    use.setAttribute('xlink:href', 'http://localhost:8080/doctype.svg#' + id);
+    svg.appendChild(use);
     let placeholder = $$('span', { class: 'coveo-placeholder' });
-    svg.appendChild(use.el);
+    svg.appendChild(use);
     container.appendChild(svg);
     container.appendChild(placeholder.el);
     return $$(<any>svg);
