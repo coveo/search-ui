2b23a51379f633138a3d438b978c2e8ce021cf36
olamothe <olamothe@coveo.com>
2017-05-15 10:44:24 -0400

    Linter
    https://coveord.atlassian.net/browse/SC-1863

--- src/ui/FacetSlider/FacetSlider.ts
+++ src/ui/FacetSlider/FacetSlider.ts
@@ -774,7 +774,7 @@ export class FacetSlider extends Component {
       // but also look for the complete result set when determining if we should show the facet.
       // This allows simple slider facet to still show with query function fields
       if (this.isSimpleSliderConfig()) {
-        this.isEmpty = data.results.results.length == 0
+        this.isEmpty = data.results.results.length == 0;
       } else {
         this.isEmpty = true;
       }
