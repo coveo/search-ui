77b53e74c2b3459235bef691eae12574a3fb33a8
Unknown <pmongeau@coveo.com>
2017-05-16 14:51:41 -0400

    DateUtils doc
    https://coveord.atlassian.net/browse/JSUI-1496

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,17 +1,74 @@
 import { Options } from '../misc/Options';
 import { Utils } from './Utils';
 import { l } from '../strings/Strings';
-import { TimeSpan } from './TimeSpanUtils';
-import * as Globalize from 'globalize';
 import * as _ from 'underscore';
 import * as moment from 'moment';
-
+/**
+ * The 'DateUtils' class renders dates and times using momentJS using the right culture languages and formats.
+ *
+ * This class can transform strings, numbers and date objects into a standardized ISO_8601 Date.
+ *
+ */
 export interface IDateToStringOptions {
+  /**
+   * Specifies whether the date and time are the current ones
+   */
   now?: Date;
+  /**
+   * Specifies whether the date should be displayed normally or using Today, Yesterday and Tomorrow
+   *
+   * **Examples**
+   *
+   * - If the date to render is equal to the current date, it will display 'Today':
+   *    Current date: march 8 2017, date to render: march 8 2017, final result: 'Today'
+   *
+   * - If the date to render is equal to the current date plus or minus 1, it will display Yesterday or Tomorrow:
+   *    Current date: march 8 2017, date to render: march 7 2017, final result: 'Yesterday'
+   */
   useTodayYesterdayAndTomorrow?: boolean;
+  /**
+   * Specifies whether the date should be displayed normally or using days of the week if the date it is part of the
+   * current week
+   *
+   * **Example**
+   *
+   * -If the date to render has less than a week's difference with the current date, it will display the corresponding
+   * day:
+   *    Current date: monday march 8 2017, date to render: march 11 2017, final result: 'Next Thursday'
+   *
+   * Default value is 'true'
+   */
   useWeekdayIfThisWeek?: boolean;
+  /**
+   * Specifies whether the date should be displayed normally or without the year
+   *
+   * **Example**
+   *
+   * - If the date to render is the same year as the current one, it won't display the year value:
+   *    Current date: march 8 2017, date to render: 09/22/2017, final result: 'September 22'
+   *
+   * Default value is 'true'
+   */
   omitYearIfCurrentOne?: boolean;
+  /**
+   * Specifies whether the date should be displayed normally or displayed using the complete format
+   *
+   * **Example**
+   *
+   * - If the date to render is 08/04/2017:
+   *    The result will be Friday, August 04, 2017
+   *
+   * Default value is 'true'
+   */
   useLongDateFormat?: boolean;
+  /**
+   * Specifies whether the date should be dislayed normally or if it should also display the timestamp if it is today.
+   *
+   * **Example**
+   *
+   * - If the date to render is equal to the current date, it will display the date and the timestamp:
+   *    Current date: march 8th 2017, date to render: 03/08/2017 7:10, final result: 'Today, 7:10 AM'
+   */
   includeTimeIfToday?: boolean;
   includeTimeIfThisWeek?: boolean;
   alwaysIncludeTime?: boolean;
@@ -40,87 +97,83 @@ export class DateUtils {
     } else if (date !== null && !isNaN(Number(date))) {
       return moment(Number(date)).toDate();
     } else if (_.isString(date)) {
-      var dateMoment = moment(date, ['YYYY/MM/DD@HH:mm:ssZ', moment.ISO_8601]);
+      const dateMoment = moment(date, ['YYYY/MM/DD@HH:mm:ssZ', moment.ISO_8601]);
       return dateMoment.toDate();
     } else {
       return undefined;
     }
   }
 
-  static dateForQuery(date: Date): string {
-    var dateMoment = moment(date);
-    var getMonth = dateMoment.month();
-    var getYear = dateMoment.year();
-    var getDate = dateMoment.date();
-    return getYear + '/' + DateUtils.padNumber((getMonth + 1).toString()) + '/' + DateUtils.padNumber(getDate.toString());
+  static setLocale():void {
+    let currentLocale = String['locale'];
+    if(currentLocale == 'no') {
+      currentLocale = 'nn'
+    }
+    else if(currentLocale == 'es-es') {
+      currentLocale = 'es';
+    }
+    moment.locale(currentLocale);
   }
 
-  private static padNumber(num: string, length: number = 2): string {
-    while (num.length < length) {
-      num = '0' + num;
-    }
-    return num;
+  static dateForQuery(date: Date): string {
+    DateUtils.setLocale();
+    const dateMoment = moment(date).format('YYYY/MM/DD');
+    return dateMoment;
   }
 
   static keepOnlyDatePart(date: Date): Date {
-    var dateMoment = moment(date);
-    var getMonth = dateMoment.month();
-    var getYear = dateMoment.year();
-    var getDate = dateMoment.date();
-    return new Date(getYear, getMonth, getDate);
-  }
-
-  static offsetDateByDays(date: Date, offset: number): Date {
-    var newDate = moment(date.valueOf()).add(offset,'day');
-    return newDate.toDate();
+    DateUtils.setLocale();
+    const dateMoment = moment(date);
+    return new Date(dateMoment.year(), dateMoment.month(),dateMoment.date())
   }
 
   static dateToString(d: Date, options?: IDateToStringOptions): string {
+    DateUtils.setLocale();
     if (Utils.isNullOrUndefined(d)) {
       return '';
     }
 
     options = new DefaultDateToStringOptions().merge(options);
 
-    var dateOnly = DateUtils.keepOnlyDatePart(d);
+    const dateOnly = moment(DateUtils.keepOnlyDatePart(d));
 
     if (options.predefinedFormat) {
-      return Globalize.format(dateOnly, options.predefinedFormat);
+      return dateOnly.format(options.predefinedFormat.replace(/yyyy/g, String.prototype.toUpperCase()));
     }
 
-    var today = DateUtils.keepOnlyDatePart(options.now);
+    let today = moment(DateUtils.keepOnlyDatePart(options.now));
     if (options.useTodayYesterdayAndTomorrow) {
       if (dateOnly.valueOf() == today.valueOf()) {
         return l('Today');
       }
-      var tomorrow = DateUtils.offsetDateByDays(today, 1);
-      if (dateOnly.valueOf() == tomorrow.valueOf()) {
+      if (dateOnly.valueOf() == moment(today).add(1,'days').valueOf()) {
         return l('Tomorrow');
       }
-      var yesterday = DateUtils.offsetDateByDays(today, -1);
-      if (dateOnly.valueOf() == yesterday.valueOf()) {
+      if (dateOnly.valueOf() == moment(today).add(-1,'days').valueOf()) {
         return l('Yesterday');
       }
     }
 
-    var isThisWeek = Math.abs(TimeSpan.fromDates(dateOnly, today).getDays()) < 7;
+    const isThisWeek = dateOnly.diff(moment(today),'weeks') == 0;
     if (options.useWeekdayIfThisWeek && isThisWeek) {
       if (dateOnly.valueOf() > today.valueOf()) {
-        return l('Next') + ' ' + Globalize.format(dateOnly, 'dddd');
+        return l('Next') + ' ' + dateOnly.format('dddd');
+      } else if (dateOnly.valueOf() < today.valueOf()){
+        return l('Last') + ' ' + dateOnly.format('dddd');
       } else {
-        return l('Last') + ' ' + Globalize.format(dateOnly, 'dddd');
+        return dateOnly.format('dddd');
       }
     }
 
-    if (options.omitYearIfCurrentOne && dateOnly.getFullYear() === today.getFullYear()) {
-      return Globalize.format(dateOnly, 'M');
+    if (options.omitYearIfCurrentOne && dateOnly.year() === today.year()) {
+      return dateOnly.format('MMMM DD');
     }
 
     if (options.useLongDateFormat) {
-      return Globalize.format(dateOnly, 'D');
+      return dateOnly.format('dddd, MMMM DD, YYYY');
     }
 
-    return Globalize.format(dateOnly, 'd');
+    return dateOnly.format('M/D/YYYY')
   }
 
   static timeToString(date: Date, options?: IDateToStringOptions): string {
@@ -128,20 +181,21 @@ export class DateUtils {
       return '';
     }
 
-    return Globalize.format(date, 't');
+    return moment(date).format('h:mm A');
   }
 
   static dateTimeToString(date: Date, options?: IDateToStringOptions): string {
+    DateUtils.setLocale();
     if (Utils.isNullOrUndefined(date)) {
       return '';
     }
 
     options = new DefaultDateToStringOptions().merge(options);
 
-    var today = DateUtils.keepOnlyDatePart(options.now);
-    var isThisWeek = Math.abs(TimeSpan.fromDates(date, today).getDays()) < 7;
-    var datePart = DateUtils.dateToString(date, options);
-    var dateWithoutTime = DateUtils.keepOnlyDatePart(date);
+    const today = DateUtils.keepOnlyDatePart(options.now);
+    const isThisWeek = moment(date).diff(moment(today),'weeks') == 0;
+    const datePart = DateUtils.dateToString(date, options);
+    const dateWithoutTime = DateUtils.keepOnlyDatePart(date);
 
     if (options.alwaysIncludeTime || (options.includeTimeIfThisWeek && isThisWeek) || (options.includeTimeIfToday && dateWithoutTime.valueOf() == today.valueOf())) {
       return datePart + ', ' + DateUtils.timeToString(date);
@@ -151,26 +205,20 @@ export class DateUtils {
   }
 
   static monthToString(month: number): string {
-    var date = moment(new Date(1980, month)).toDate();
-    return Globalize.format(date, 'MMMM');
+    DateUtils.setLocale();
+    let date = moment(new Date(1980, month)).toDate();
+    return moment(date).format('MMMM');
   }
 
   static isValid(date: any) {
+    DateUtils.setLocale();
     if (date instanceof Date) {
       return !isNaN(date.getTime());
     }
     return false;
   }
 
-  static timeBetween(from: Date, to: Date) {
-    if (Utils.isNullOrUndefined(from) || Utils.isNullOrUndefined(to)) {
-      return '';
-    }
 
-    return ('0' + ((to.getTime() - from.getTime()) / (1000 * 60 * 60)).toFixed()).slice(-2) +
-        ':' + ('0' + ((to.getTime() - from.getTime()) % (1000 * 60 * 60) / (1000 * 60)).toFixed()).slice(-2) +
-        ':' + ('0' + ((to.getTime() - from.getTime()) % (1000 * 60) / (1000)).toFixed()).slice(-2);
-  }
 }
 
 // Shim for IE8 Date.toISOString
