83fb05fe9fa1d8cc78a85a091663930f130e7994
Unknown <pmongeau@coveo.com>
2017-05-26 10:48:54 -0400

    Only show selectors if query returned results.
    https://coveord.atlassian.net/browse/JSUI-1588

--- src/ui/ResultLayout/ResultLayout.ts
+++ src/ui/ResultLayout/ResultLayout.ts
@@ -262,7 +262,7 @@ export class ResultLayout extends Component {
   }
 
   private updateSelectorAppearance() {
-    if (this.shouldShowSelector()) {
+    if (this.shouldShowSelector() && !QueryEvents.noResults) {
       this.show();
     } else {
       this.hide();
