2d3d1ddf482af01dcddd8d098bbe0658be4c9b86
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2017-05-30 16:32:10 -0400

    Lazy.ts double-quote to single-quote
    I'm sorry travis

--- src/Lazy.ts
+++ src/Lazy.ts
@@ -2,7 +2,7 @@ export * from './Core';
 
 export { SearchInterface, StandaloneSearchInterface } from './ui/SearchInterface/SearchInterface';
 
-export { PublicPathUtils } from "./utils/PublicPathUtils";
+export { PublicPathUtils } from './utils/PublicPathUtils';
 
 import { PublicPathUtils } from './utils/PublicPathUtils';
 PublicPathUtils.detectPublicPath();
