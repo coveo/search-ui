64eee7e1a3fa05f67e0f313f8c36fb36dc281fd0
Francois LG <francois.lguillemette@gmail.com>
2017-05-30 16:00:38 -0400

    Export Coveo.PublicPathUtils. Allow to get the public path without setting it.

--- src/utils/PublicPathUtils.ts
+++ src/utils/PublicPathUtils.ts
@@ -9,16 +9,18 @@ export class PublicPathUtils {
   // Fallback on last parsed script if document.currentScript is not available.
   public static detectPublicPath() {
     if (!this.pathHasBeenConfigured) {
-      let path;
-      let currentScript = DomUtils.getCurrentScript();
-      if (Utils.isNullOrUndefined(currentScript)) {
-        let scripts = document.getElementsByTagName('script');
-        path = this.parseScriptDirectoryPath(scripts[scripts.length - 1]);
-      } else {
-        let script = currentScript;
-        path = this.parseScriptDirectoryPath(script);
-      }
-      __webpack_public_path__ = path;
+      __webpack_public_path__ = PublicPathUtils.getDynamicPublicPath();
+    }
+  }
+
+  public static getDynamicPublicPath() {
+    let currentScript = DomUtils.getCurrentScript();
+    if (Utils.isNullOrUndefined(currentScript)) {
+      let scripts = document.getElementsByTagName('script');
+      return this.parseScriptDirectoryPath(scripts[scripts.length - 1]);
+    } else {
+      let script = currentScript;
+      return this.parseScriptDirectoryPath(script);
     }
   }
 
