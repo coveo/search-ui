75d0b4e65e3996e2b15101014e822a221addf883
Unknown <pmongeau@coveo.com>
2017-06-07 16:58:18 -0400

    Prettify modifications
    https://coveord.atlassian.net/browse/JSUI-1608

--- src/ui/SimpleFilter/LazySimpleFilter.ts
+++ src/ui/SimpleFilter/LazySimpleFilter.ts
@@ -3,12 +3,12 @@ import { LazyInitialization } from '../Base/Initialization';
 import { lazyExport } from '../../GlobalExports';
 
 export function lazySimpleFilter() {
-    LazyInitialization.registerLazyComponent('SimpleFilter', () => {
-        return new Promise((resolve, reject) => {
-            require.ensure(['./SimpleFilter'], () => {
-                let loaded = require<IComponentDefinition>('./SimpleFilter.ts')['SimpleFilter'];
-                lazyExport(loaded, resolve);
-            }, LazyInitialization.buildErrorCallback('SimpleFilter'), 'SimpleFilter');
-        });
+  LazyInitialization.registerLazyComponent('SimpleFilter', () => {
+    return new Promise((resolve, reject) => {
+      require.ensure(['./SimpleFilter'], () => {
+        let loaded = require<IComponentDefinition>('./SimpleFilter.ts')['SimpleFilter'];
+        lazyExport(loaded, resolve);
+      }, LazyInitialization.buildErrorCallback('SimpleFilter', resolve), 'SimpleFilter');
     });
+  });
 }
