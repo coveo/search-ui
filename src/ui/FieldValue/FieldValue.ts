0da8de750034eab673d27fe41a37acd57a9ce56b
dms1lva <dario@live.ca>
2017-06-16 11:04:18 -0400

    capitalize first letter when rendering a field value
    https://coveord.atlassian.net/browse/JSUI-1589

--- src/ui/FieldValue/FieldValue.ts
+++ src/ui/FieldValue/FieldValue.ts
@@ -13,6 +13,7 @@ import { Facet } from '../Facet/Facet';
 import { $$ } from '../../utils/Dom';
 import * as _ from 'underscore';
 import { exportGlobally } from '../../GlobalExports';
+import { StringUtils } from '../../utils/StringUtils';
 
 export interface IFieldValueOptions {
   field?: IFieldOption;
@@ -270,6 +271,7 @@ export class FieldValue extends Component {
       }
     }
 
+    toRender = StringUtils.capitalizeFirstLetter(toRender);
     if (this.options.htmlValue) {
       element.innerHTML = toRender;
     } else {
