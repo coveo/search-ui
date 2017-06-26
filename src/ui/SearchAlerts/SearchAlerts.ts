a70b202be5e9b11a7a6b3f92e5b68e01de034a14
olamothe <olamothe@coveo.com>
2017-06-26 15:41:48 -0400

    Title for search alerts context
    https://coveord.atlassian.net/browse/JSUI-1663

--- src/ui/SearchAlerts/SearchAlerts.ts
+++ src/ui/SearchAlerts/SearchAlerts.ts
@@ -355,6 +355,7 @@ export class SearchAlerts extends Component {
 
     const contextElement = $$('td', {
       className: 'coveo-subscriptions-panel-context',
+      title : context
     });
     contextElement.setHtml(context);
 
