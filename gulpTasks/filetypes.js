39e350f61867f211e026bc79ccb0982d43bab789
dms1lva <dario@live.ca>
2017-07-11 15:09:10 -0400

    fix background color
    https://coveord.atlassian.net/browse/JSUI-1505

--- gulpTasks/filetypes.js
+++ gulpTasks/filetypes.js
@@ -45,27 +45,37 @@ function generateSass(json) {
   // match all the time the casing output by the connectors.
   var sass = '';
 
+  sass += '@import "../node_modules/@salesforce-ux/design-system/scss/design-tokens";\n';
+  
+  sass += '.coveo-filetype-custom { background-image: url(../../image/svg/filetypes/custom.svg); width: 60px; height: 60px; background-size: 60px 60px;\n &.coveo-small {width: 30px; height: 30px;} background-size: 30px 30px;};\n';
+  
+  sass += '@mixin background-color($id) { background-color: map-get($bg-standard-map, $id) };\n';
+
   sass += '@mixin GeneratedIcons() {\n';
   sass += '  .coveo-icon-caption-overlay { display: none; }';
 
   var defaultIcon = '.coveo-sprites-custom';
 
   sass += '  &.objecttype {\n';
+  sass += '@extend .coveo-filetype-custom;\n'
   sass +=     'display: inline-block;\n'
   sass += generateInnerObjecttype(json, false);
   sass += '  }\n';
 
   sass += '  &.objecttype.coveo-small {\n';
+  sass += '@extend .coveo-filetype-custom;\n'
   sass += '    display: inline-block;\n'
   sass += generateInnerObjecttype(json, true);
   sass += '  }\n';
 
   sass += '  &.filetype, &.sysfiletype {\n'; // we include the version with a sys prefix for backward compatibility
+  sass += '@extend .coveo-filetype-custom;\n'
   sass += '    display: inline-block;\n'
   sass += generateInnerFiletype(json, false)
   sass += '  }\n';
 
   sass += '  &.filetype.coveo-small, &.sysfiletype.coveo-small {\n';
+  sass += '@extend .coveo-filetype-custom;\n'
   sass += 'display: inline-block;\n'
   sass += generateInnerFiletype(json, true);
   sass += '  }\n';
@@ -74,11 +84,15 @@ function generateSass(json) {
 
   return sass;
 }
-
 function generateInnerObjecttype(json, small) {
   let ret = '';
   _.each(_.keys(json.objecttype), function (objecttype) {
     const svgName = json.objecttype[objecttype].icon;
+    let backgroundColor = '';
+    if (svgName.indexOf('salesforce') != -1) {
+      let basename = svgName.split('-').slice(2).join('-');
+      backgroundColor = '@include background-color(\'' + basename + '\'); border-radius: $default-border-radius; ';
+    }
     ensureImageIsValid(svgName, objecttype);
     // This is a special case that we still need to support
     // Old templates in salesforce are using something like this
@@ -88,7 +102,7 @@ function generateInnerObjecttype(json, small) {
     let width, height;
     ({width, height} = getSVGSize(svgName, small));
     ret += '    &.' + objecttype.toLowerCase() +
-      ` { width: ${width}px; height: ${height}px; background-size: ${width}px ${height}px; background-image: url(../../image/svg/filetypes/${svgName}.svg);` +
+      ` { ${backgroundColor}width: ${width}px; height: ${height}px; background-size: ${width}px ${height}px; background-image: url(../../image/svg/filetypes/${svgName}.svg);` +
       generateShouldDisplayLabel(json.objecttype[objecttype].shouldDisplayLabel) +
       ' }\n';
   });
@@ -99,6 +113,11 @@ function generateInnerFiletype(json, small) {
    let ret = '';
   _.each(_.keys(json.filetype), function (filetype) {
     const svgName = json.filetype[filetype].icon;
+    let backgroundColor = '';
+    if (svgName.indexOf('salesforce') != -1) {
+      let basename = svgName.split('-').slice(2).join('-');
+      backgroundColor = '@include background-color(\'' + basename + '\'); border-radius: $default-border-radius; ';
+    }
     ensureImageIsValid(svgName, filetype);
     // Be careful to output lowercase filetypes, since the JS UI helpers do the same,
     // and CSS class names are case sensitive. I do that because I can't expect to
@@ -106,7 +125,7 @@ function generateInnerFiletype(json, small) {
     let width, height;
     ({width, height} = getSVGSize(svgName,small));
     ret += '    &.' + removeSpace(filetype.toLowerCase()) +
-      ` { width: ${width}px; height: ${height}px; background-size: ${width}px ${height}px; background-image: url(../../image/svg/filetypes/${svgName}.svg);` +
+      ` { ${backgroundColor}width: ${width}px; height: ${height}px; background-size: ${width}px ${height}px; background-image: url(../../image/svg/filetypes/${svgName}.svg);` +
       generateShouldDisplayLabel(json.filetype[filetype].shouldDisplayLabel) +
       '}\n';
 
@@ -126,6 +145,9 @@ function getSVGSize(svgName, small) {
     width = small ? Math.floor(svg.svg.$.width / 2) : svg.svg.$.width;
     height = small ? Math.floor(svg.svg.$.height / 2) : svg.svg.$.height;
   });
+  if (isNaN(width) || isNaN(height)) {
+    console.warn(`${svgName} has no width or/and height attribute.`);
+  }
   return {width: width, height: height}
 }
 
