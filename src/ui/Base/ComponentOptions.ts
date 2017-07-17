e8e1211a785b910929226f5bd94aef93851eed5b
Dario Martins Silva <dario@live.ca>
2017-07-17 17:12:53 -0400

    Jsui 1724 card overlay icons (#560)
    * default icon for small mode
    
    https://coveord.atlassian.net/browse/JSUI-1724
    
    * fix card overlay header icons
    
    https://coveord.atlassian.net/browse/JSUI-1724
    
    * fix tests
    
    https://coveord.atlassian.net/browse/JSUI-1724
    
    * fix merge
    
    https://coveord.atlassian.net/browse/JSUI-1724
    
    * fix trailing whitespace
    
    https://coveord.atlassian.net/browse/JSUI-1724

--- src/ui/Base/ComponentOptions.ts
+++ src/ui/Base/ComponentOptions.ts
@@ -372,14 +372,12 @@ export class ComponentOptions {
   /**
    * Builds an icon option.
    *
-   * This loads an SVG icon from its name.
-   *
-   *
+   * This takes an SVG icon name, validates it and returns the name of the icon.
    * **Markup Examples:**
    *
-   * > `data-foo="user"`
+   * > `data-foo="search"`
    *
-   * > `data-foo="database"`
+   * > `data-foo="facetExpand"`
    *
    * @param optionArgs The arguments to apply when building the option.
    * @returns {string} The resulting option value.
@@ -748,7 +746,10 @@ export class ComponentOptions {
 
   static loadIconOption(element: HTMLElement, name: string, option: IComponentOptions<any>): string {
     const svgIconName = ComponentOptions.loadStringOption(element, name, option);
-    return SVGIcons.icons[svgIconName];
+    if (Utils.isNullOrUndefined(SVGIcons.icons[svgIconName])) {
+      new Logger(element).warn(`Icon with name ${svgIconName} not found.`);
+    }
+    return svgIconName;
   }
 
   static loadFieldOption(element: HTMLElement, name: string, option: IComponentOptionsOption<any>): string {
