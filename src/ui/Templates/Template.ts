fc3e30ce073932f377f1513e529fd24892b422e6
Unknown <pmongeau@coveo.com>
2017-08-01 11:18:04 -0400

    getTemplateInfo tries to return some info
    https://coveord.atlassian.net/browse/JSUI-1741

--- src/ui/Templates/Template.ts
+++ src/ui/Templates/Template.ts
@@ -236,6 +236,7 @@ export class Template implements ITemplateProperties {
   }
 
   getTemplateInfo() {
-    return null;
+    // Try to get info on the template by returning the first parameter found that is not undefined.
+   return (this.conditionToParse != undefined? this.conditionToParse : this.condition != undefined? this.condition : this.fieldsToMatch);
   }
 }
