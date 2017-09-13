05f339d2c33cd9ad2ccf6aeafcc753c0e2594323
COVEO\ssayegh <samisayegh1@gmail.com>
2017-09-13 10:13:02 -0400

    Revert "removing '/' from gulp coverage regex"
    This reverts commit 1b3be2d08b563f234e503bd10581e1a5dec67bbd.

--- gulpTasks/test.js
+++ gulpTasks/test.js
@@ -49,7 +49,7 @@ gulp.task('uploadCoverage', ['lcovCoverage'], shell.task([
 gulp.task('remapCoverage', function (done) {
   return gulp.src(`${COVERAGE_DIR}/coverage-es5.json`)
     .pipe(remapIstanbul({
-      exclude: /(webpack|vertx|es6-promise|analytics|jstimezonedetect|latinize|whatwg|circular-json|d3|d3-.*|dist|process|underscore|coveomagicbox|modal-box|moment|pikaday|test|lib)/
+      exclude: /(webpack|~\/d3\/|~\/es6-promise\/dist\/|~\/process\/|~\/underscore\/|vertx|~\/coveomagicbox\/|~\/d3-.*\/|~\/modal-box\/|~\/moment\/|~\/pikaday\/|test\/|lib\/|es6-promise|analytics|jstimezonedetect|latinize|whatwg|circular-json)/
     }))
     .pipe(rename('coverage.json'))
     .pipe(gulp.dest(COVERAGE_DIR));
