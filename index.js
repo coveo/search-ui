72dd0b17c059a13a5acf8521b75ac7033d2374f5
olamothe <olamothe@coveo.com>
2017-10-04 10:18:00 -0400

    change heroku server to serve qa folder
    https://coveord.atlassian.net/browse/JSUI-1817

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 const express = require('express');
 const app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', process.env.PORT || 8080);
 app.use('/', express.static(__dirname + '/bin'));
-
+app.use('/qa', express.static(__dirname + '/qa'));
 
 app.get('/', function(request, response) {
   console.log('accessing ' + __dirname + '/bin/Index.html');
   response.sendFile(__dirname + '/bin/Index.html');
 });
 
-app.get(/^(.+)$/, function(req, res){
+app.get(/^(.+)$/, function(req, res) {
   console.log('accessing ' + __dirname + req.params[0]);
-     res.sendfile( __dirname + req.params[0]);
+  res.sendfile(__dirname + req.params[0]);
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
