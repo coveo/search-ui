f602edbec76ffc7fe87c989ccf0cc0c1d6bd2165
olamothe <olamothe@coveo.com>
2017-10-04 14:03:17 -0400

    Set travis to build on official branches
    https://coveord.atlassian.net/browse/JSUI-1817

--- index.js
+++ index.js
@@ -2,10 +2,54 @@
 
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
+const bodyParser = require('body-parser');
+app.use(
+  bodyParser.text({
+    type: 'text/html'
+  })
+);
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  user: 'olamothe',
+  database: 'qa'
+});
+
+client.connect();
 
 app.set('port', process.env.PORT || 8080);
 app.use('/', express.static(__dirname + '/bin'));
-app.use('/qa', express.static(__dirname + '/qa'));
+
+app.get('/qa/:pagename', function(request, response) {
+  client.query(`SELECT content FROM pages WHERE name LIKE $1 LIMIT 1;`, [request.params.pagename], (err, res) => {
+    if (err) {
+      response.send(err.message);
+    } else {
+      if (res.rowCount == 1) {
+        return response.send(res.rows[0].content);
+      } else {
+        return response.sendStatus(404);
+      }
+    }
+  });
+});
+
+app.post('/qa/:pagename', function(request, response) {
+  if (request.body) {
+    client.query(`SELECT 1 FROM pages WHERE name LIKE $1;`, [request.params.pagename], (err, res) => {
+      if (res.rowCount == 1) {
+        client.query(`UPDATE pages SET content=$1 WHERE name LIKE $2;`, [request.body, request.params.pagename], (err, res) => {
+          response.sendStatus(200);
+        });
+      } else {
+        client.query(`INSERT INTO pages (content, name) VALUES ($1, $2);`, [request.body, request.params.pagename], (err, res) => {
+          response.sendStatus(200);
+        });
+      }
+    });
+  }
+});
 
 app.get('/', function(request, response) {
   console.log('accessing ' + __dirname + '/bin/Index.html');
