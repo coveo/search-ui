0ac1488e978e01bb81579b9cc1e7a4551f866715
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2017-10-19 15:55:11 -0400

    PR Comments
    https://coveord.atlassian.net/browse/JSUI-1872

--- test/ui/BreadcrumbValuesListTest.ts
+++ test/ui/BreadcrumbValuesListTest.ts
@@ -27,8 +27,7 @@ export function BreadcrumbValueListTest() {
 
     it('should support rendering as a simple string and not an HTML element', () => {
       facet.options.title = 'My facet title';
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const builtAsString = breadcrumbList.buildAsString();
+      const builtAsString = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).buildAsString();
       expect(_.isString(builtAsString)).toBe(true);
       expect(builtAsString).toContain('My facet title');
       expect(builtAsString).toContain('foo0');
@@ -38,22 +37,19 @@ export function BreadcrumbValueListTest() {
 
     it('should output the title with the facet title option', () => {
       facet.options.title = 'My facet title';
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
+      const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
       const title = $$(built).find('.coveo-facet-breadcrumb-title');
       expect($$(title).text()).toContain('My facet title');
     });
 
-    it('should display a clickable label for each selected values', () => {
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
+    it('should display a label for each selected values', () => {
+      const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
       const values = $$(built).findAll('.coveo-facet-breadcrumb-value');
       expect(values.length).toBe(3);
     });
 
     it('should display a caption for each selected values', () => {
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
+      const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
       const captions = $$(built).findAll('.coveo-facet-breadcrumb-caption');
       expect($$(captions[0]).text()).toContain('foo0');
       expect($$(captions[1]).text()).toContain('foo1');
@@ -61,53 +57,77 @@ export function BreadcrumbValueListTest() {
     });
 
     it('should display a clear button for each selected values', () => {
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
+      const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
       const clear = $$(built).findAll('.coveo-facet-breadcrumb-clear');
       expect(clear.length).toBe(3);
     });
 
-    it('should display excluded values differently', () => {
-      facetValues[2].excluded = true;
-      facetValues[2].selected = false;
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
-      const excluded = $$(built).findAll('.coveo-facet-breadcrumb-value.coveo-excluded');
-      expect(excluded.length).toBe(1);
-    });
-
     it('should respect the facet option for the number of values in breadcrumb', () => {
       facet.options.numberOfValuesInBreadcrumb = 2;
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
+      const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
       const andShowMore = $$(built).findAll('.coveo-facet-breadcrumb-multi-count');
       expect(andShowMore.length).toBe(1);
       expect($$(andShowMore[0]).text()).toContain('1 more...');
     });
 
-    it('should add the excluded and selected values together when it exceeds the number of values in breadcrumb', () => {
-      facet.options.numberOfValuesInBreadcrumb = 1;
-      facetValues[2].excluded = true;
-      facetValues[2].selected = false;
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
-      const andShowMore = $$(built).findAll('.coveo-facet-breadcrumb-multi-count');
-      expect(andShowMore.length).toBe(1);
-      expect($$(andShowMore[0]).text()).toContain('2 more...');
-    });
+    describe('when some values are excluded', () => {
+      beforeEach(() => {
+        facetValues[2].excluded = true;
+        facetValues[2].selected = false;
+      });
 
-    it('should show hidden values on clicking show more', () => {
-      facet.options.numberOfValuesInBreadcrumb = 1;
-      facetValues[2].excluded = true;
-      facetValues[2].selected = false;
-      const breadcrumbList = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement);
-      const built = breadcrumbList.build();
-      const andShowMore = $$(built).find('.coveo-facet-breadcrumb-multi-count');
-      $$(andShowMore).trigger('click');
-      const captions = $$(built).findAll('.coveo-facet-breadcrumb-caption');
-      expect($$(captions[0]).text()).toContain('foo0');
-      expect($$(captions[1]).text()).toContain('foo1');
-      expect($$(captions[2]).text()).toContain('foo2');
+      it('should display excluded values differently', () => {
+        const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
+        const excluded = $$(built).findAll('.coveo-facet-breadcrumb-value.coveo-excluded');
+        expect(excluded.length).toBe(1);
+      });
+
+      it('should add the excluded and selected values together when it exceeds the number of values in breadcrumb', () => {
+        facet.options.numberOfValuesInBreadcrumb = 1;
+        const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
+        const andShowMore = $$(built).findAll('.coveo-facet-breadcrumb-multi-count');
+        expect(andShowMore.length).toBe(1);
+        expect($$(andShowMore[0]).text()).toContain('2 more...');
+      });
+
+      it('should display the hidden values correctly when every values are excluded', () => {
+        facetValues = _.map(facetValues, facetValue => {
+          facetValue.excluded = true;
+          facetValue.selected = false;
+          return facetValue;
+        });
+
+        facet.options.numberOfValuesInBreadcrumb = 1;
+        const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
+        const andShowMore = $$(built).findAll('.coveo-facet-breadcrumb-multi-count');
+        expect(andShowMore.length).toBe(1);
+        expect($$(andShowMore[0]).text()).toContain('2 more...');
+      });
+
+      it('should display the hidden values correctly when every values are included', () => {
+        facetValues = _.map(facetValues, facetValue => {
+          facetValue.excluded = false;
+          facetValue.selected = true;
+          return facetValue;
+        });
+
+        facet.options.numberOfValuesInBreadcrumb = 1;
+        const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
+        const andShowMore = $$(built).findAll('.coveo-facet-breadcrumb-multi-count');
+        expect(andShowMore.length).toBe(1);
+        expect($$(andShowMore[0]).text()).toContain('2 more...');
+      });
+
+      it('should show hidden values on clicking show more', () => {
+        facet.options.numberOfValuesInBreadcrumb = 1;
+        const built = new BreadcrumbValueList(facet, facetValues, BreadcrumbValueElement).build();
+        const andShowMore = $$(built).find('.coveo-facet-breadcrumb-multi-count');
+        $$(andShowMore).trigger('click');
+        const captions = $$(built).findAll('.coveo-facet-breadcrumb-caption');
+        expect($$(captions[0]).text()).toContain('foo0');
+        expect($$(captions[1]).text()).toContain('foo1');
+        expect($$(captions[2]).text()).toContain('foo2');
+      });
     });
   });
 }
