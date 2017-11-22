95c096cc53efb141e86c239782bd6d7bd9a097df
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2017-11-22 11:41:36 -0500

    Add AWS sdk
    https://coveord.atlassian.net/browse/JSUI-1923

--- invalidate.cloudfront.js
+++ invalidate.cloudfront.js
@@ -1,33 +1,53 @@
+const exec = require('child_process').exec;
 const colors = require('colors');
 const AWS = require('aws-sdk');
 
 const cloudfront = new AWS.CloudFront();
 const pathToInvalidate = `/searchui/v${process.env.PACKAGE_JSON_VERSION}/*`;
 
-const invalidationRequest = cloudfront.createInvalidation({
-  DistributionId: process.env.AWS_CLOUDFRONT_DISTRIBUTION_ID,
-  InvalidationBatch: {
-    CallerReference: new Date().getTime().toString(),
-    Paths: {
-      Quantity: 1,
-      Items: [pathToInvalidate]
-    }
+const shouldDoInvalidation = () => {
+  return true;
+  /*if (!process.env.TRAVIS) {
+    return false;
   }
-});
+  if (!process.env.TRAVIS_TAG) {
+    return false;
+  }
+  if (process.env.TRAVIS_TAG.indexOf('beta') != -1) {
+    return false;
+  }
+  return true;*/
+};
 
-invalidationRequest.send((error, success) => {
-  if (error) {
-    colors.red('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT');
-    colors.red('*************');
-    colors.red(error.message);
-    colors.red('*************');
-    throw error;
-  }
-  if (success) {
-    colors.green('INVALIDATION ON CLOUDFRONT SUCCESSFUL');
-    colors.green('*************');
-    colors.green(`PATH INVALIDATED: ${pathToInvalidate}`);
-    colors.green(`INVALIDATION ID : ${success.Invalidation.Id}`);
-    colors.green('*************');
-  }
-});
+if (shouldDoInvalidation()) {
+  const invalidationRequest = cloudfront.createInvalidation({
+    DistributionId: process.env.AWS_CLOUDFRONT_DISTRIBUTION_ID,
+    InvalidationBatch: {
+      CallerReference: new Date().getTime().toString(),
+      Paths: {
+        Quantity: 1,
+        Items: [pathToInvalidate]
+      }
+    }
+  });
+
+  invalidationRequest.send((error, success) => {
+    if (error) {
+      colors.red('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT');
+      colors.red('*************');
+      colors.red(error.message);
+      colors.red('*************');
+      exec('travis_terminate 1');
+      throw error;
+    }
+    if (success) {
+      colors.green('INVALIDATION ON CLOUDFRONT SUCCESSFUL');
+      colors.green('*************');
+      colors.green(`PATH INVALIDATED: ${pathToInvalidate}`);
+      colors.green(`INVALIDATION ID : ${success.Invalidation.Id}`);
+      colors.green('*************');
+    }
+  });
+} else {
+  colors.white('INVALIDATION FROM CLOUDFRONT SKIPPED BECAUSE THIS IS NOT AN OFFICIAL RELEASE');
+}
