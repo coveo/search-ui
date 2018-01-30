58a73c871ff4490647fa55ad51353e3d704b0339
Travis CI User <travis@example.org>
2018-01-30 18:37:38 +0000

    publish: Automatically created by Travis CI
    generated from commit a1c58ccddae75057706b1c9bb1326489403e1cac

--- assets/gen/js/CoveoJsSearch.Searchbox.js
+++ assets/gen/js/CoveoJsSearch.Searchbox.js
@@ -203,8 +203,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	"use strict";
 	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.version = {
-	    'lib': '1.2537.42-beta',
-	    'product': '1.2537.42-beta',
+	    'lib': '1.2537.43',
+	    'product': '1.2537.43',
 	    'supportedApiVersion': 2
 	};
 
--- assets/gen/js/playground.js
+++ assets/gen/js/playground.js
@@ -9049,8 +9049,8 @@ var playground =
 	"use strict";
 	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.version = {
-	    'lib': '1.2537.42-beta',
-	    'product': '1.2537.42-beta',
+	    'lib': '1.2537.43',
+	    'product': '1.2537.43',
 	    'supportedApiVersion': 2
 	};
 
