8623800944322f346dd32a4675425207b682776f
Dario Martins Silva <dario@live.ca>
2018-04-23 14:40:42 -0400

    add option to use wildcards for facet searches (#787)
    https://coveord.atlassian.net/browse/JSUI-2051

--- gulpTasks/definition.js
+++ gulpTasks/definition.js
@@ -33,7 +33,7 @@ gulp.task('cleanDefs', function() {
       .pipe(replace(/readonly/gm, ''))
       .pipe(replace(/undefined/g, 'any'))
       .pipe(replace(/ Record<.*>/g, ' any'))
-      .pipe(replace(/(enum [a-zA-Z_$]+\s{$)((?:\n^\s*[a-zA-Z_$]+ = "[a-zA-Z_$\s]+",$)*)/gm, clearEnumVariableDeclaration))
+      .pipe(replace(getEnumRegex(), clearEnumVariableDeclaration))
       .pipe(replace(/extends agGridModule\.[a-zA-Z]+/g, 'extends Object'))
       .pipe(replace(/implements agGridModule\.[a-zA-Z]+/g, 'implements Object'))
       .pipe(replace(/agGridModule\.[a-zA-Z]+/g, 'any'))
@@ -46,6 +46,13 @@ function clearEnumVariableDeclaration(match, p1, p2) {
   let lines = p2.split('\n');
   lines = lines.map(line => line.replace(/ = ["|'][a-zA-Z_$\s]*["|']/, ''));
   return p1 + lines.join('\n');
+}
+
+function getEnumRegex() {
+  const enumIdentifier = '(enum [a-zA-Z_$]+\\s{$)';
+  const enumDeclaration = '\\n^\\s*[a-zA-Z_$]+ = "[a-zA-Z_$\\s]+",?$';
+  const documentation = '\\n^\\s*[@{}\\[\\]\\w\\/*.,\\s]+$';
+  return new RegExp(`${enumIdentifier}((?:${enumDeclaration}|${documentation})*)`, 'gm');
 }
 
 gulp.task('externalDefs', function() {
--- src/rest/GroupByRequest.ts
+++ src/rest/GroupByRequest.ts
@@ -1,5 +1,6 @@
 import { IComputedFieldRequest } from './ComputedFieldRequest';
 import { IRangeValue } from './RangeValue';
+import { AllowedValuesPatternType } from './AllowedValuesPatternType';
 
 /**
  * The `IGroupByRequest` interface describes a Group By operation to perform against the index.
@@ -89,6 +90,12 @@ export interface IGroupByRequest {
    * > The array `["foo", "bar*"]` would return Group By values for `foo` and any value starting with `bar`.
    */
   allowedValues?: string[];
+
+  /**
+   * The type of pattern used in {@link IGroupByRequest.allowedValues}. See {@link AllowedValuesPatternType}.
+   * Not specifying a value for this parameter is equivalent to using legacy.
+   */
+  allowedValuesPatternType?: AllowedValuesPatternType;
 
   /**
    * Specifies an array of computed fields that should be evaluated for each Group By value that is returned.
