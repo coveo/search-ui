19c1a06840b6b45bbae7e5467e9daa6dad3b9c83
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2018-05-03 13:38:50 -0400

    Do not redirect with empty UA object
    https://coveord.atlassian.net/browse/JSUI-2082

--- src/ui/SearchInterface/SearchInterface.ts
+++ src/ui/SearchInterface/SearchInterface.ts
@@ -1032,7 +1032,7 @@ export class StandaloneSearchInterface extends SearchInterface {
       stateValues['firstQueryCause'] = uaCausedBy;
     }
     const uaMeta = this.usageAnalytics.getCurrentEventMeta();
-    if (uaMeta != null) {
+    if (uaMeta != null && !isEmpty(uaMeta)) {
       stateValues['firstQueryMeta'] = uaMeta;
     }
 
