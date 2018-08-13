8ef68d4a778d1b894be333cd7c1599d6f41849da
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2018-08-13 11:39:08 -0400

    Fix couple of issue on ErrorReport accessibility features
    https://coveord.atlassian.net/browse/JSUI-2199

--- accessibilityTest/AccessibilityErrorReport.ts
+++ accessibilityTest/AccessibilityErrorReport.ts
@@ -1,24 +1,44 @@
 import * as axe from 'axe-core';
 import { $$, Component, ErrorReport } from 'coveo-search-ui';
-import { addQueryFilter, afterQueryError, getResultsColumn, getRoot } from './Testing';
+import { addQueryFilter, afterQueryError, getResultsColumn, getRoot, afterQuerySuccess } from './Testing';
 
 export const AccessibilityErrorReport = () => {
   describe('ErrorReport', () => {
-    beforeEach(() => {
-      // !BOOM
-      addQueryFilter('$qre()');
-    });
-
-    it('should be accessible', async done => {
+    const buildErrortReportElement = () => {
       const errorReportElement = $$('div', {
         className: Component.computeCssClassName(ErrorReport)
       });
 
       getResultsColumn().appendChild(errorReportElement.el);
-      await afterQueryError();
-      const axeResults = await axe.run(getRoot());
-      expect(axeResults).toBeAccessible();
-      done();
+    };
+
+    describe('with a query that triggers an error', () => {
+      beforeEach(() => {
+        // !BOOM
+        addQueryFilter('$qre()');
+      });
+
+      it('should be accessible', async done => {
+        buildErrortReportElement();
+        await afterQueryError();
+        const axeResults = await axe.run(getRoot());
+        expect(axeResults).toBeAccessible();
+        done();
+      });
+    });
+
+    describe('with a query that does not trigger an error', () => {
+      beforeEach(() => {
+        addQueryFilter('@uri');
+      });
+
+      it('should be accessible', async done => {
+        buildErrortReportElement();
+        await afterQuerySuccess();
+        const axeResults = await axe.run(getRoot());
+        expect(axeResults).toBeAccessible();
+        done();
+      });
     });
   });
 };
