86110bb9164fce1cdffee27c603a6bfeb4e9dfac
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2018-08-13 15:59:59 -0400

    export types
    https://coveord.atlassian.net/browse/JSUI-2196

--- src/magicbox/doMagicBoxExport.ts
+++ src/magicbox/doMagicBoxExport.ts
@@ -1,5 +1,5 @@
 import { exportGlobally } from '../GlobalExports';
-import { EndOfInputResult, Result } from './Result/Result';
+import { Utils } from '../utils/Utils';
 import { ExpressionConstant } from './Expression/ExpressionConstant';
 import { ExpressionEndOfInput } from './Expression/ExpressionEndOfInput';
 import { ExpressionFunction } from './Expression/ExpressionFunction';
@@ -10,11 +10,13 @@ import { ExpressionRegExp } from './Expression/ExpressionRegExp';
 import { Grammar } from './Grammar';
 import * as Grammars from './Grammars/Grammars';
 import { InputManager } from './InputManager';
-import { MagicBoxInstance, createMagicBox, requestAnimationFrame } from './MagicBox';
+import { createMagicBox, MagicBoxInstance, requestAnimationFrame } from './MagicBox';
 import { OptionResult } from './Result/OptionResult';
 import { RefResult } from './Result/RefResult';
-import { Utils } from '../utils/Utils';
+import { EndOfInputResult, Result } from './Result/Result';
 import { SuggestionsManager } from './SuggestionsManager';
+import { Expression } from './Expression/Expression';
+import { Expression as ExpressionType } from './Expression/Expression';
 
 export function doMagicBoxExport() {
   exportGlobally({
@@ -41,3 +43,27 @@ export function doMagicBoxExport() {
     }
   });
 }
+
+export type Expression = ExpressionType;
+
+export const MagicBox = {
+  EndOfInputResult,
+  ExpressionConstant,
+  ExpressionEndOfInput,
+  ExpressionFunction,
+  ExpressionList,
+  ExpressionOptions,
+  ExpressionRef,
+  ExpressionRegExp,
+  Grammar,
+  Grammars,
+  InputManager,
+  Instance: MagicBoxInstance,
+  OptionResult,
+  RefResult,
+  Result,
+  SuggestionsManager,
+  Utils,
+  create: createMagicBox,
+  requestAnimationFrame
+};
