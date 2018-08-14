4280fad3c00331136f2d4db7e2c1917656fa36e2
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2018-08-14 10:54:55 -0400

    Modify links color with dark background + new accessibility tests
    https://coveord.atlassian.net/browse/JSUI-2201

--- accessibilityTest/AccessibilityCardOverlay.ts
+++ accessibilityTest/AccessibilityCardOverlay.ts
@@ -1,6 +1,14 @@
 import * as axe from 'axe-core';
-import { $$, Component, CardOverlay, get } from 'coveo-search-ui';
-import { afterQuerySuccess, getRoot, testResultElement } from './Testing';
+import { $$, Component, CardOverlay, get, FieldTable, Facet } from 'coveo-search-ui';
+import {
+  afterQuerySuccess,
+  getRoot,
+  testResultElement,
+  getFacetColumn,
+  afterDeferredQuerySuccess,
+  addFieldEqualFilter,
+  afterDelay
+} from './Testing';
 
 export const AccessibilityCardOverlay = () => {
   describe('CardOverlay', () => {
@@ -8,6 +16,11 @@ export const AccessibilityCardOverlay = () => {
       return $$('div', {
         className: Component.computeCssClassName(CardOverlay)
       });
+    };
+
+    const getOverlayInstance = () => {
+      const cardOverlayInDocument = $$(getRoot()).find(`.${Component.computeCssClassName(CardOverlay)}`);
+      return get(cardOverlayInDocument as HTMLElement) as CardOverlay;
     };
 
     it('should be accessible', async done => {
@@ -21,18 +34,62 @@ export const AccessibilityCardOverlay = () => {
     });
 
     it('should be accessible when opened', async done => {
-      const cardOverlayElement = $$('div', {
-        className: Component.computeCssClassName(CardOverlay)
-      });
+      const cardOverlayElement = getOverlayElement();
 
       testResultElement(cardOverlayElement.el);
       await afterQuerySuccess();
-      const cardOverlayInDocument = $$(getRoot()).find(`.${Component.computeCssClassName(CardOverlay)}`);
-      const cardOverlayInstance = get(cardOverlayInDocument as HTMLElement) as CardOverlay;
-      cardOverlayInstance.openOverlay();
+      getOverlayInstance().openOverlay();
+      await afterDelay(1000);
       const axeResults = await axe.run(getRoot());
       expect(axeResults).toBeAccessible();
       done();
+    });
+
+    describe('with a complex clickable field table element inside', () => {
+      beforeEach(() => {
+        addFieldEqualFilter('@filetype', 'youtubevideo');
+
+        const cardOverlayElement = getOverlayElement();
+
+        const fieldTableElement = $$(
+          'table',
+          {
+            className: Component.computeCssClassName(FieldTable),
+            'data-allow-minimization': 'false'
+          },
+          $$('tr', { 'data-field': '@author', 'data-caption': 'Author' })
+        );
+
+        const facetElement = $$('div', {
+          className: Component.computeCssClassName(Facet),
+          'data-field': '@author'
+        });
+
+        cardOverlayElement.append(fieldTableElement.el);
+        getFacetColumn().appendChild(facetElement.el);
+        testResultElement(cardOverlayElement.el);
+      });
+
+      it('should be accessible', async done => {
+        await afterDeferredQuerySuccess();
+        getOverlayInstance().openOverlay();
+        await afterDelay(1000);
+        const axeResults = await axe.run(getRoot());
+        expect(axeResults).toBeAccessible();
+        done();
+      });
+
+      it('should be accessible when the clickable element is focused', async done => {
+        await afterDeferredQuerySuccess();
+        getOverlayInstance().openOverlay();
+        await afterDelay(1000);
+        $$(getRoot())
+          .find('span.coveo-accessible-button')
+          .focus();
+        const axeResults = await axe.run(getRoot());
+        expect(axeResults).toBeAccessible();
+        done();
+      });
     });
   });
 };
