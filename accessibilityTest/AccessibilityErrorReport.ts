b796287ba7bf816b8a68dce6016b5904ffd7b0fb
Olivier Lamothe <olivier_lamothe@hotmail.fr>
2018-08-15 10:24:12 -0400

    PR comments
    https://coveord.atlassian.net/browse/JSUI-2199

--- accessibilityTest/AccessibilityErrorReport.ts
+++ accessibilityTest/AccessibilityErrorReport.ts
@@ -12,33 +12,23 @@ export const AccessibilityErrorReport = () => {
       getResultsColumn().appendChild(errorReportElement.el);
     };
 
-    describe('with a query that triggers an error', () => {
-      beforeEach(() => {
-        // !BOOM
-        addQueryFilter('$qre()');
-      });
-
-      it('should be accessible', async done => {
-        buildErrortReportElement();
-        await afterQueryError();
-        const axeResults = await axe.run(getRoot());
-        expect(axeResults).toBeAccessible();
-        done();
-      });
+    it('should be accessible when a query triggers an error', async done => {
+      // !BOOM
+      addQueryFilter('$qre()');
+      buildErrortReportElement();
+      await afterQueryError();
+      const axeResults = await axe.run(getRoot());
+      expect(axeResults).toBeAccessible();
+      done();
     });
 
-    describe('with a query that does not trigger an error', () => {
-      beforeEach(() => {
-        addQueryFilter('@uri');
-      });
-
-      it('should be accessible', async done => {
-        buildErrortReportElement();
-        await afterQuerySuccess();
-        const axeResults = await axe.run(getRoot());
-        expect(axeResults).toBeAccessible();
-        done();
-      });
+    it('should be accessible when a query does not trigger an error', async done => {
+      addQueryFilter('@uri');
+      buildErrortReportElement();
+      await afterQuerySuccess();
+      const axeResults = await axe.run(getRoot());
+      expect(axeResults).toBeAccessible();
+      done();
     });
   });
 };
