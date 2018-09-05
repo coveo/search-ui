05924ea5624156e144f77cd467abe7d663fe5cde
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2018-09-05 14:55:06 -0400

    Add post-build test to check if docgen.json is generated correctly

--- gulpTasks/doc.js
+++ gulpTasks/doc.js
@@ -5,9 +5,17 @@ const fs = require('fs');
 const shell = require('gulp-shell');
 const notSupportedFeaturesConfig = require('./notSupportedFeaturesConfig');
 const del = require('del');
+const runsequence = require('run-sequence');
+const _ = require('underscore');
 const baseTsConfig = require('../tsconfig.json');
 
-gulp.task('doc', ['copyBinToDoc', 'buildPlayground'], function() {
+const docgenJsonPath = './bin/docgen/docgen.json';
+
+gulp.task('doc', done => {
+  runsequence('testDoc', done);
+});
+
+gulp.task('buildDoc', ['copyBinToDoc', 'buildPlayground'], () => {
   const typedocConfig = {
     ...baseTsConfig.compilerOptions,
     mode: 'file',
@@ -24,7 +32,7 @@ gulp.task('doc', ['copyBinToDoc', 'buildPlayground'], function() {
   const src = app.expandInputFiles(['src']);
   const project = app.convert(src);
   app.generateDocs(project, 'docgen');
-  app.generateJson(project, './bin/docgen/docgen.json', 'https://coveo.github.io/search-ui/');
+  app.generateJson(project, docgenJsonPath, 'https://coveo.github.io/search-ui/');
   return gulp.src('./readme.png').pipe(gulp.dest('./docgen'));
 });
 
@@ -32,32 +40,58 @@ gulp.task('cleanGeneratedThemesFiles', () => {
   return del(['./docs/theme/assets/gen/**/*']);
 });
 
-gulp.task('copyBinToDoc', ['cleanGeneratedThemesFiles'], function() {
+gulp.task('copyBinToDoc', ['cleanGeneratedThemesFiles'], () => {
   return gulp.src('./bin/{js,image,css}/**/*').pipe(gulp.dest('./docs/theme/assets/gen'));
 });
 
 gulp.task('buildPlayground', shell.task(['node node_modules/webpack/bin/webpack.js --config ./webpack.playground.config.js']));
 
-function copyFile(source, target, cb) {
-  var cbCalled = false;
+gulp.task('testDoc', () => {
+  const docgenJson = JSON.parse(fs.readFileSync(docgenJsonPath));
 
-  var rd = fs.createReadStream(source);
-  rd.on('error', function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(target);
-  wr.on('error', function(err) {
-    done(err);
-  });
-  wr.on('close', function(ex) {
-    done();
-  });
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
+  // Check if object is defined and non-empty
+  if (!docgenJson || !docgenJson.length) {
+    throw new Error('Invalid object');
   }
-}
+
+  // Check that all attributes are defined correctly
+  const attributes = [
+    { name: 'name', types: ['string'] },
+    { name: 'notSupportedIn', types: ['string', 'object'] },
+    { name: 'comment', types: ['string'] },
+    { name: 'type', types: ['string'] },
+    { name: 'constrainedValues', types: ['object'] },
+    { name: 'miscAttributes', types: ['object'] }
+  ];
+
+  _.each(docgenJson, element =>
+    _.each(attributes, ({ name, types }) => {
+      if (!_.has(element, name) || !_.contains(types, typeof element[name])) {
+        throw new Error(`Invalid or missing attribute "${name}" for doc element "${JSON.stringify(element)}"`);
+      }
+    })
+  );
+
+  // Check that some specific doc elements are present
+  const elements = [
+    {
+      name: 'SearchAlerts.usageAnalytics',
+      notSupportedIn: '',
+      comment:
+        '<p>A reference to the <a href="https://coveo.github.io/search-ui/components/analytics.html#client">Analytics.client</a>.</p>\n',
+      type: 'IAnalyticsClient'
+    },
+    {
+      name: 'Thumbnail.options',
+      notSupportedIn: '',
+      comment: '<p>Options for the Thumbnail</p>\n',
+      type: 'object'
+    }
+  ];
+
+  _.each(elements, element => {
+    if (!_.findWhere(docgenJson, element)) {
+      throw new Error(`Can't find doc element "${JSON.stringify(element)}"`);
+    }
+  });
+});
