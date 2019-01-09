92f60e123a60540e23f8ee7e1a13212ef2ba1ada
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2019-01-09 15:35:05 -0500

    JSUI-2316 Make the Category Facet Breadcrumb & Preference Breadcrumb Accessible (#998)
    * Make the Category facet & preference breadcrumb Accessible
    * Add unit tests for accessibility

--- src/ui/CategoryFacet/CategoryFacetBreadcrumb.ts
+++ src/ui/CategoryFacet/CategoryFacetBreadcrumb.ts
@@ -2,6 +2,8 @@ import { $$ } from '../../utils/Dom';
 import { SVGDom } from '../../utils/SVGDom';
 import { SVGIcons } from '../../utils/SVGIcons';
 import { CategoryValueDescriptor } from './CategoryFacet';
+import { AccessibleButton } from '../../utils/AccessibleButton';
+import { l } from '../../strings/Strings';
 import { without } from 'underscore';
 
 export class CategoryFacetBreadcrumb {
@@ -23,12 +25,18 @@ export class CategoryFacetBreadcrumb {
 
     SVGDom.addClassToSVGInContainer(clear.el, 'coveo-facet-breadcrumb-clear-svg');
     const pathToRender = without(this.categoryValueDescriptor.path, ...this.basePath);
+    const captionLabel = pathToRender.join('/');
 
-    const title = `${this.categoryValueDescriptor.value} ${this.categoryValueDescriptor.count}`;
     const breadcrumbTitle = $$('span', { className: 'coveo-category-facet-breadcrumb-title' }, `${this.categoryFacetTitle}: `);
-    const values = $$('span', { className: 'coveo-category-facet-breadcrumb-values' }, pathToRender.join('/'), clear);
-    const breadcrumb = $$('span', { className: 'coveo-category-facet-breadcrumb', title }, breadcrumbTitle, values);
-    breadcrumb.on('click', this.onClickHandler);
+    const valuesContainer = $$('span', { className: 'coveo-category-facet-breadcrumb-values' }, captionLabel, clear);
+
+    new AccessibleButton()
+      .withElement(valuesContainer)
+      .withLabel(l('RemoveFilterOn', captionLabel))
+      .withSelectAction(this.onClickHandler)
+      .build();
+
+    const breadcrumb = $$('span', { className: 'coveo-category-facet-breadcrumb' }, breadcrumbTitle, valuesContainer);
     return breadcrumb.el;
   }
 }
