bbb847398845b77aeec41dac2b53bd05a69555c5
Sami Sayegh <samisayegh1@gmail.com>
2019-02-22 10:46:27 -0500

    moving breadcrumb creation into a function
    https://coveord.atlassian.net/browse/JSUI-2385

--- unitTests/ui/CategoryFacet/CategoryFacetBreadcrumbTest.ts
+++ unitTests/ui/CategoryFacet/CategoryFacetBreadcrumbTest.ts
@@ -1,10 +1,15 @@
 import { CategoryFacetBreadcrumb } from '../../../src/ui/CategoryFacet/CategoryFacetBreadcrumb';
 import { $$ } from '../../../src/Core';
 import { CategoryValueDescriptor, ICategoryFacetOptions } from '../../../src/ui/CategoryFacet/CategoryFacet';
+
 export function CategoryFacetBreadcrumbTest() {
   describe('CategoryFacetBreadcrumb', () => {
     let categoryValueDescriptor: CategoryValueDescriptor;
     let categoryFacetOptions: ICategoryFacetOptions;
+
+    function buildCategoryFacetBreadcrumb() {
+      return new CategoryFacetBreadcrumb(categoryFacetOptions, () => {}, categoryValueDescriptor).build();
+    }
 
     beforeEach(() => {
       categoryValueDescriptor = { value: 'value', count: 1, path: [] };
@@ -16,13 +21,13 @@ export function CategoryFacetBreadcrumbTest() {
     });
 
     it('build a breadcrumb', () => {
-      const breadcrumb = new CategoryFacetBreadcrumb(categoryFacetOptions, () => {}, categoryValueDescriptor).build();
+      const breadcrumb = buildCategoryFacetBreadcrumb();
       expect($$(breadcrumb).hasClass('coveo-category-facet-breadcrumb')).toBe(true);
     });
 
     it('has the right accessibility attributes', () => {
       categoryValueDescriptor.path = ['path_one', 'path_two'];
-      const breadcrumb = new CategoryFacetBreadcrumb(categoryFacetOptions, () => {}, categoryValueDescriptor).build();
+      const breadcrumb = buildCategoryFacetBreadcrumb();
       const breadcrumbValues = $$(breadcrumb).find('.coveo-category-facet-breadcrumb-values');
       expect(breadcrumbValues.getAttribute('aria-label')).toBe('Remove filter on path_one/path_two');
       expect(breadcrumbValues.getAttribute('role')).toBe('button');
@@ -42,7 +47,7 @@ export function CategoryFacetBreadcrumbTest() {
 
     it('build a breadcrumb with the full path if there is no base path', () => {
       categoryValueDescriptor.path = ['path_one', 'path_two'];
-      const breadcrumb = new CategoryFacetBreadcrumb(categoryFacetOptions, () => {}, categoryValueDescriptor).build();
+      const breadcrumb = buildCategoryFacetBreadcrumb();
       const values = $$(breadcrumb).find('.coveo-category-facet-breadcrumb-values');
       expect($$(values).text()).toEqual('path_one/path_two');
     });
@@ -51,7 +56,7 @@ export function CategoryFacetBreadcrumbTest() {
       categoryValueDescriptor.path = ['path_one', 'path_two'];
       categoryFacetOptions.basePath = ['path_one'];
 
-      const breadcrumb = new CategoryFacetBreadcrumb(categoryFacetOptions, () => {}, categoryValueDescriptor).build();
+      const breadcrumb = buildCategoryFacetBreadcrumb();
       const values = $$(breadcrumb).find('.coveo-category-facet-breadcrumb-values');
       expect($$(values).text()).toEqual('path_two');
     });
