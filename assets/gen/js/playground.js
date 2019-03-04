f2f25cceba3661ea2b035df28119452e11932951
Travis CI User <travis@example.org>
2019-03-04 20:35:33 +0000

    publish: Automatically created by Travis CI
    generated from commit 5a7e37a0e263b0d564f22f42d83ae45c0c2f3ae9

--- assets/gen/js/playground.js
+++ assets/gen/js/playground.js
@@ -2964,13 +2964,6 @@ var Dom = /** @class */ (function () {
         return Dom.ONLY_WHITE_SPACE_REGEX.test(this.el.innerHTML);
     };
     /**
-     * Check if the element is not a locked node (`{ toString(): string }`) and thus have base element properties.
-     * @returns {boolean}
-     */
-    Dom.prototype.isValid = function () {
-        return this.el != null && this.el.getAttribute != undefined;
-    };
-    /**
      * Check if the element is a descendant of parent
      * @param other
      */
@@ -4006,7 +3999,7 @@ var SearchEndpoint = /** @class */ (function () {
     SearchEndpoint.prototype.buildViewAsHtmlQueryString = function (uniqueId, callOptions) {
         callOptions = _.extend({}, callOptions);
         return {
-            uniqueId: Utils_1.Utils.safeEncodeURIComponent(uniqueId),
+            uniqueId: uniqueId,
             enableNavigation: 'true',
             requestedOutputSize: callOptions.requestedOutputSize ? callOptions.requestedOutputSize.toString() : null,
             contentType: callOptions.contentType
@@ -5881,8 +5874,8 @@ exports.ResponsiveComponents = ResponsiveComponents;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = {
-    lib: '2.5652.2',
-    product: '2.5652.2',
+    lib: '2.5549.4',
+    product: '2.5549.4',
     supportedApiVersion: 2
 };
 
@@ -8473,8 +8466,6 @@ var dict = {
     "Collapse": "Collapse",
     "Collapsable": "Collapsible",
     "Expand": "Expand",
-    "CollapseFacet": "Collapse {0} facet",
-    "ExpandFacet": "Expand {0} facet",
     "Today": "Today",
     "Yesterday": "Yesterday",
     "Tomorrow": "Tomorrow",
