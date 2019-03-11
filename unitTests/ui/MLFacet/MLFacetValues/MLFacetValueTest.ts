f8f1cd8591ee58e723b39e7858c55b8d5d74e418
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-03-11 16:56:20 -0400

    Finish MLFacetValueTest
    https://coveord.atlassian.net/browse/JSUI-2373

--- unitTests/ui/MLFacet/MLFacetValues/MLFacetValueTest.ts
+++ unitTests/ui/MLFacet/MLFacetValues/MLFacetValueTest.ts
@@ -1,4 +1,6 @@
+import * as Globalize from 'globalize';
 import { MLFacetValue } from '../../../../src/ui/MLFacet/MLFacetValues/MLFacetValue';
+import { IMLFacetOptions } from '../../../../src/ui/MLFacet/MLFacetOptions';
 import { MLFacetTestUtils } from '../MLFacetTestUtils';
 import { MLFacet } from '../../../../src/ui/MLFacet/MLFacet';
 
@@ -8,9 +10,13 @@ export function MLFacetValueTest() {
     let facet: MLFacet;
 
     beforeEach(() => {
-      facet = MLFacetTestUtils.createFakeFacet();
-      mLFacetValue = new MLFacetValue(MLFacetTestUtils.createFakeFacetValues(1)[0], facet);
+      initializeComponent();
     });
+
+    function initializeComponent(options?: IMLFacetOptions) {
+      facet = MLFacetTestUtils.createFakeFacet(options);
+      mLFacetValue = new MLFacetValue(MLFacetTestUtils.createFakeFacetValues(1)[0], facet);
+    }
 
     it('should toggle selection correctly', () => {
       mLFacetValue.toggleSelect();
@@ -60,9 +66,37 @@ export function MLFacetValueTest() {
       expect(mLFacetValue.equals(value)).toBe(true);
     });
 
-    // TODO: tests
-    // formattedCount
-    // valueCaption
-    // render (only check that is doesn't throw)
+    it(`when getting formattedCount with a non zero value
+      it should return a string in the Globalize format`, () => {
+      expect(mLFacetValue.formattedCount).toBe(Globalize.format(mLFacetValue.numberOfResults, 'n0'));
+    });
+
+    it(`when getting formattedCount with a zero value
+      it should return an empty string`, () => {
+      mLFacetValue.numberOfResults = 0;
+      expect(mLFacetValue.formattedCount).toBe('');
+    });
+
+    it(`when using the valueCaption option with a function
+      should bypass it and return the original value`, () => {
+      initializeComponent({ valueCaption: () => 'allo' });
+      expect(mLFacetValue.valueCaption).toBe(mLFacetValue.value);
+    });
+
+    it(`when using the valueCaption with an object that contains the original value
+      should return the caption`, () => {
+      initializeComponent({ valueCaption: { [mLFacetValue.value]: 'allo' } });
+      expect(mLFacetValue.valueCaption).toBe('allo');
+    });
+
+    it(`when using the valueCaption with an object that does not contain the original value
+      should return original value`, () => {
+      initializeComponent({ valueCaption: { randomValue: 'allo' } });
+      expect(mLFacetValue.valueCaption).toBe(mLFacetValue.value);
+    });
+
+    it(`should render without error`, () => {
+      expect(() => mLFacetValue.render()).not.toThrow();
+    });
   });
 }
