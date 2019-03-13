de95c406c76f16ba33d60d904f3893fd6839c839
Sami Sayegh <samisayegh1@gmail.com>
2019-03-13 10:38:28 -0400

    switching default to use sequential navigation
    https://coveord.atlassian.net/browse/JSUI-2367

--- src/ui/FacetSearchDropdownNavigation/SearchDropdownNavigatorFactory.ts
+++ src/ui/FacetSearchDropdownNavigation/SearchDropdownNavigatorFactory.ts
@@ -6,11 +6,11 @@ import { Facet } from '../Facet/Facet';
 
 export function SearchDropdownNavigatorFactory(facetSearch: IFacetSearch, config: ISearchDropdownConfig): ISearchDropdownNavigator {
   switch (facetSearch.facetType) {
-    case CategoryFacet.ID:
-      return new DefaultSearchDropdownNavigator(config);
-
     case Facet.ID:
-    default:
       return new FacetSearchDropdownNavigator({ ...config, facetSearch });
+
+    case CategoryFacet.ID:
+    default:
+      return new DefaultSearchDropdownNavigator(config);
   }
 }
