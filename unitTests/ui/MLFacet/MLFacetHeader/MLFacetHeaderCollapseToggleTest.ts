da4c1c1646e0a305300dc9d8b47e4a12710f1b96
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-04-02 17:17:26 -0400

    Add more precise unit tests
    https://coveord.atlassian.net/browse/JSUI-2415

--- unitTests/ui/MLFacet/MLFacetHeader/MLFacetHeaderCollapseToggleTest.ts
+++ unitTests/ui/MLFacet/MLFacetHeader/MLFacetHeaderCollapseToggleTest.ts
@@ -10,7 +10,10 @@ export function MLFacetHeaderCollapseToggleTest() {
     let facet: MLFacet;
 
     beforeEach(() => {
-      facet = MLFacetTestUtils.createFakeFacet();
+      facet = MLFacetTestUtils.createFakeFacet({
+        enableCollapse: true,
+        collapsedByDefault: false
+      });
       initializeComponent();
     });
 
@@ -19,28 +22,29 @@ export function MLFacetHeaderCollapseToggleTest() {
       collapseToggleElement = collapseToggle.element;
     }
 
-    it(`should display the accessible collapse & expand buttons`, () => {
+    it(`should display the collapse & expand buttons`, () => {
       const collapseElement = $$(collapseToggleElement).find('.coveo-ml-facet-header-collapse');
       const expandElement = $$(collapseToggleElement).find('.coveo-ml-facet-header-expand');
 
-      expect($$(collapseElement).getAttribute('aria-label')).toBeTruthy();
-      expect($$(collapseElement).getAttribute('title')).toBeTruthy();
       expect($$(collapseElement).isVisible()).toBe(true);
       expect($$(expandElement).isVisible()).toBe(false);
     });
 
+    it(`should not add the "coveo-ml-facet-collapsed" class to the facet`, () => {
+      expect($$(facet.element).hasClass('coveo-ml-facet-collapsed')).toBe(false);
+    });
+
     it(`when passing the option enableCollapse (true) & collapsedByDefault (true)
-      should display the accessible expand button`, () => {
+      should display the expand button and add the "coveo-ml-facet-collapsed" class to the facet`, () => {
       facet.options.collapsedByDefault = true;
       initializeComponent();
 
       const collapseElement = $$(collapseToggleElement).find('.coveo-ml-facet-header-collapse');
       const expandElement = $$(collapseToggleElement).find('.coveo-ml-facet-header-expand');
 
-      expect($$(expandElement).getAttribute('aria-label')).toBeTruthy();
-      expect($$(expandElement).getAttribute('title')).toBeTruthy();
       expect($$(collapseElement).isVisible()).toBe(false);
       expect($$(expandElement).isVisible()).toBe(true);
+      expect($$(facet.element).hasClass('coveo-ml-facet-collapsed')).toBe(true);
     });
 
     it(`when clicking on the collapse button
@@ -51,6 +55,7 @@ export function MLFacetHeaderCollapseToggleTest() {
 
       expect($$(collapseElement).isVisible()).toBe(false);
       expect($$(expandElement).isVisible()).toBe(true);
+      expect($$(facet.element).hasClass('coveo-ml-facet-collapsed')).toBe(true);
     });
 
     it(`when clicking on the expand button
@@ -64,6 +69,7 @@ export function MLFacetHeaderCollapseToggleTest() {
 
       expect($$(collapseElement).isVisible()).toBe(true);
       expect($$(expandElement).isVisible()).toBe(false);
+      expect($$(facet.element).hasClass('coveo-ml-facet-collapsed')).toBe(false);
     });
   });
 }
