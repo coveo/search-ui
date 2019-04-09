def9d85a08ffc0c8b46b4479e2b8d123eaf66852
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-04-09 09:34:44 -0400

    Add options including a callback to update the facet
    https://coveord.atlassian.net/browse/JSUI-2439

--- src/ui/MLFacetManager/MLFacetManager.ts
+++ src/ui/MLFacetManager/MLFacetManager.ts
@@ -7,16 +7,84 @@ import { exportGlobally } from '../../GlobalExports';
 import { shuffle, find } from 'underscore';
 import { IFacetResponse } from '../../rest/Facet/FacetResponse';
 import { $$ } from '../../utils/Dom';
+import { Utils } from '../../utils/Utils';
+import { ComponentOptions } from '../Base/ComponentOptions';
+
+export interface IMLFacetManagerOptions {
+  enableReorder?: boolean;
+  onUpdate?: IMLFacetManagerOnUpdate;
+}
+
+export interface IMLFacetManagerOnUpdate {
+  (facet: MLFacet, index: number): void;
+}
 
 export class MLFacetManager extends Component {
   static ID = 'MLFacetManager';
   static doExport = () => exportGlobally({ MLFacetManager });
 
+  /**
+   * The options for the MLFacet
+   * @componentOptions
+   */
+  static options: IMLFacetManagerOptions = {
+    /**
+     * Whether to allow the reordering of the facets depending on the server's response.
+     *
+     * **Default:** `true`
+     */
+    enableReorder: ComponentOptions.buildBooleanOption({ defaultValue: true, section: 'Filtering' }),
+    /**
+     * Specifies the event handler function to execute when the facets are updated on the query response
+     * For each `MLFacet`, you can provide a callback function to execute after the facet is reordered.
+     *
+     * **Note:**
+     * > You cannot set this option directly in the component markup as an HTML attribute. You must either set it in the
+     * > [`init`]{@link init} call of your search interface (see
+     * > [Components - Passing Component Options in the init Call](https://developers.coveo.com/x/PoGfAQ#Components-PassingComponentOptionsintheinitCall)),
+     * > or before the `init` call, using the `options` top-level function (see
+     * > [Components - Passing Component Options Before the init Call](https://developers.coveo.com/x/PoGfAQ#Components-PassingComponentOptionsBeforetheinitCall)).
+     *
+     * **Example:**
+     *
+     * ```javascript
+     *
+     * var myOnUpdateFunction = function(mLFacetComponent, index) {
+     *     // Collapse every facet except the first one
+     *     if (index > 0) {
+     *       mLFacetComponent.collapse();
+     *     } else {
+     *       mLFacetComponent.expand();
+     *     }
+     *
+     * };
+     *
+     * // You can set the option in the 'init' call:
+     * Coveo.init(document.querySelector("#search"), {
+     *    MLFacetManager : {
+     *      onUpdate : myOnUpdateFunction
+     *    }
+     * });
+     *
+     * // Or before the 'init' call, using the 'options' top-level function:
+     * // Coveo.options(document.querySelector("#search"), {
+     * //   MLFacetManager : {
+     * //     onUpdate : myOnUpdateFunction
+     * //   }
+     * // });
+     * ```
+     */
+    onUpdate: ComponentOptions.buildCustomOption<IMLFacetManagerOnUpdate>(() => {
+      return null;
+    })
+  };
+
   // Children MLFacet components of the MLFacetManager
   private mLFacets: MLFacet[];
 
-  constructor(element: HTMLElement, public options?: any, private bindings?: IComponentBindings) {
+  constructor(element: HTMLElement, public options?: IMLFacetManagerOptions, private bindings?: IComponentBindings) {
     super(element, 'MLFacetManager');
+    this.options = ComponentOptions.initComponentOptions(element, MLFacetManager, options);
 
     this.initEvents();
   }
@@ -40,7 +108,14 @@ export class MLFacetManager extends Component {
       return;
     }
 
-    this.reorderFacetsInDom(shuffle(data.results.facets));
+    if (Utils.isNullOrUndefined(data.results.facets)) {
+      return this.notImplementedError();
+    }
+
+    if (this.options.enableReorder) {
+      // TODO: remove shuffle for the results
+      this.reorderMLFacetsInDom(shuffle(data.results.facets));
+    }
   }
 
   private getMLFacetComponentById(id: string) {
@@ -64,12 +139,9 @@ export class MLFacetManager extends Component {
     $$(component.element).insertAfter(targetComponent.element);
   }
 
-  private reorderFacetsInDom(facetsResponse: IFacetResponse[]) {
-    // console.log('facets', this.mLFacets.map(facet => facet.options.id));
-    // console.log('response', facetsResponse.map(facet => facet.facetId));
-
+  private reorderMLFacetsInDom(facetsResponse: IFacetResponse[]) {
     let previousMLFacetComponent: MLFacet;
-    facetsResponse.forEach(facetResponse => {
+    facetsResponse.forEach((facetResponse, index) => {
       const id = facetResponse.facetId;
       const mLFacetComponent = this.getMLFacetComponentById(id);
       if (!mLFacetComponent) {
@@ -80,7 +152,16 @@ export class MLFacetManager extends Component {
         this.insertMLFacetComponentAfter(mLFacetComponent, previousMLFacetComponent);
       }
 
+      if (this.options.onUpdate) {
+        this.options.onUpdate(mLFacetComponent, index);
+      }
+
       previousMLFacetComponent = mLFacetComponent;
     });
+  }
+
+  private notImplementedError() {
+    this.logger.error('MLFacetManager is not supported by your current search endpoint. Disabling this component.');
+    this.disable();
   }
 }
