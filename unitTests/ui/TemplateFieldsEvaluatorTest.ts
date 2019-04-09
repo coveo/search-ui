61395af6b367b3e9d15d4ba6d748ea78c4e1eda5
Sami Sayegh <samisayegh1@gmail.com>
2019-04-09 18:27:21 -0400

    supporting multi-value fields as template field conditions
    https://coveord.atlassian.net/browse/JSUI-2437

--- unitTests/ui/TemplateFieldsEvaluatorTest.ts
+++ unitTests/ui/TemplateFieldsEvaluatorTest.ts
@@ -54,5 +54,60 @@ export function TemplateFieldsEvaluatorTest() {
       ];
       expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(false);
     });
+
+    describe(`when the result raw has a field that has an array of values (e.g. a multi-value field)`, () => {
+      const multiValueField = 'language';
+      const multiValueFieldValues = ['francais'];
+
+      beforeEach(() => {
+        result.raw[multiValueField] = multiValueFieldValues;
+      });
+
+      it(`when the fieldsToMatch #field is the multi-value field, it returns true`, () => {
+        fieldsToMatch = [{ field: multiValueField }];
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(true);
+      });
+
+      it(`when the fieldsToMatch #field is the multi-value field
+      and the #values is an empty array, it returns false`, () => {
+        fieldsToMatch = [{ field: multiValueField, values: [] }];
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(false);
+      });
+
+      it(`when the fieldsToMatch #field is the multi-value field
+      and the #values is an array containing the multi-value field value,
+      it returns true`, () => {
+        fieldsToMatch = [{ field: multiValueField, values: [...multiValueFieldValues] }];
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(true);
+      });
+
+      it(`when the fieldsToMatch #field is the multi-value field
+      and the #values is a populated array not containing the multi-value field value,
+      it returns false`, () => {
+        fieldsToMatch = [{ field: multiValueField, values: ['english'] }];
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(false);
+      });
+
+      it(`when the fieldsToMatch #field is the multi-value field
+      and the #values is an array containing the multi-value field value as uppercase,
+      it returns true`, () => {
+        const uppercaseValues = multiValueFieldValues.map(val => val.toUpperCase());
+        fieldsToMatch = [{ field: multiValueField, values: uppercaseValues }];
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(true);
+      });
+
+      it(`given the result raw multi-value field has uppercase values,
+      when the fieldsToMatch #field is the multi-value field
+      and the #values is an array containing the multi-value field value as lowercase,
+      it returns true`, () => {
+        const uppercaseValues = multiValueFieldValues.map(val => val.toUpperCase());
+        const lowercaseValues = multiValueFieldValues.map(val => val.toLowerCase());
+
+        result.raw[multiValueField] = uppercaseValues;
+        fieldsToMatch = [{ field: multiValueField, values: lowercaseValues }];
+
+        expect(TemplateFieldsEvaluator.evaluateFieldsToMatch(fieldsToMatch, result)).toBe(true);
+      });
+    });
   });
 }
