4ab73624a22d06137b0398e22c7bc4ead23068d6
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-06-27 17:43:28 -0400

    add DynamicFacetSearchInputTest UTs
    https://coveord.atlassian.net/browse/JSUI-2528

--- unitTests/ui/DynamicFacetSearch/DynamicFacetSearchInputTest.ts
+++ unitTests/ui/DynamicFacetSearch/DynamicFacetSearchInputTest.ts
@@ -1,1 +1,38 @@
-export function DynamicFacetSearchInputTest() {}
+import { DynamicFacetSearchInput } from '../../../src/ui/DynamicFacetSearch/DynamicFacetSearchInput';
+import { DynamicFacet } from '../../../src/ui/DynamicFacet/DynamicFacet';
+import { DynamicFacetTestUtils } from '../DynamicFacet/DynamicFacetTestUtils';
+import { $$, KEYBOARD } from '../../../src/Core';
+import { Simulate } from '../../Simulate';
+
+export function DynamicFacetSearchInputTest() {
+  describe('DynamicFacetSearchInputTest', () => {
+    let facet: DynamicFacet;
+    let facetSearchInput: DynamicFacetSearchInput;
+    let onChangeSpy = jasmine.createSpy('onChange');
+
+    beforeEach(() => {
+      facet = DynamicFacetTestUtils.createFakeFacet();
+      facetSearchInput = new DynamicFacetSearchInput(facet, onChangeSpy);
+    });
+
+    function getInput() {
+      return $$(facetSearchInput.element).find('input');
+    }
+
+    function simulateInputChange(value: string) {
+      getInput().setAttribute('value', value);
+      Simulate.keyUp(getInput(), KEYBOARD.CTRL);
+    }
+
+    it('should render the input', () => {
+      expect(getInput()).toBeTruthy();
+    });
+
+    it(`when triggering a change on the input
+    should call the "onChange" passed in the constructor`, () => {
+      const value = 'test';
+      simulateInputChange(value);
+      expect(onChangeSpy).toHaveBeenCalledWith(value);
+    });
+  });
+}
