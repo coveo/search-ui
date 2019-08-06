182925d8075a4e781ec8c6bbb92a2098be9527a5
Olivier Robert <orobert@coveo.com>
2019-08-06 09:31:20 -0400

    refactor omniboxAnalytics
    https://coveord.atlassian.net/browse/JSUI-2585

--- src/ui/Omnibox/OmniboxAnalytics.ts
+++ src/ui/Omnibox/OmniboxAnalytics.ts
@@ -1,16 +1,13 @@
-import { last, chain } from 'underscore';
+import { last, compact, filter, map, reduceRight } from 'underscore';
 import { IAnalyticsOmniboxSuggestionMeta } from '../Analytics/AnalyticsActionListMeta';
 
 export class OmniboxAnalytics {
-  public partialQueries: string[];
+  public partialQueries: string[] = [];
   public suggestionRanking: number;
-  public suggestions: string[];
+  public suggestions: string[] = [];
   public partialQuery: string;
 
-  constructor() {
-    this.partialQueries = [];
-    this.suggestions = [];
-  }
+  constructor() {}
 
   public buildCustomDataForPartialQueries(index?: number, suggestions?: string[]): IAnalyticsOmniboxSuggestionMeta {
     this.partialQuery = last(this.partialQueries);
@@ -27,35 +24,54 @@ export class OmniboxAnalytics {
 
   private cleanCustomData(toClean: string[], rejectLength = 256) {
     // Filter out only consecutive values that are the identical
-    const reducedToRejectLengthOrLess = [];
-    chain(toClean)
-      .compact()
+    const filterOutConsecutiveValues = this.filterOutConsecutiveValues(toClean);
 
-      // Custom dimensions cannot be an array in analytics service: Send a string joined by ; instead.
-      // Need to replace ;
-      .map(partial => {
-        return partial.replace(/;/g, '');
+    // Custom dimensions cannot be an array in analytics service: Send a string joined by ; instead.
+    // Need to replace ;
+    const redimensionedArray = this.removeCustomDimension(filterOutConsecutiveValues);
+
+    // Reduce right to get the last X words that adds to less then rejectLength
+    const reducedToRejectLengthOrLess = this.toAnalyticsLenghtLimit(redimensionedArray, rejectLength);
+
+    const cleanStrings = reducedToRejectLengthOrLess.reverse();
+    const ret = cleanStrings.join(';');
+
+    // analytics service can store max 256 char in a custom event
+    // if we're over that, call cleanup again with an arbitrary 10 less char accepted
+    if (ret.length >= 256) {
+      return this.cleanCustomData(cleanStrings, rejectLength - 10);
+    }
+
+    return cleanStrings.join(';');
+  }
+
+  private filterOutConsecutiveValues(toClean: string[]) {
+    return compact(
+      filter(toClean, (partial: string, pos?: number, array?: string[]) => {
+        return pos === 0 || partial !== array[pos - 1];
       })
+    );
+  }
 
-      // Reduce right to get the last X words that adds to less then rejectLength
+  private removeCustomDimension(toClean: string[]) {
+    return map(toClean, partial => {
+      return partial.replace(/;/g, '');
+    });
+  }
 
-      .reduceRight((memo: number, partial: string) => {
+  private toAnalyticsLenghtLimit(toClean: string[], rejectLength: number) {
+    const reducedToRejectLengthOrLess: string[] = [];
+    reduceRight(
+      toClean,
+      (memo: number, partial: string) => {
         const totalSoFar = memo + partial.length;
         if (totalSoFar <= rejectLength) {
           reducedToRejectLengthOrLess.push(partial);
         }
         return totalSoFar;
-      }, 0)
-      .value();
-    const toCleanLocal = reducedToRejectLengthOrLess.reverse() as string[];
-    const ret = toCleanLocal.join(';');
-
-    // analytics service can store max 256 char in a custom event
-    // if we're over that, call cleanup again with an arbitrary 10 less char accepted
-    if (ret.length >= 256) {
-      return this.cleanCustomData(toCleanLocal, rejectLength - 10);
-    }
-
-    return toCleanLocal.join(';');
+      },
+      0
+    );
+    return reducedToRejectLengthOrLess;
   }
 }
