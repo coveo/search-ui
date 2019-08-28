ae8ed19152d9befbde49777d30a117db4439efee
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-08-28 17:25:43 -0400

    Add search value captions for months
    https://coveord.atlassian.net/browse/JSUI-2619

--- src/controllers/FacetSearchController.ts
+++ src/controllers/FacetSearchController.ts
@@ -3,23 +3,40 @@ import { DynamicFacet } from '../ui/DynamicFacet/DynamicFacet';
 import { IFacetSearchResponse } from '../rest/Facet/FacetSearchResponse';
 import { FileTypes } from '../ui/Misc/FileTypes';
 import { QueryUtils } from '../utils/QueryUtils';
+import { IStringMap } from '../rest/GenericParam';
+import { DateUtils } from '../utils/DateUtils';
 
 export class FacetSearchController {
   constructor(private facet: DynamicFacet) {}
 
-  private addTypesCaptionsIfNecessary() {
+  private getMonthsValueCaptions() {
+    const monthsValueCaptions: IStringMap<string> = {};
+    for (let month = 1; month <= 12; month++) {
+      const key = `0${month}`.substr(-2);
+      monthsValueCaptions[key] = DateUtils.monthToString(month - 1);
+    }
+
+    return monthsValueCaptions;
+  }
+
+  private addTypesCaptionsIfNecessary(): IStringMap<string> {
     const field = this.facet.options.field.toLowerCase();
     const isFileType = QueryUtils.isStratusAgnosticField(field, '@filetype');
     const isObjectType = QueryUtils.isStratusAgnosticField(field, '@objecttype');
+    const isMonth = QueryUtils.isStratusAgnosticField(field, '@month');
 
     if (isFileType || isObjectType) {
       return FileTypes.getFileTypeCaptions();
     }
 
+    if (isMonth) {
+      return this.getMonthsValueCaptions();
+    }
+
     return {};
   }
 
-  private get captions() {
+  private get captions(): IStringMap<string> {
     return {
       ...this.addTypesCaptionsIfNecessary(),
       ...this.facet.options.valueCaption
