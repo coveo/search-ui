f02f11b1e98eac55bc4f5e35036db15c6838e377
Travis CI User <travis@example.org>
2019-09-06 21:16:17 +0000

    publish: Automatically created by Travis CI
    generated from commit b9010c58980aecc6998a63f40a9478e8de9421e1

--- assets/gen/js/playground.js
+++ assets/gen/js/playground.js
@@ -5916,8 +5916,8 @@ exports.ResponsiveComponents = ResponsiveComponents;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = {
-    lib: '2.7023.4',
-    product: '2.7023.4',
+    lib: '2.7023.5-beta',
+    product: '2.7023.5-beta',
     supportedApiVersion: 2
 };
 
