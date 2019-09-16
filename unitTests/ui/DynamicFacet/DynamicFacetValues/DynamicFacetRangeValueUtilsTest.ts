63406bdd6c979fe2a4f65079b855f64a6399fec7
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-09-16 16:06:05 -0400

    Test date format
    https://coveord.atlassian.net/browse/JSUI-2633

--- unitTests/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueUtilsTest.ts
+++ unitTests/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueUtilsTest.ts
@@ -5,19 +5,19 @@ export function DynamicFacetRangeValueUtilsTest() {
   describe('DynamicFacetRangeValueUtils', () => {
     describe('testing with the number format', () => {
       describe('testing formatDisplayValue', () => {
-        it(`when the value a integer
+        it(`when the value is a integer
           should return the format correctly`, () => {
           const value = DynamicFacetRangeValueUtils.formatDisplayValue(10, DynamicFacetRangeValueFormat.number);
           expect(value).toBe('10');
         });
 
-        it(`when the value a big integer
+        it(`when the value is a big integer
           should return the format correctly`, () => {
           const value = DynamicFacetRangeValueUtils.formatDisplayValue(1000, DynamicFacetRangeValueFormat.number);
           expect(value).toBe('1,000');
         });
 
-        it(`when the value a float
+        it(`when the value is a float
           should return the format correctly`, () => {
           const value = DynamicFacetRangeValueUtils.formatDisplayValue(1.001, DynamicFacetRangeValueFormat.number);
           expect(value).toBe('1.001');
@@ -92,6 +92,72 @@ export function DynamicFacetRangeValueUtilsTest() {
           const range = DynamicFacetRangeValueUtils.createRangeFromValue('1..100ish', DynamicFacetRangeValueFormat.number);
           expect(range).toBeNull();
         });
+      });
+    });
+
+    describe('testing with the date format', () => {
+      describe('testing formatDisplayValue', () => {
+        it(`when the value is a Date object
+          should return the format correctly`, () => {
+          const value = DynamicFacetRangeValueUtils.formatDisplayValue(new Date('April 17 1999'), DynamicFacetRangeValueFormat.date);
+          expect(value).toBe('4/17/1999');
+        });
+
+        it(`when the value is a integer
+          should return the format correctly`, () => {
+          const value = DynamicFacetRangeValueUtils.formatDisplayValue(1466662921705, DynamicFacetRangeValueFormat.date);
+          expect(value).toBe('6/23/2016');
+        });
+
+        it(`when the value is a string
+          should return the format correctly`, () => {
+          const value = DynamicFacetRangeValueUtils.formatDisplayValue('April 17 1999', DynamicFacetRangeValueFormat.date);
+          expect(value).toBe('4/17/1999');
+        });
+      });
+
+      describe(`testing parseRange`, () => {
+        it(`when sending a valid range
+        should return it correctly parsed`, () => {
+          const validatedRange = DynamicFacetRangeValueUtils.parseRange(
+            {
+              start: '4/17/1999',
+              end: '6/23/2016',
+              endInclusive: true
+            },
+            DynamicFacetRangeValueFormat.date
+          );
+
+          expect(validatedRange).toEqual({
+            start: '1999/04/17@00:00:00',
+            end: '2016/06/23@00:00:00',
+            endInclusive: true
+          });
+        });
+
+        it(`when sending a invalid range
+        should return null`, () => {
+          const validatedRange = DynamicFacetRangeValueUtils.parseRange(
+            {
+              start: 'yes',
+              end: 'no',
+              endInclusive: true
+            },
+            DynamicFacetRangeValueFormat.date
+          );
+
+          expect(validatedRange).toBeNull();
+        });
+      });
+
+      it('should return createValueFromRange correctly', () => {
+        const value = DynamicFacetRangeValueUtils.createValueFromRange({
+          start: '1999/04/17@00:00:00',
+          end: '2016/06/23@00:00:00',
+          endInclusive: true
+        });
+
+        expect(value).toBe('1999/04/17@00:00:00..2016/06/23@00:00:00inc');
       });
     });
   });
