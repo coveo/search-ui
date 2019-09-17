4d37456097ab8a54d222a833f933fd8a373c67d4
Olivier Lamothe <olamothe@coveo.com>
2019-09-17 16:55:42 -0400

    Change facet settings to append to facet parent element (#1226)
    https://coveord.atlassian.net/browse/JSUI-2641

--- src/ui/Facet/FacetSettings.ts
+++ src/ui/Facet/FacetSettings.ts
@@ -1,10 +1,12 @@
+import Popper from 'popper.js';
 import 'styling/_FacetSettings';
-import { each, find, map, contains, compact, filter, findWhere, escape } from 'underscore';
+import { compact, contains, each, escape, filter, find, findWhere, map } from 'underscore';
 import { InitializationEvents } from '../../events/InitializationEvents';
 import { QueryStateModel } from '../../models/QueryStateModel';
 import { l } from '../../strings/Strings';
 import { AccessibleButton } from '../../utils/AccessibleButton';
 import { $$ } from '../../utils/Dom';
+import { KEYBOARD, KeyboardUtils } from '../../utils/KeyboardUtils';
 import { LocalStorageUtils } from '../../utils/LocalStorageUtils';
 import { SVGDom } from '../../utils/SVGDom';
 import { SVGIcons } from '../../utils/SVGIcons';
@@ -12,8 +14,6 @@ import { Utils } from '../../utils/Utils';
 import { analyticsActionCauseList, IAnalyticsFacetMeta } from '../Analytics/AnalyticsActionListMeta';
 import { Facet } from './Facet';
 import { FacetSort, IFacetSortDescription } from './FacetSort';
-import Popper from 'popper.js';
-import { KEYBOARD, KeyboardUtils } from '../../utils/KeyboardUtils';
 
 export interface IFacetSettingsKlass {
   new (sorts: string[], facet: Facet): FacetSettings;
@@ -153,14 +153,8 @@ export class FacetSettings extends FacetSort {
    * Open the settings menu
    */
   public open() {
-    $$(this.settingsPopup).insertAfter(this.settingsButton);
-    new Popper(this.settingsButton, this.settingsPopup, {
-      modifiers: {
-        preventOverflow: {
-          boundariesElement: this.facet.element.parentElement
-        }
-      }
-    });
+    $$(this.settingsPopup).insertAfter(this.facet.element.parentElement);
+    new Popper(this.settingsButton, this.settingsPopup);
 
     if (this.hideSection && this.showSection) {
       $$(this.hideSection).toggle(!$$(this.facet.element).hasClass('coveo-facet-collapsed'));
