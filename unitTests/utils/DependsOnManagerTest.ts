6aa2999e26ff0cc169df63d19f0ef25e9ced82c0
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-09-19 10:47:42 -0400

    Rename UTs
    https://coveord.atlassian.net/browse/JSUI-2643

--- unitTests/utils/DependsOnManagerTest.ts
+++ unitTests/utils/DependsOnManagerTest.ts
@@ -9,7 +9,7 @@ import { buildCategoryFacetResults } from '../ui/CategoryFacet/CategoryFacetTest
 
 export function DependsOnManagerTest() {
   describe('DependsOnManager', () => {
-    describe('CategoryFacet', () => {
+    describe('using CategoryFacet', () => {
       const masterCategoryFacetField = '@masterFacet';
       const dependentCategoryFacetField = '@dependentFacet';
       let simulateQueryData: ISimulateQueryData;
@@ -112,51 +112,120 @@ export function DependsOnManagerTest() {
       });
     });
 
-    describe('Facet', () => {
+    describe('using Facet', () => {
       let test: IBasicComponentSetup<Facet>;
-      describe('when a facet has the dependsOn option set to the field of another facet', () => {
-        let masterFacet: { env: Mock.IMockEnvironment; cmp: Facet };
-        const masterFacetField = '@masterFacet';
-        const dependentFacetField = '@field';
+      let masterFacet: { env: Mock.IMockEnvironment; cmp: Facet };
+      const masterFacetField = '@masterFacet';
+      const dependentFacetField = '@field';
 
-        function getMasterAndDependentFacetResults() {
-          const results = FakeResults.createFakeResults();
-          results.groupByResults = [
-            FakeResults.createFakeGroupByResult(dependentFacetField, 'foo', 15),
-            FakeResults.createFakeGroupByResult(masterFacetField, 'foo', 15)
-          ];
+      function getMasterAndDependentFacetResults() {
+        const results = FakeResults.createFakeResults();
+        results.groupByResults = [
+          FakeResults.createFakeGroupByResult(dependentFacetField, 'foo', 15),
+          FakeResults.createFakeGroupByResult(masterFacetField, 'foo', 15)
+        ];
 
-          return results;
+        return results;
+      }
+
+      function getMasterDependentFacetStateAttributes() {
+        return {
+          ...getMasterFacetStateAttributes(),
+          ...getDependentFacetStateAttributes()
+        };
+      }
+
+      function getMasterFacetStateAttributes() {
+        return {
+          [`f:${masterFacetField}`]: masterFacet.cmp.getSelectedValues(),
+          [`f:${masterFacetField}:not`]: masterFacet.cmp.getExcludedValues()
+        };
+      }
+
+      function getDependentFacetStateAttributes() {
+        return {
+          [`f:${dependentFacetField}`]: test.cmp.getSelectedValues(),
+          [`f:${dependentFacetField}:not`]: test.cmp.getExcludedValues()
+        };
+      }
+
+      beforeEach(() => {
+        test = Mock.optionsComponentSetup<Facet, IFacetOptions>(Facet, {
+          field: dependentFacetField,
+          dependsOn: masterFacetField
+        });
+
+        masterFacet = Mock.advancedComponentSetup<Facet>(
+          Facet,
+          new Mock.AdvancedComponentSetupOptions(
+            undefined,
+            {
+              field: masterFacetField
+            },
+            (builder: Mock.MockEnvironmentBuilder) => {
+              return builder.withRoot(test.env.root);
+            }
+          )
+        );
+
+        test.cmp.queryStateModel.get = key => {
+          const attributes = getMasterDependentFacetStateAttributes();
+          return attributes[key];
+        };
+
+        Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
+      });
+
+      it('the dependent facet is hidden at startup', () => {
+        expect($$(test.cmp.element).isVisible()).toBeFalsy();
+      });
+
+      it('the master facet is visible at startup', () => {
+        expect($$(masterFacet.cmp.element).isVisible()).toBeTruthy();
+      });
+
+      it(`when the master facet has one selected value,
+      the dependent facet is visible`, () => {
+        masterFacet.cmp.selectValue('master value');
+        Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
+
+        expect($$(test.cmp.element).isVisible()).toBeTruthy();
+      });
+
+      it(`when the master facet has one excluded value,
+      the dependent facet is still hidden`, () => {
+        masterFacet.cmp.excludeValue('excluded master value');
+        Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
+
+        expect($$(test.cmp.element).isVisible()).toBeFalsy();
+      });
+
+      describe(`given the master facet has two selected values and the dependent facet has one selected and one excluded value`, () => {
+        const selectedMasterValue1 = 'selected master value 1';
+        const selectedMasterValue2 = 'selected master value 2';
+
+        function triggerStateChangeOnDependentFacet() {
+          rebindDependentFacetStateChangeListeners();
+          $$(test.env.root).trigger('change', { attributes: {} });
         }
 
-        function getMasterDependentFacetStateAttributes() {
-          return {
-            ...getMasterFacetStateAttributes(),
-            ...getDependentFacetStateAttributes()
-          };
-        }
-
-        function getMasterFacetStateAttributes() {
-          return {
-            [`f:${masterFacetField}`]: masterFacet.cmp.getSelectedValues(),
-            [`f:${masterFacetField}:not`]: masterFacet.cmp.getExcludedValues()
-          };
-        }
-
-        function getDependentFacetStateAttributes() {
-          return {
-            [`f:${dependentFacetField}`]: test.cmp.getSelectedValues(),
-            [`f:${dependentFacetField}:not`]: test.cmp.getExcludedValues()
-          };
+        function rebindDependentFacetStateChangeListeners() {
+          test.cmp.queryStateModel.getEventName = name => name;
+          test.cmp['initQueryStateEvents']();
         }
 
         beforeEach(() => {
-          test = Mock.optionsComponentSetup<Facet, IFacetOptions>(Facet, {
-            field: dependentFacetField,
-            dependsOn: masterFacetField
-          });
+          masterFacet.cmp.selectValue(selectedMasterValue1);
+          masterFacet.cmp.selectValue(selectedMasterValue2);
 
-          masterFacet = Mock.advancedComponentSetup<Facet>(
+          test.cmp.selectValue('selected dependent value');
+          test.cmp.excludeValue('excluded dependent value');
+          Simulate.query(test.env);
+        });
+
+        it(`when the master facet id is not unique,
+          it doesn't update keepDisplayedValuesNextTime`, () => {
+          Mock.advancedComponentSetup<Facet>(
             Facet,
             new Mock.AdvancedComponentSetupOptions(
               undefined,
@@ -168,117 +237,46 @@ export function DependsOnManagerTest() {
               }
             )
           );
-
-          test.cmp.queryStateModel.get = key => {
-            const attributes = getMasterDependentFacetStateAttributes();
-            return attributes[key];
-          };
-
-          Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
+          masterFacet.cmp.keepDisplayedValuesNextTime = undefined;
+          expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
+          triggerStateChangeOnDependentFacet();
+          expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
         });
 
-        it('the dependent facet is hidden at startup', () => {
-          expect($$(test.cmp.element).isVisible()).toBeFalsy();
-        });
-
-        it('the master facet is visible at startup', () => {
-          expect($$(masterFacet.cmp.element).isVisible()).toBeTruthy();
-        });
-
-        it(`when the master facet has one selected value,
-      the dependent facet is visible`, () => {
-          masterFacet.cmp.selectValue('master value');
-          Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
-
-          expect($$(test.cmp.element).isVisible()).toBeTruthy();
-        });
-
-        it(`when the master facet has one excluded value,
-      the dependent facet is still hidden`, () => {
-          masterFacet.cmp.excludeValue('excluded master value');
-          Simulate.query(test.env, { results: getMasterAndDependentFacetResults() });
-
-          expect($$(test.cmp.element).isVisible()).toBeFalsy();
-        });
-
-        describe(`given the master facet has two selected values and the dependent facet has one selected and one excluded value`, () => {
-          const selectedMasterValue1 = 'selected master value 1';
-          const selectedMasterValue2 = 'selected master value 2';
-
-          function triggerStateChangeOnDependentFacet() {
-            rebindDependentFacetStateChangeListeners();
-            $$(test.env.root).trigger('change', { attributes: {} });
-          }
-
-          function rebindDependentFacetStateChangeListeners() {
-            test.cmp.queryStateModel.getEventName = name => name;
-            test.cmp['initQueryStateEvents']();
-          }
-
-          beforeEach(() => {
-            masterFacet.cmp.selectValue(selectedMasterValue1);
-            masterFacet.cmp.selectValue(selectedMasterValue2);
-
-            test.cmp.selectValue('selected dependent value');
-            test.cmp.excludeValue('excluded dependent value');
-            Simulate.query(test.env);
-          });
-
-          it(`when the master facet id is not unique,
-          it doesn't update keepDisplayedValuesNextTime`, () => {
-            Mock.advancedComponentSetup<Facet>(
-              Facet,
-              new Mock.AdvancedComponentSetupOptions(
-                undefined,
-                {
-                  field: masterFacetField
-                },
-                (builder: Mock.MockEnvironmentBuilder) => {
-                  return builder.withRoot(test.env.root);
-                }
-              )
-            );
-            masterFacet.cmp.keepDisplayedValuesNextTime = undefined;
-            expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
-            triggerStateChangeOnDependentFacet();
-            expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
-          });
-
-          it(`when the master facet id is unique,
+        it(`when the master facet id is unique,
           it update keepDisplayedValuesNextTime`, () => {
-            masterFacet.cmp.keepDisplayedValuesNextTime = undefined;
-            expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
-            triggerStateChangeOnDependentFacet();
-            expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBe(false);
-          });
+          masterFacet.cmp.keepDisplayedValuesNextTime = undefined;
+          expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBeUndefined();
+          triggerStateChangeOnDependentFacet();
+          expect(masterFacet.cmp.keepDisplayedValuesNextTime).toBe(false);
+        });
 
-          it(`when resetting the master facet,
+        it(`when resetting the master facet,
         it resets the dependent facet`, () => {
-            masterFacet.cmp.reset();
-            triggerStateChangeOnDependentFacet();
+          masterFacet.cmp.reset();
+          triggerStateChangeOnDependentFacet();
 
-            expect(test.cmp.getSelectedValues().length).toBe(0);
-            expect(test.cmp.getExcludedValues().length).toBe(0);
-          });
+          expect(test.cmp.getSelectedValues().length).toBe(0);
+          expect(test.cmp.getExcludedValues().length).toBe(0);
+        });
 
-          it(`when deselecting one of the master facet selected options,
+        it(`when deselecting one of the master facet selected options,
         it does not reset the dependent facet`, () => {
-            masterFacet.cmp.deselectValue(selectedMasterValue1);
-            triggerStateChangeOnDependentFacet();
+          masterFacet.cmp.deselectValue(selectedMasterValue1);
+          triggerStateChangeOnDependentFacet();
 
-            expect(test.cmp.getSelectedValues().length).toBe(1);
-            expect(test.cmp.getExcludedValues().length).toBe(1);
-          });
+          expect(test.cmp.getSelectedValues().length).toBe(1);
+          expect(test.cmp.getExcludedValues().length).toBe(1);
+        });
 
-          it(`when deselecting both master facet selected options,
+        it(`when deselecting both master facet selected options,
         it resets the dependent facet`, () => {
-            masterFacet.cmp.deselectValue(selectedMasterValue1);
-            masterFacet.cmp.deselectValue(selectedMasterValue2);
-            triggerStateChangeOnDependentFacet();
+          masterFacet.cmp.deselectValue(selectedMasterValue1);
+          masterFacet.cmp.deselectValue(selectedMasterValue2);
+          triggerStateChangeOnDependentFacet();
 
-            expect(test.cmp.getSelectedValues().length).toBe(0);
-            expect(test.cmp.getExcludedValues().length).toBe(0);
-          });
+          expect(test.cmp.getSelectedValues().length).toBe(0);
+          expect(test.cmp.getExcludedValues().length).toBe(0);
         });
       });
     });
