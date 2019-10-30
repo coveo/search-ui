84be0fa364cc77f6c3cdcf9b2e78e79e3f82f631
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-10-30 14:13:32 -0400

    Fix spread error
    https://coveord.atlassian.net/browse/JSUI-2674

--- src/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueCreator.ts
+++ src/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueCreator.ts
@@ -44,7 +44,11 @@ export class DynamicFacetRangeValueCreator implements ValueCreator {
 
     return new DynamicFacetValue(
       {
-        ...responseValue,
+        state: responseValue.state,
+        numberOfResults: responseValue.numberOfResults,
+        start: responseValue.start,
+        end: responseValue.end,
+        endInclusive: responseValue.endInclusive,
         value,
         displayValue,
         position: index + 1
