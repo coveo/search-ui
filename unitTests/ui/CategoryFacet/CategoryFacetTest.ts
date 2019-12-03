dfd4e7be407fbf8b6e75fc00a0a5e000feb940a3
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2019-12-03 10:44:28 -0500

    Test that clear was called once
    https://coveord.atlassian.net/browse/JSUI-2699

--- unitTests/ui/CategoryFacet/CategoryFacetTest.ts
+++ unitTests/ui/CategoryFacet/CategoryFacetTest.ts
@@ -14,7 +14,7 @@ export function CategoryFacetTest() {
   describe('CategoryFacet', () => {
     let test: Mock.IBasicComponentSetup<CategoryFacet>;
     let options: ICategoryFacetOptions;
-    let mockFacetValues: IFacetResponseValue[]
+    let mockFacetValues: IFacetResponseValue[];
 
     beforeEach(() => {
       options = { field: '@dummy' };
@@ -25,9 +25,11 @@ export function CategoryFacetTest() {
       test = CategoryFacetTestUtils.createAdvancedFakeFacet(options);
       mockFacetValues = CategoryFacetTestUtils.createFakeFacetResponseValues(3, 5);
 
-      test.cmp.values.createFromResponse(CategoryFacetTestUtils.getCompleteFacetResponse(test.cmp, {
-        values: mockFacetValues
-      }));
+      test.cmp.values.createFromResponse(
+        CategoryFacetTestUtils.getCompleteFacetResponse(test.cmp, {
+          values: mockFacetValues
+        })
+      );
 
       spyOn(test.cmp, 'selectPath').and.callThrough();
       spyOn(test.cmp, 'scrollToTop').and.callThrough();
@@ -61,9 +63,11 @@ export function CategoryFacetTest() {
 
     function fakeResultsWithFacets() {
       const fakeResultsWithFacets = FakeResults.createFakeResults();
-      fakeResultsWithFacets.facets = [CategoryFacetTestUtils.getCompleteFacetResponse(test.cmp, {
-        values: mockFacetValues
-      })];
+      fakeResultsWithFacets.facets = [
+        CategoryFacetTestUtils.getCompleteFacetResponse(test.cmp, {
+          values: mockFacetValues
+        })
+      ];
       return fakeResultsWithFacets;
     }
 
@@ -133,7 +137,7 @@ export function CategoryFacetTest() {
     should update the QueryStateModel correctly`, () => {
       mockFacetValues[0].children = [];
       mockFacetValues[0].state = FacetValueState.selected;
-      const results = fakeResultsWithFacets()
+      const results = fakeResultsWithFacets();
       Simulate.query(test.env, { results });
 
       testQueryStateModelValues([results.facets[0].values[0].value]);
@@ -245,7 +249,7 @@ export function CategoryFacetTest() {
 
     it('allows to trigger a new isolated query', () => {
       spyOn(test.cmp.categoryFacetQueryController, 'getQueryResults');
-      const beforeExecuteQuery = jasmine.createSpy('beforeExecuteQuery', () => { });
+      const beforeExecuteQuery = jasmine.createSpy('beforeExecuteQuery', () => {});
       test.cmp.ensureDom();
       test.cmp.triggerNewIsolatedQuery(beforeExecuteQuery);
 
@@ -397,7 +401,7 @@ export function CategoryFacetTest() {
         should call clear`, () => {
         test.cmp.values.selectPath(['hello']);
         test.cmp.deselectCurrentValue();
-        expect(test.cmp.clear).toHaveBeenCalled();
+        expect(test.cmp.clear).toHaveBeenCalledTimes(1);
       });
 
       it(`when a path with multiple values is selected
@@ -409,7 +413,7 @@ export function CategoryFacetTest() {
     });
 
     it('getCaption should return the caption for a value', () => {
-      options.valueCaption = { 'test': 'this is a test' };
+      options.valueCaption = { test: 'this is a test' };
       initializeComponent();
       expect(test.cmp.getCaption('test')).toBe('this is a test');
     });
