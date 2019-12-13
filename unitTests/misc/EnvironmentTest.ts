9076efb4819e1433f897f18416497d687b6ea4d9
Sami Sayegh <samisayegh1@gmail.com>
2019-12-13 14:37:30 -0500

    add clarifying comment
    https://coveord.atlassian.net/browse/JSUI-2780

--- unitTests/misc/EnvironmentTest.ts
+++ unitTests/misc/EnvironmentTest.ts
@@ -5,6 +5,8 @@ declare const _;
 export function EnvironmentTest() {
   describe('Environment', () => {
     it(`the '_' global variable should hold the underscore library`, () => {
+      // If this test fails, it might be because the global variable is being
+      // overwritten by lodash. Check that there are no lodash imports.
       const globalUnderscore = _;
       expect(globalUnderscore).toEqual(underscore);
     });
