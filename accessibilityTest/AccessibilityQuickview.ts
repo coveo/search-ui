e6dfc2cdbdcc36cde577c91f63d2536335e0a4c6
Benjamin <btaillon@coveo.com>
2020-01-02 14:01:24 -0500

    Added accessibility tests
    https://coveord.atlassian.net/browse/JSUI-2798

--- accessibilityTest/AccessibilityQuickview.ts
+++ accessibilityTest/AccessibilityQuickview.ts
@@ -1,32 +1,100 @@
 import * as axe from 'axe-core';
-import { $$, Quickview, Component, get } from 'coveo-search-ui';
-import { afterQuerySuccess, getRoot, testResultElement, afterDelay } from './Testing';
+import { $$, Quickview, Component, get, Dom } from 'coveo-search-ui';
+import { afterQuerySuccess, getRoot, testResultElement } from './Testing';
+
+function nodeListToArray<T extends Node>(nodeList: NodeListOf<T>) {
+  const result: T[] = [];
+  for (let i = 0; i < nodeList.length; i++) {
+    result.push(nodeList.item(i));
+  }
+  return result;
+}
+
+function getFocusableElements() {
+  return nodeListToArray<HTMLElement>(
+    document.querySelectorAll(`
+      a[href]:not([tabindex='-1']),
+      area[href]:not([tabindex='-1']),
+      input:not([disabled]):not([tabindex='-1']),
+      select:not([disabled]):not([tabindex='-1']),
+      textarea:not([disabled]):not([tabindex='-1']),
+      button:not([disabled]):not([tabindex='-1']),
+      iframe:not([tabindex='-1']),
+      [tabindex]:not([tabindex='-1']),
+      [contentEditable=true]:not([tabindex='-1'])
+    `)
+  ).sort((a, b) => a.tabIndex - b.tabIndex);
+}
+
+function getNextFocusableElement() {
+  const elements = getFocusableElements();
+  const currentIndex = document.activeElement ? elements.indexOf(document.activeElement as HTMLElement) : -1;
+  return elements[(currentIndex + 1) % elements.length];
+}
+
+function simulateTabKey() {
+  const event = new KeyboardEvent('keydown');
+  Object.defineProperty(event, 'keyCode', { get: () => 9 });
+  document.body.dispatchEvent(event);
+  if (!event.defaultPrevented) {
+    getNextFocusableElement().focus();
+  }
+}
 
 export const AccessibilityQuickview = () => {
   describe('Quickview', () => {
-    it('should be accessible', async done => {
-      const quickviewElement = $$('div', {
+    async function openQuickview() {
+      await (get(document.querySelector(`.${Component.computeCssClassName(Quickview)}`)) as Quickview).open();
+    }
+
+    let quickviewElement: Dom;
+    beforeEach(async done => {
+      quickviewElement = $$('div', {
         className: Component.computeCssClassName(Quickview)
       });
 
       testResultElement(quickviewElement.el);
       await afterQuerySuccess();
+      done();
+    });
+
+    it('should be accessible', async done => {
       const axeResults = await axe.run(getRoot());
       expect(axeResults).toBeAccessible();
       done();
     });
 
     it('should be accessible when opened', async done => {
-      const quickviewElement = $$('div', {
-        className: Component.computeCssClassName(Quickview)
-      });
-
-      testResultElement(quickviewElement.el);
-      await afterQuerySuccess();
-      (get(document.querySelector(`.${Component.computeCssClassName(Quickview)}`)) as Quickview).open();
-      await afterDelay(500);
+      await openQuickview();
       const axeResults = await axe.run(getRoot());
       expect(axeResults).toBeAccessible();
+      done();
+    });
+
+    it('should have a close button when opened', async done => {
+      await openQuickview();
+      expect(document.querySelector('.coveo-small-close')).not.toBeNull();
+      done();
+    });
+
+    it('should select the close button when opened', async done => {
+      await openQuickview();
+      expect(document.activeElement).toBe(document.querySelector('.coveo-small-close'));
+      done();
+    });
+
+    it('should wrap the focus when pressing tab', async done => {
+      await openQuickview();
+      const container: HTMLElement = document.querySelector('.coveo-modal-container');
+      const initialFocus = document.activeElement;
+      const maxAttempts = 200;
+      for (let attempt = 0; attempt < maxAttempts; attempt++) {
+        simulateTabKey();
+        if (document.activeElement === initialFocus || !container.contains(document.activeElement)) {
+          break;
+        }
+      }
+      expect(document.activeElement.outerHTML).toBe(initialFocus.outerHTML);
       done();
     });
   });
