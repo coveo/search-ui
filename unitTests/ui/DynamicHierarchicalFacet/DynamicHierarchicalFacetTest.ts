1f1c831651ea0bcaab10cd8b78894201f768712c
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2020-01-06 12:59:50 -0500

    Fix toggleDependentFacet for DynamicFacet and add UTs (#1347)
    https://coveord.atlassian.net/browse/JSUI-2797

--- unitTests/ui/DynamicHierarchicalFacet/DynamicHierarchicalFacetTest.ts
+++ unitTests/ui/DynamicHierarchicalFacet/DynamicHierarchicalFacetTest.ts
@@ -13,6 +13,7 @@ import { ResultListUtils } from '../../../src/utils/ResultListUtils';
 import { IDynamicHierarchicalFacetOptions } from '../../../src/ui/DynamicHierarchicalFacet/IDynamicHierarchicalFacet';
 import { DynamicFacetManager } from '../../../src/ui/DynamicFacetManager/DynamicFacetManager';
 import { analyticsActionCauseList } from '../../../src/ui/Analytics/AnalyticsActionListMeta';
+import { ComponentsTypes } from '../../../src/utils/ComponentsTypes';
 
 export function DynamicHierarchicalFacetTest() {
   describe('DynamicHierarchicalFacet', () => {
@@ -387,7 +388,14 @@ export function DynamicHierarchicalFacetTest() {
     });
 
     describe('testing the DependsOnManager', () => {
+      let dependentFacet: DynamicHierarchicalFacet;
+
       beforeEach(() => {
+        dependentFacet = DynamicHierarchicalFacetTestUtils.createAdvancedFakeFacet({
+          field: '@dependentField',
+          dependsOn: test.cmp.options.id
+        }).cmp;
+        spyOn(ComponentsTypes, 'getAllFacetsInstance').and.returnValue([test.cmp, dependentFacet]);
         spyOn(test.cmp.dependsOnManager, 'updateVisibilityBasedOnDependsOn');
       });
 
@@ -399,6 +407,21 @@ export function DynamicHierarchicalFacetTest() {
       should call the "updateVisibilityBasedOnDependsOn" method of the DependsOnManager`, () => {
         Simulate.query(test.env, { results: fakeResultsWithFacets() });
         expect(test.cmp.dependsOnManager.updateVisibilityBasedOnDependsOn).toHaveBeenCalled();
+      });
+
+      it(`when facet has no selected values
+        when triggering a newQuery
+        dependent facet should be disabled`, () => {
+        $$(test.env.root).trigger(QueryEvents.newQuery);
+        expect(dependentFacet.disabled).toBe(true);
+      });
+
+      it(`when facet has selected values
+        when triggering a newQuery
+        dependent facet should be enabled`, () => {
+        test.cmp.selectPath(['value']);
+        $$(test.env.root).trigger(QueryEvents.newQuery);
+        expect(dependentFacet.disabled).toBe(false);
       });
     });
 
