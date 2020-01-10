d82ae28248fb3ed25bc555e6382437b3e7f1fa55
Olivier Lamothe <olamothe@coveo.com>
2020-01-10 09:37:50 -0500

    Capture more than one number for major version
    https://coveord.atlassian.net/browse/JSUI-2817

--- read.version.sh
+++ read.version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PACKAGE_JSON_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | sed -E 's/\.[0-9]+$//g' | xargs`
-export PACKAGE_PATCH_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | sed -E 's/[0-9]\.[0-9]+\.//g' | xargs`
+export PACKAGE_PATCH_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | sed -E 's/[0-9]+\.[0-9]+\.//g' | xargs`
