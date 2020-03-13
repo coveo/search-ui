c9d51716d1e338f43d8455d35d8069964d5aee31
Benjamin <btaillon@coveo.com>
2020-03-13 13:40:59 -0400

    Added missing accessibility tests for `FieldValue`
    https://coveord.atlassian.net/browse/JSUI-2912

--- accessibilityTest/AccessibilityFieldValue.ts
+++ accessibilityTest/AccessibilityFieldValue.ts
@@ -1,6 +1,7 @@
 import * as axe from 'axe-core';
 import { $$, Component, FieldValue, Facet } from 'coveo-search-ui';
 import { afterQuerySuccess, getRoot, testResultElement, getResultsColumn } from './Testing';
+import { ContrastChecker } from './ContrastChecker';
 
 export const AccessibilityFieldValue = () => {
   describe('FieldValue', () => {
@@ -17,22 +18,41 @@ export const AccessibilityFieldValue = () => {
       done();
     });
 
-    it('should be accessible with an associated facet', async done => {
-      const fieldValueElement = $$('div', {
-        className: Component.computeCssClassName(FieldValue),
-        'data-field': '@filetype'
+    describe('with an associated facet', () => {
+      function getFirstFieldValue() {
+        return getResultsColumn().querySelector<HTMLElement>(Component.computeSelectorForType(FieldValue.ID));
+      }
+
+      beforeEach(() => {
+        const fieldValueElement = $$('div', {
+          className: Component.computeCssClassName(FieldValue),
+          'data-field': '@filetype',
+          'data-text-caption': 'File'
+        });
+
+        const facetElement = $$('div', {
+          className: Component.computeCssClassName(Facet),
+          'data-field': '@filetype'
+        });
+        getResultsColumn().appendChild(facetElement.el);
+        testResultElement(fieldValueElement.el);
       });
 
-      const facetElement = $$('div', {
-        className: Component.computeCssClassName(Facet),
-        'data-field': '@filetype'
+      it('should be accessible with an associated facet', async done => {
+        await afterQuerySuccess();
+        const axeResults = await axe.run(getRoot());
+        expect(axeResults).toBeAccessible();
+        done();
       });
-      getResultsColumn().appendChild(facetElement.el);
-      testResultElement(fieldValueElement.el);
-      await afterQuerySuccess();
-      const axeResults = await axe.run(getRoot());
-      expect(axeResults).toBeAccessible();
-      done();
+
+      it('should have enough contrast between the label and link', async done => {
+        await afterQuerySuccess();
+        const fieldValueElement = getFirstFieldValue();
+        const labelColor = ContrastChecker.getColor(fieldValueElement.querySelector('.coveo-field-caption'));
+        const linkColor = ContrastChecker.getColor(fieldValueElement.querySelector('.coveo-clickable'));
+        expect(ContrastChecker.getContrastBetweenColors(labelColor, linkColor)).not.toBeLessThan(ContrastChecker.MinimumContrastRatio);
+        done();
+      });
     });
   });
 };
