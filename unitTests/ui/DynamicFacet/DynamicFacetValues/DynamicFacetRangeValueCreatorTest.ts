44a0d3c67677d1533ff2e130becb39885a03ba59
Sami Sayegh <samisayegh1@gmail.com>
2020-03-31 15:02:00 -0400

    adjust dynamicfacetRangeValueCreator tests
    https://coveord.atlassian.net/browse/JSUI-2933

--- unitTests/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueCreatorTest.ts
+++ unitTests/ui/DynamicFacet/DynamicFacetValues/DynamicFacetRangeValueCreatorTest.ts
@@ -66,22 +66,36 @@ export function DynamicFacetRangeValueCreatorTest() {
       });
     });
 
-    describe('testing createFromRanges', () => {
+    it('when the facet does not have ranges, getDefaultValues returns an empty array', () => {
+      expect(valueCreator.getDefaultValues().length).toEqual(0);
+    });
+
+    it('when the facet has multiple ranges configured, getDefaultValues returns an array with as many elements as configured ranges', () => {
+      facet.options.ranges = DynamicFacetRangeTestUtils.createFakeRanges(2);
+      expect(valueCreator.getDefaultValues().length).toEqual(facet.options.ranges.length);
+    });
+
+    describe('the facet has one range value, when calling getDefaultValues', () => {
       let index: number;
       let rangeValue: IRangeValue;
-      let facetValue: DynamicFacetValue;
+
+      function firstDefaultValue() {
+        return valueCreator.getDefaultValues()[0];
+      }
+
+      function setRangeOnFacet() {
+        facet.options.ranges = [rangeValue];
+      }
 
       beforeEach(() => {
         index = 0;
         rangeValue = DynamicFacetRangeTestUtils.createFakeRanges(1)[0];
-        initializeValue();
+        setRangeOnFacet();
       });
 
-      function initializeValue() {
-        facetValue = valueCreator.createFromRange(rangeValue, index);
-      }
+      it('the default value should have the right basic properties', () => {
+        const facetValue = firstDefaultValue();
 
-      it('should have the right basic properties', () => {
         expect(facetValue.start).toBe(rangeValue.start);
         expect(facetValue.endInclusive).toBe(rangeValue.endInclusive);
         expect(facetValue.end).toBe(rangeValue.end);
@@ -91,17 +105,18 @@ export function DynamicFacetRangeValueCreatorTest() {
         expect(facetValue.value).toBeTruthy();
       });
 
-      it(`when there is no label 
+      it(`when the range does not have a label,
         should format the displayValue correctly`, () => {
+        const facetValue = firstDefaultValue();
         expect(facetValue.displayValue).toBe(`${facetValue.start} ${facet.options.valueSeparator} ${facetValue.end}`);
       });
 
-      it(`when there is a label 
+      it(`when the range has a label, 
         should assign it to the displayValue`, () => {
-        const label = 'this is a label';
-        rangeValue.label = label;
-        initializeValue();
-        expect(facetValue.displayValue).toBe(label);
+        rangeValue.label = 'this is a label';
+        setRangeOnFacet();
+
+        expect(firstDefaultValue().displayValue).toBe(rangeValue.label);
       });
     });
 
