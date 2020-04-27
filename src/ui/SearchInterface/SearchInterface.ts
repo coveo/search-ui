695a3c0b5f1892c965ec996575fcb891d8a59080
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2020-04-27 12:08:05 -0400

    JSUI-2951 Retrieve All facets from the SearchInterface directly instead of looking inside the root element (#1486)
    * Retrieve All facets from the SearchInterface directly instead of looking inside the root element
    
    https://coveord.atlassian.net/browse/JSUI-2951

--- src/ui/SearchInterface/SearchInterface.ts
+++ src/ui/SearchInterface/SearchInterface.ts
@@ -538,7 +538,7 @@ export class SearchInterface extends RootComponent implements IComponentBindings
     this.componentOptionsModel = new ComponentOptionsModel(element);
     this.usageAnalytics = this.initializeAnalytics();
     this.queryController = new QueryController(element, this.options, this.usageAnalytics, this);
-    this.facetValueStateHandler = new FacetValueStateHandler(this.element);
+    this.facetValueStateHandler = new FacetValueStateHandler(this);
     new SentryLogger(this.queryController);
 
     const missingTermManagerArgs: IMissingTermManagerArgs = {
@@ -1056,7 +1056,7 @@ export class SearchInterface extends RootComponent implements IComponentBindings
 
   private get duplicatesFacets() {
     const duplicate = [];
-    const facets = ComponentsTypes.getAllFacetsInstance(this.root);
+    const facets = ComponentsTypes.getAllFacetsFromSearchInterface(this);
 
     facets.forEach(facet => {
       facets.forEach(cmp => {
