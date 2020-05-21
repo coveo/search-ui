6a0da7183f51ac1d47dcd10075b36397360289bc
Benjamin <btaillon@coveo.com>
2020-05-21 09:20:51 -0400

    Removed unneeded methods
    https://coveord.atlassian.net/browse/JSUI-2735

--- src/controllers/HierarchicalFacetSearchController.ts
+++ src/controllers/HierarchicalFacetSearchController.ts
@@ -10,40 +10,6 @@ type Path = string[];
 export class HierarchicalFacetSearchController {
   constructor(private facet: IDynamicHierarchicalFacet) {}
 
-  private getMonthsValueCaptions() {
-    const monthsValueCaptions: Record<string, string> = {};
-    for (let month = 1; month <= 12; month++) {
-      const key = `0${month}`.substr(-2);
-      monthsValueCaptions[key] = DateUtils.monthToString(month - 1);
-    }
-
-    return monthsValueCaptions;
-  }
-
-  private addTypesCaptionsIfNecessary(): Record<string, string> {
-    const field = this.facet.options.field.toLowerCase();
-    const isFileType = QueryUtils.isStratusAgnosticField(field, '@filetype');
-    const isObjectType = QueryUtils.isStratusAgnosticField(field, '@objecttype');
-    const isMonth = QueryUtils.isStratusAgnosticField(field, '@month');
-
-    if (isFileType || isObjectType) {
-      return FileTypes.getFileTypeCaptions();
-    }
-
-    if (isMonth) {
-      return this.getMonthsValueCaptions();
-    }
-
-    return {};
-  }
-
-  private get captions(): Record<string, string> {
-    return {
-      ...this.addTypesCaptionsIfNecessary(),
-      ...this.facet.options.valueCaption
-    };
-  }
-
   private get ignoredPaths() {
     return this.flattenPaths(this.facet.values.allFacetValues.map(value => this.getAllPaths(value)));
   }
@@ -63,7 +29,7 @@ export class HierarchicalFacetSearchController {
       numberOfValues: this.facet.options.numberOfValues,
       ignorePaths: this.ignoredPaths,
       basePath: this.facet.options.basePath,
-      captions: this.captions,
+      captions: this.facet.options.valueCaption,
       searchContext: this.facet.queryController.getLastQuery(),
       delimitingCharacter: this.facet.options.delimitingCharacter,
       query: terms.length ? `*${terms}*` : '*'
