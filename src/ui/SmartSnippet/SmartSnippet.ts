2b8f05a8c1309164f306595192f7da5d8140912f
Benjamin <btaillon@coveo.com>
2020-05-25 11:16:15 -0400

    Added style customization
    https://coveord.atlassian.net/browse/JSUI-2949

--- src/ui/SmartSnippet/SmartSnippet.ts
+++ src/ui/SmartSnippet/SmartSnippet.ts
@@ -18,9 +18,36 @@ export class SmartSnippet extends Component {
     });
   };
 
+  private snippetContainer: HTMLElement;
+
   constructor(public element: HTMLElement, public options?: ISmartSnippetOptions, bindings?: IComponentBindings) {
     super(element, SmartSnippet.ID, bindings);
+    this.buildShadow();
     this.bind.onRootElement(QueryEvents.querySuccess, (data: IQuerySuccessEventArgs) => this.handleQuerySuccess(data));
+  }
+
+  private get style() {
+    return $$(this.element)
+      .children()
+      .filter(element => element instanceof HTMLScriptElement && element.type.toLowerCase() === 'text/css')
+      .map(element => element.innerHTML)
+      .join('\n');
+  }
+
+  private buildShadow() {
+    const shadow = this.element.attachShadow({ mode: 'open' });
+    shadow.appendChild((this.snippetContainer = $$('main').el));
+    shadow.appendChild(this.buildStyle());
+  }
+
+  private buildStyle() {
+    const style = this.style;
+    if (!style) {
+      return;
+    }
+    const styleTag = document.createElement('style');
+    styleTag.innerHTML = style;
+    return styleTag;
   }
 
   private handleQuerySuccess(data: IQuerySuccessEventArgs) {
@@ -33,7 +60,7 @@ export class SmartSnippet extends Component {
   }
 
   private render(questionAnswer: IQueryAnswerResponse) {
-    this.element.innerHTML = questionAnswer.answerSnippet;
+    this.snippetContainer.innerHTML = questionAnswer.answerSnippet;
   }
 }
 Initialization.registerAutoCreateComponent(SmartSnippet);
