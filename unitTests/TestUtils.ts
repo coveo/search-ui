8d231ee643dcc2ac3e5be31fa8963a85acb9dc17
Benjamin <btaillon@coveo.com>
2020-07-09 14:12:22 -0400

    Added unit tests for `UserFeedbackBanner`
    https://coveord.atlassian.net/browse/JSUI-3015

--- unitTests/TestUtils.ts
+++ unitTests/TestUtils.ts
@@ -2,7 +2,9 @@ export function expectChildren(element: HTMLElement | DocumentFragment, classNam
   expect(element.childNodes.length).toEqual(classNames.length, `Expected only ${classNames.length} children.`);
   return classNames.map((className, index) => {
     const childAtIndex = element.childNodes.item(index) as HTMLElement;
-    expect(childAtIndex.classList).toContain(className, `Expected element at index ${index} to contain class ${className}.`);
+    if (className) {
+      expect(childAtIndex.classList).toContain(className, `Expected element at index ${index} to contain class ${className}.`);
+    }
     return childAtIndex;
   });
 }
