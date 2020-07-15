5b71a21b11462527586e2dd3869b2dd72ea523d1
Frederic Beaudoin <fbeaudoin@coveo.com>
2020-07-15 16:36:42 -0400

    Point to faster search page

--- docs/theme/assets/js/coveo-connect-header.js
+++ docs/theme/assets/js/coveo-connect-header.js
@@ -2,7 +2,7 @@ const headerNamespace = {
   initializeStandaloneSearchbox: () => {
     Coveo.SearchEndpoint.configureCloudV2Endpoint('coveosearch', 'xxf4d1df7a-7916-481e-9917-f562dddc322d');
     const root = Coveo.$$(document).find('#coveoConnectHeaderStandaloneSearchbox');
-    Coveo.initSearchbox(root, 'https://connect.coveo.com/s/global-search/%40uri');
+    Coveo.initSearchbox(root, 'https://docs.coveo.com/en/search');
   },
 
   initializeDropdownMenus: () => {
