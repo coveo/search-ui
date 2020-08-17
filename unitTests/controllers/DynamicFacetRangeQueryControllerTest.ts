e2e80762914b17af904bb671f7c4045fd694d9ba
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2020-08-17 16:23:53 -0400

    add tests
    https://coveord.atlassian.net/browse/JSUI-2655

--- unitTests/controllers/DynamicFacetRangeQueryControllerTest.ts
+++ unitTests/controllers/DynamicFacetRangeQueryControllerTest.ts
@@ -3,6 +3,7 @@ import { DynamicFacetRangeTestUtils } from '../ui/DynamicFacet/DynamicFacetRange
 import { DynamicFacetRange } from '../../src/ui/DynamicFacet/DynamicFacetRange';
 import { IDynamicFacetRangeOptions } from '../../src/ui/DynamicFacet/IDynamicFacetRange';
 import { QueryBuilder } from '../../src/Core';
+import { FacetValueState } from '../../src/rest/Facet/FacetValueState';
 
 export function DynamicFacetRangeQueryControllerTest() {
   describe('DynamicFacetRangeQueryController', () => {
@@ -12,8 +13,7 @@ export function DynamicFacetRangeQueryControllerTest() {
     let ranges = DynamicFacetRangeTestUtils.createFakeRanges();
 
     beforeEach(() => {
-      facetOptions = { field: '@field', ranges };
-
+      facetOptions = {};
       initializeComponents();
     });
 
@@ -26,10 +26,6 @@ export function DynamicFacetRangeQueryControllerTest() {
       return dynamicFacetRangeQueryController.buildFacetRequest(new QueryBuilder().build());
     }
 
-    function facetValues() {
-      return facet.values.allFacetValues;
-    }
-
     it('should send the right basic facetRequest parameters', () => {
       expect(facetRequest().facetId).toBe(facet.options.id);
       expect(facetRequest().field).toBe(facet.fieldName);
@@ -38,48 +34,67 @@ export function DynamicFacetRangeQueryControllerTest() {
       expect(facetRequest().injectionDepth).toBe(facet.options.injectionDepth);
     });
 
-    it('should send the current values', () => {
-      const currentValues = facetRequest().currentValues;
-      const facetValue = facetValues()[0];
-      expect(currentValues[0]).toEqual(
-        jasmine.objectContaining({
-          start: facetValue.start,
-          end: facetValue.end,
-          endInclusive: facetValue.endInclusive,
-          state: facetValue.state
-        })
-      );
+    it('freezeCurrentValues should be false', () => {
+      expect(facetRequest().freezeCurrentValues).toBe(false);
     });
 
-    describe('when there are values', () => {
-      it('freezeCurrentValues should be true', () => {
-        expect(facetRequest().freezeCurrentValues).toBe(true);
+    describe('when the ranges option is defined', () => {
+      beforeEach(() => {
+        facetOptions = { field: '@field', ranges };
+        initializeComponents();
       });
 
       it('numberOfValues should be equal to the number of current values', () => {
         expect(facetRequest().numberOfValues).toBe(facetRequest().currentValues.length);
       });
+
+      it('generateAutomaticRanges should be false', () => {
+        expect(facetRequest().generateAutomaticRanges).toBe(false);
+      });
+
+      it('should send the ranges values as current', () => {
+        const currentValues = facetRequest().currentValues;
+        const facetValue = ranges[0];
+        expect(currentValues[0]).toEqual(
+          jasmine.objectContaining({
+            start: facetValue.start,
+            end: facetValue.end,
+            endInclusive: facetValue.endInclusive,
+            state: FacetValueState.idle
+          })
+        );
+      });
     });
 
-    describe('when calling reset', () => {
-      beforeEach(() => {
-        facet.values.resetValues();
-      });
-
-      it('freezeCurrentValues should be true', () => {
-        expect(facetRequest().freezeCurrentValues).toBe(true);
-      });
-
+    describe('when the ranges option is not defined (empty ranges)', () => {
       it('numberOfValues should be equal to the numberOfValues option', () => {
         expect(facetRequest().numberOfValues).toBe(facet.options.numberOfValues);
       });
-    });
 
-    it('when no ranges are specified, freezeCurrentValues should be false', () => {
-      facetOptions.ranges = [];
-      initializeComponents();
+      describe('when there are no values in the facet', () => {
+        it('generateAutomaticRanges should be true', () => {
+          expect(facetRequest().generateAutomaticRanges).toBe(true);
+        });
 
-      expect(facetRequest().freezeCurrentValues).toBe(false);
+        it('should send no current values', () => {
+          expect(facetRequest().currentValues).toEqual([]);
+        });
+      });
+
+      describe('when there are values in the facet', () => {
+        beforeEach(() => {
+          facet.selectMultipleValues(['0..100inc', '100..200inc', '200..300inc']);
+          facet.deselectValue('0..100inc');
+        });
+
+        it('generateAutomaticRanges should be true', () => {
+          expect(facetRequest().generateAutomaticRanges).toBe(true);
+        });
+
+        it('should send current available values', () => {
+          expect(facetRequest().currentValues.length).toEqual(3);
+        });
+      });
     });
   });
 }
