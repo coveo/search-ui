e44209e76d2a81b811d4f8fda60508cb4bb0a1e1
Olivier Lamothe <olamothe@coveo.com>
2020-09-02 11:02:28 -0400

    Add list container
    https://coveord.atlassian.net/browse/JSUI-3077

--- src/ui/Facet/BreadcrumbValueElement.ts
+++ src/ui/Facet/BreadcrumbValueElement.ts
@@ -21,12 +21,13 @@ export class BreadcrumbValueElement {
   public build(): Dom {
     Assert.exists(this.facetValue);
 
-    const { container, caption, clear } = this.buildElements();
+    const { container, caption, clear, listContainer } = this.buildElements();
 
     container.append(caption.el);
     container.append(clear.el);
+    listContainer.append(container.el);
 
-    return container;
+    return listContainer;
   }
 
   public getBreadcrumbTooltip(): string {
@@ -42,7 +43,8 @@ export class BreadcrumbValueElement {
     return {
       container: this.buildContainer(),
       clear: this.buildClear(),
-      caption: this.buildCaption()
+      caption: this.buildCaption(),
+      listContainer: this.buildListContainer()
     };
   }
 
@@ -64,6 +66,12 @@ export class BreadcrumbValueElement {
       .build();
 
     return container;
+  }
+
+  private buildListContainer() {
+    return $$('li', {
+      className: 'coveo-facet-breadcrumb-value-list-item'
+    });
   }
 
   private buildClear() {
