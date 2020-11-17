990e60592e4921f682ecc16e7c25c3078d0b48b8
Sami <samisayegh1@gmail.com>
2020-11-17 17:11:28 -0500

    JSUI-3150 Prevent adding highlights containing no text (#1653)
    https://coveord.atlassian.net/browse/JSUI-3150

--- unitTests/ui/QuickviewDocumentWordsTest.ts
+++ unitTests/ui/QuickviewDocumentWordsTest.ts
@@ -21,58 +21,45 @@ export function QuickviewDocumentWordsTest() {
     });
 
     it('should detect multiple keywords, and ignore random elements', () => {
-      fakeBody.append(
-        $$('div', {
-          id: 'CoveoHighlight:1.1.1'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'CoveoHighlight:1.1.1' }, 'A').el);
 
-      fakeBody.append(
-        $$('div', {
-          id: 'randomStuff'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'randomStuff' }).el);
 
-      fakeBody.append(
-        $$('div', {
-          id: 'CoveoHighlight:2.1.1'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'CoveoHighlight:2.1.1' }, 'B').el);
 
-      fakeBody.append(
-        $$('div', {
-          id: 'randomStuff2'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'randomStuff2' }).el);
 
       const words = new QuickviewDocumentWords(fakeIframe, fakeResult);
+
       expect(words.words['1'].indexIdentifier).toBe('1');
+      expect(words.words['1'].text).toBe('A');
+
       expect(words.words['2'].indexIdentifier).toBe('2');
+      expect(words.words['2'].text).toBe('B');
+
       expect(Object.keys(words.words).length).toBe(2);
     });
 
     it('should detect multiple keywords in the same group', () => {
-      fakeBody.append(
-        $$('div', {
-          id: 'CoveoHighlight:1.1.1'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'CoveoHighlight:1.1.1' }, 'A').el);
 
-      fakeBody.append(
-        $$('div', {
-          id: 'CoveoHighlight:1.2.1'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'CoveoHighlight:1.2.1' }, 'A').el);
 
-      fakeBody.append(
-        $$('div', {
-          id: 'CoveoHighlight:1.3.1'
-        }).el
-      );
+      fakeBody.append($$('div', { id: 'CoveoHighlight:1.3.1' }, 'A').el);
 
       const words = new QuickviewDocumentWords(fakeIframe, fakeResult);
       expect(words.words['1'].indexIdentifier).toBe('1');
       expect(words.words['1'].elements.length).toBe(3);
+    });
+
+    it('should ignore highlights whose text resolves to an empty string', () => {
+      const span = $$('span', { id: 'CoveoHighlight:1.1.1' }, $$('coveotaggedword', { id: 'CoveoHighlight:2.1.1' }, 'A'));
+
+      fakeBody.append(span.el);
+      const words = new QuickviewDocumentWords(fakeIframe, fakeResult);
+
+      expect(words.words['1'].text).toBe('A');
+      expect(words.words['2']).toBe(undefined);
     });
   });
 }
