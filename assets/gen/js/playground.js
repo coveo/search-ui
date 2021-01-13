7f004efd06419f373aa1e84c619972101b519d79
deploy.sh <sandbox_JSUI@coveo.com>
2021-01-13 17:07:16 +0000

    publish: Automatically created by Github Actions
    generated from commit f2c0336e093cc85882b6a73e45952f398b025e73

--- assets/gen/js/playground.js
+++ assets/gen/js/playground.js
@@ -15144,8 +15144,8 @@ exports.TimeSpan = TimeSpan;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = {
-    lib: '2.10082.4-beta',
-    product: '2.10082.4-beta',
+    lib: '2.10082.5-beta',
+    product: '2.10082.5-beta',
     supportedApiVersion: 2
 };
 
