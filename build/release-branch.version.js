88125e1462c4bea971b730c741e35777dec47a8d
Sami Sayegh <ssayegh@coveo.com>
2021-03-16 18:29:50 -0400

    create or update branch and bump version

--- build/release-branch.version.js
+++ build/release-branch.version.js
@@ -1,14 +1,17 @@
 const { promisify } = require('util');
+const { getLatestVersionOnNpm } = require('./npm.utilities');
 const exec = promisify(require('child_process').exec);
 
 async function main() {
   const branchName = process.argv[2];
 
-  const exists = await branchExists(branchName);
-  console.log(exists);
+  const exists = await checkIfBranchExists(branchName);
+  exists ? updateBranch() : createBranch();
+
+  await push();
 }
 
-async function branchExists(branchName) {
+async function checkIfBranchExists(branchName) {
   try {
     await exec(`git show-ref --verify --quiet refs/heads/${branchName}`);
     return true;
@@ -16,6 +19,32 @@ async function branchExists(branchName) {
     console.log(`did not find a branch called: ${branchName}`);
     return false;
   }
+}
+
+async function updateBranch(branchName) {
+  await exec(`git checkout ${branchName}`);
+  await exec(`npm version patch`);
+}
+
+async function createBranch(branchName) {
+  await exec(`git checkout -b ${branchName}`);
+  const newVersion = await getNewReleaseVersion();
+  await exec(`npm version ${newVersion}`);
+}
+
+async function getNewReleaseVersion() {
+  const version = await getLatestVersionOnNpm();
+  const { major, minor } = parseVersion(version);
+  return `${major}.${minor + 1}.0`;
+}
+
+function parseVersion(version) {
+  const [major, minor, patch] = version.split('.').map(num => parseInt(num, 10));
+  return { major, minor, patch };
+}
+
+async function push(branchName) {
+  await exec(`git push origin ${branchName}`);
 }
 
 main();
