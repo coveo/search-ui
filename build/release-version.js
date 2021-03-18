a23df3587f3a33fe37d5c0636e8918a36d9f69a9
Sami <ssayegh@coveo.com>
2021-03-18 13:53:31 -0400

    JSUI-3202 Fix release version logic (#1706)
    https://coveord.atlassian.net/browse/JSUI-3202

--- build/release-version.js
+++ build/release-version.js
@@ -10,35 +10,41 @@ async function main() {
     return console.log('please specify a branch name');
   }
 
+  await configureGit();
   const exists = await checkIfBranchExists(branchName);
-  exists ? updateBranch() : createBranch();
+  exists ? await updateBranch(branchName) : await createBranch(branchName);
 
-  await push();
+  await push(branchName);
+}
+
+async function configureGit() {
+  await exec(`git config --global user.email "sandbox_JSUI@coveo.com"`);
+  await exec(`git config --global user.name "Github Action Bot"`);
 }
 
 async function checkIfBranchExists(branchName) {
-  try {
-    await exec(`git show-ref --verify --quiet refs/heads/${branchName}`);
-    return true;
-  } catch (e) {
-    console.log(`did not find a branch called: ${branchName}`);
-    return false;
-  }
+  const { stdout } = await exec(`git ls-remote --heads origin ${branchName}`);
+  return stdout !== '';
 }
 
 async function updateBranch(branchName) {
   console.log(`updating branch: ${branchName}`);
 
   await exec(`git checkout ${branchName}`);
-  await exec(`npm version patch`);
+  await bumpVersion('patch');
 }
 
 async function createBranch(branchName) {
   console.log(`creating branch: ${branchName}`);
 
   await exec(`git checkout -b ${branchName}`);
-  const newVersion = await getNewReleaseVersion();
-  await exec(`npm version ${newVersion}`);
+  const version = await getNewReleaseVersion();
+  await bumpVersion(version);
+}
+
+async function bumpVersion(version) {
+  console.log(`bumping version: ${version}`);
+  await exec(`npm version ${version} -m "[version bump] %s"`);
 }
 
 async function getNewReleaseVersion() {
@@ -49,7 +55,7 @@ async function getNewReleaseVersion() {
 
 async function push(branchName) {
   console.log(`pushing branch: ${branchName}`);
-  await exec(`git push origin ${branchName}`);
+  await exec(`git push origin ${branchName} --tags`);
 }
 
 main();
