f5dc167a9f5acfa78f7f007893229e94f2cb3d2c
Sami <ssayegh@coveo.com>
2021-09-02 14:07:11 -0400

    JSUI-3275 Add SafeLocalStorage, replace direct calls to localStorage (#1774)
    https://coveord.atlassian.net/browse/JSUI-3275

--- src/utils/LocalStorageUtils.ts
+++ src/utils/LocalStorageUtils.ts
@@ -1,12 +1,18 @@
-var localStorage;
+var localStorage: Storage;
 
 // This check must be made in a try/catch. If cookies are disabled for a
 // browser then window.localStorage will throw an undefined exception.
 try {
   localStorage = window.localStorage;
 } catch (error) {
+  console.warn(
+    'Unable to access localStorage. Certain features like analytics will not work. If this is unintended, consider adjusting your browser settings to allow third-party cookies and data.\n\n',
+    error
+  );
   localStorage = null;
 }
+
+export const localStorageExists = !!localStorage;
 
 export class LocalStorageUtils<T> {
   constructor(public id: string) {}
@@ -47,5 +53,51 @@ export class LocalStorageUtils<T> {
 
   private getLocalStorageKey() {
     return 'coveo-' + this.id;
+  }
+}
+
+export class SafeLocalStorage implements Storage {
+  [key: string]: any;
+
+  public getItem(key: string) {
+    try {
+      return localStorage.getItem(key);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  public removeItem(key: string) {
+    try {
+      localStorage.removeItem(key);
+    } catch (e) {}
+  }
+
+  public setItem(key: string, value: string) {
+    try {
+      localStorage.setItem(key, value);
+    } catch (e) {}
+  }
+
+  public clear() {
+    try {
+      localStorage.clear();
+    } catch (e) {}
+  }
+
+  public key(index: number) {
+    try {
+      return localStorage.key(index);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  get length() {
+    try {
+      return localStorage.length;
+    } catch (e) {
+      return 0;
+    }
   }
 }
