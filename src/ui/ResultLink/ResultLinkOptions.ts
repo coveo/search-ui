b60249680126b3606755e817ea251f4bc8715208
btaillon <54454747+btaillon@users.noreply.github.com>
2021-09-29 14:19:28 -0400

    JSUI-3282 Added specific documentOpen analytics for SmartSnippet and SmartSnippetSuggestions (#1781)
    https://coveord.atlassian.net/browse/JSUI-3282

--- src/ui/ResultLink/ResultLinkOptions.ts
+++ src/ui/ResultLink/ResultLinkOptions.ts
@@ -2,6 +2,7 @@ import { IQueryResult } from '../../rest/QueryResult';
 import { IFieldOption } from '../Base/IComponentOptions';
 
 export interface IResultLinkOptions {
+  logAnalytics?: (href: string) => void;
   onClick?: (e: Event, result: IQueryResult) => any;
   field?: IFieldOption;
   openInOutlook?: boolean;
