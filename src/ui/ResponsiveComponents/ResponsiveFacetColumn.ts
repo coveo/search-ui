25420f455097e28eabf276de34d0648ab09b1caa
Olivier Lamothe <olamothe@coveo.com>
2021-12-14 13:20:51 -0500

    fix unused
    https://coveord.atlassian.net/browse/JSUI-3332

--- src/ui/ResponsiveComponents/ResponsiveFacetColumn.ts
+++ src/ui/ResponsiveComponents/ResponsiveFacetColumn.ts
@@ -21,8 +21,6 @@ import { FacetsMobileModeEvents } from '../../events/FacetsMobileModeEvents';
 export class ResponsiveFacetColumn implements IResponsiveComponent {
   public static DEBOUNCE_SCROLL_WAIT = 250;
 
-  private static DROPDOWN_MIN_WIDTH: number = 280;
-  private static DROPDOWN_WIDTH_RATIO: number = 0.35; // Used to set the width relative to the coveo root.
   private static DROPDOWN_HEADER_LABEL_DEFAULT_VALUE = 'Filters';
 
   private searchInterface: SearchInterface;
