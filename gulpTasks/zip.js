879575d6945cda0ad08d87d5e34a7829df0304c6
Olivier Lamothe <olamothe@coveo.com>
2022-03-01 13:58:55 -0500

    move from veracode to snyk code
    https://coveord.atlassian.net/browse/JSUI-3362

--- gulpTasks/zip.js
+++ gulpTasks/zip.js
@@ -4,17 +4,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 
 function zipForGitReleases() {
-  return gulp
-    .src('bin/**/*')
-    .pipe(zip('search-ui.zip'))
-    .pipe(gulp.dest('./'));
+  return gulp.src('bin/**/*').pipe(zip('search-ui.zip')).pipe(gulp.dest('./'));
 }
 
-function zipForVeracode() {
-  return gulp
-    .src(['src/**/*', 'yarn.lock', 'package.json'])
-    .pipe(zip('veracode.zip'))
-    .pipe(gulp.dest('./'));
-}
-
-module.exports = { zipForGitReleases, zipForVeracode };
+module.exports = { zipForGitReleases };
