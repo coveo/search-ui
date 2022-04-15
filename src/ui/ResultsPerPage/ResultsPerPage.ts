113b4a88fe8fdf003472232c1f8b27494b3fb818
btaillon <54454747+btaillon@users.noreply.github.com>
2022-04-15 08:35:21 -0400

    fix accessibility insights reports (#1870)
    https://coveord.atlassian.net/browse/JSUI-3382

--- src/ui/ResultsPerPage/ResultsPerPage.ts
+++ src/ui/ResultsPerPage/ResultsPerPage.ts
@@ -53,8 +53,8 @@ export class ResultsPerPage extends Component {
      * Default value is `[10, 25, 50, 100]`.
      */
     choicesDisplayed: ComponentOptions.buildCustomListOption<number[]>(
-      function(list: string[]) {
-        const values = _.map(list, function(value) {
+      function (list: string[]) {
+        const values = _.map(list, function (value) {
           return parseInt(value, 10);
         });
         return values.length == 0 ? null : values;
@@ -221,7 +221,6 @@ export class ResultsPerPage extends Component {
     this.element.appendChild(this.span);
     this.list = $$('ul', {
       className: 'coveo-results-per-page-list',
-      role: 'radiogroup',
       'aria-labelledby': this.span.id
     }).el;
     this.element.appendChild(this.list);
@@ -232,37 +231,34 @@ export class ResultsPerPage extends Component {
     const numResultsList: number[] = this.options.choicesDisplayed;
     for (var i = 0; i < numResultsList.length; i++) {
       const listItem = $$('li', {
-        className: 'coveo-results-per-page-list-item',
-        role: 'radio',
-        tabindex: 0
+        className: 'coveo-results-per-page-list-item'
       }).el;
       const resultsPerPage = numResultsList[i];
       const isActive = resultsPerPage === this.currentResultsPerPage;
       if (isActive) {
         $$(listItem).addClass('coveo-active');
       }
-      listItem.setAttribute('aria-checked', isActive.toString());
 
       const clickAction = () => this.handleClickPage(resultsPerPage);
 
+      const button = $$(
+        'span',
+        {
+          className: 'coveo-results-per-page-list-item-text',
+          tabindex: 0,
+          ariaPressed: isActive.toString()
+        },
+        numResultsList[i].toString()
+      ).el;
+      listItem.appendChild(button);
+
       new AccessibleButton()
-        .withElement(listItem)
+        .withElement(button)
         .withLabel(l('DisplayResultsPerPage', numResultsList[i].toString()))
         .withClickAction(clickAction)
         .withEnterKeyboardAction(clickAction)
         .build();
 
-      listItem.appendChild(
-        $$(
-          'a',
-          {
-            className: 'coveo-results-per-page-list-item-text',
-            tabindex: -1,
-            ariaHidden: 'true'
-          },
-          numResultsList[i].toString()
-        ).el
-      );
       this.list.appendChild(listItem);
     }
   }
