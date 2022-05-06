211eeeff62f01bea3a77382dff61c1944e0c7f5d
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-05-06 12:42:06 -0400

    add ID
    https://coveord.atlassian.net/browse/JSUI-3398

--- invalidate.cloudfront.js
+++ invalidate.cloudfront.js
@@ -6,7 +6,7 @@ const cloudfront = new AWS.CloudFront();
 const pathToInvalidate = `/searchui/v2.10096/*`;
 
 const invalidationRequest = cloudfront.createInvalidation({
-  DistributionId: process.env.AWS_CLOUDFRONT_DISTRIBUTION_ID,
+  DistributionId: 'E2VWLFSCSD1GLA',
   InvalidationBatch: {
     CallerReference: new Date().getTime().toString(),
     Paths: {
