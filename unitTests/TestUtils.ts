ef3f66f27c9f21838c3e68e97e504dbfdd41e575
Benjamin Taillon <btaillon@coveo.com>
2022-09-08 14:44:25 -0500

    added tests
    https://coveord.atlassian.net/browse/JSUI-3435

--- unitTests/TestUtils.ts
+++ unitTests/TestUtils.ts
@@ -1,4 +1,6 @@
-export function expectChildren(element: HTMLElement | DocumentFragment, classNames: string[]) {
+import { toArray } from 'underscore';
+
+export function expectChildren(element: HTMLElement | DocumentFragment, classNames: (string | null)[]) {
   expect(element.childNodes.length).toEqual(classNames.length, `Expected only ${classNames.length} children.`);
   return classNames.map((className, index) => {
     const childAtIndex = element.childNodes.item(index) as HTMLElement;
@@ -7,4 +9,48 @@ export function expectChildren(element: HTMLElement | DocumentFragment, classNam
     }
     return childAtIndex;
   });
+}
+
+export function expectEquivalentDOM(
+  actual: HTMLElement,
+  expected: HTMLElement,
+  options?: { ignoreRoot?: boolean; ignoreAttributes?: string[] | boolean }
+): boolean {
+  const getAttributes = (element: HTMLElement) => {
+    let attributes = toArray<Attr>(element.attributes).map(({ name, value }) => ({ name, value }));
+    if (options) {
+      if (options.ignoreAttributes === true) {
+        attributes = [];
+      } else if (Array.isArray(options.ignoreAttributes) && options.ignoreAttributes.length) {
+        const attributesToIgnore = options.ignoreAttributes;
+        attributes = attributes.filter(attribute => attributesToIgnore.indexOf(attribute.name) === -1);
+      }
+    }
+    return attributes;
+  };
+
+  if (!options || !options.ignoreRoot) {
+    if (expect(actual.tagName).toEqual(expected.tagName)) {
+      return true;
+    }
+    if (expect(getAttributes(actual)).toEqual(getAttributes(expected))) {
+      return true;
+    }
+  }
+  const actualChildren = toArray<HTMLElement>(actual.children);
+  const expectedChildren = toArray<HTMLElement>(expected.children);
+  if (
+    expect(actualChildren.length).toEqual(
+      expectedChildren.length,
+      `${actual.tagName} is expected to have ${expectedChildren.length} children but has ${actualChildren.length}.`
+    )
+  ) {
+    return true;
+  }
+  return actualChildren.some((actualChild, index) =>
+    expectEquivalentDOM(actualChild, expectedChildren[index], {
+      ignoreRoot: false,
+      ignoreAttributes: options && options.ignoreAttributes
+    })
+  );
 }
