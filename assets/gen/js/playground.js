bdba07fa174a14840eb037a5d6df70de79609c9a
deploy.sh <sandbox_JSUI@coveo.com>
2024-06-17 18:52:14 +0000

    publish: [version bump] 2.10118.0
    generated from commit f5eb6ef82248b972691af9174dbb4be6fb92fbaf

--- assets/gen/js/playground.js
+++ assets/gen/js/playground.js
@@ -15583,8 +15583,8 @@ exports.TimeSpan = TimeSpan;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = {
-    lib: '2.10117.0',
-    product: '2.10117.0',
+    lib: '2.10118.0',
+    product: '2.10118.0',
     supportedApiVersion: 2
 };
 
