8b430b1cb916d61b271bac22344034d8fac360c2
Lucille Vu <lvu@coveo.com>
2024-07-03 09:38:08 -0400

    QA-1494 Test for Paper, ResultPerPage & ResultLayout (#194)
    * QA-1495 Test for Paper, ResultPerPage & ResultLayout
    
    ---------
    
    Co-authored-by: wmannard <75636363+wmannard@users.noreply.github.com>

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    bracketSpacing: false,
+};
--- utils/selectors.ts
+++ utils/selectors.ts
@@ -1,28 +1,48 @@
-import { Page } from "@playwright/test";
+import {Page} from '@playwright/test';
 
-export const BreadcrumbSelectors = (page: Page) => Object.freeze({
-    breadcrumbItemRow: page.locator('.coveo-breadcrumb-item'),
-    breadcrumbDynamicFacetValue: page.locator('.coveo-dynamic-facet-breadcrumb-value'),
-    breadcrumbFacetValue: page.locator('.coveo-facet-breadcrumb-value'),
-    breadcrumbClearFacet: page.locator('.CoveoBreadcrumb').getByLabel(/Remove inclusion filter/),
-    breadcrumbClearAll: page.getByRole('button', { name: 'Clear All Filters' })
-})
+export const BreadcrumbSelectors = (page: Page) =>
+    Object.freeze({
+        breadcrumbItemRow: page.locator('.coveo-breadcrumb-item'),
+        breadcrumbDynamicFacetValue: page.locator('.coveo-dynamic-facet-breadcrumb-value'),
+        breadcrumbFacetValue: page.locator('.coveo-facet-breadcrumb-value'),
+        breadcrumbClearFacet: page.locator('.CoveoBreadcrumb').getByLabel(/Remove inclusion filter/),
+        breadcrumbClearAll: page.getByRole('button', {name: 'Clear All Filters'}),
+    });
 
-export const DocumentFieldSelectors = (page: Page) => Object.freeze({
-    fieldValue: page.locator('.CoveoFieldValue span.coveo-clickable')
-})
+export const DocumentFieldSelectors = (page: Page) =>
+    Object.freeze({
+        fieldValue: page.locator('.CoveoFieldValue span.coveo-clickable'),
+    });
 
-export const DynamicFacetSelectors = (page: Page, facetField = '@author') => Object.freeze({
-    facet: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`),
-    facetCheckbox: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByRole('checkbox'),
-    facetValue: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"] span.coveo-checkbox-span-label`),
-    facetSearchbox: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByPlaceholder('Search'),
-    facetSearchValue: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).locator('ul.coveo-combobox-values span.coveo-checkbox-span-label'),
-    facetShowMore: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByLabel(/Show more/),
-    facetShowLess: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByLabel(/Show fewer/)
-})
+export const DynamicFacetSelectors = (page: Page, facetField = '@author') =>
+    Object.freeze({
+        facet: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`),
+        facetCheckbox: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByRole('checkbox'),
+        facetValue: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"] span.coveo-checkbox-span-label`),
+        facetSearchbox: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByPlaceholder('Search'),
+        facetSearchValue: page
+            .locator(`.CoveoDynamicFacet[data-field="${facetField}"]`)
+            .locator('ul.coveo-combobox-values span.coveo-checkbox-span-label'),
+        facetShowMore: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByLabel(/Show more/),
+        facetShowLess: page.locator(`.CoveoDynamicFacet[data-field="${facetField}"]`).getByLabel(/Show fewer/),
+    });
 
-export const SearchboxSelectors = (page: Page) => Object.freeze({
-    searchboxInput: page.getByRole('combobox', { name: 'Search' }),
-    searchboxIcon: page.getByRole('button', { name: 'Search', exact: true })
-})
+export const PagerSelectors = (page: Page) =>
+    Object.freeze({
+        nextButton: page.getByLabel('Next'),
+        previousButton: page.getByLabel('Previous'),
+        currentPagerItem: page.locator('.coveo-pager-list-item.coveo-active'),
+    });
+
+export const ResultLayoutSelectors = (page: Page) =>
+    Object.freeze({
+        listButton: page.getByLabel('Display results as List'),
+        cardButton: page.getByLabel('Display results as Card'),
+        tableButton: page.getByLabel('Display results as Table'),
+    });
+
+export const SearchboxSelectors = (page: Page) =>
+    Object.freeze({
+        searchboxInput: page.getByRole('combobox', {name: 'Search'}),
+        searchboxIcon: page.getByRole('button', {name: 'Search', exact: true}),
+    });
