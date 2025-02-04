fbccd2f579c96af5fd9bee093526f5fb5c1dfc77
samiusss <sasab21@ulaval.ca>
2025-02-04 11:46:50 -0500

    fix: dry-run edits to test repo communication with private one

--- scripts/deploy/trigger-search-ui-cd.mjs
+++ scripts/deploy/trigger-search-ui-cd.mjs
@@ -1,12 +1,14 @@
 import {context, getOctokit} from '@actions/github';
 
 const octokit = getOctokit(process.env.GH_TOKEN);
+const branchName = process.env.TARGET_BRANCH || 'main';
 
 await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy',
   client_payload: {
     run_Id: context.runId,
     version: 'v3',
+    branch: branchName,
   },
   owner: 'coveo-platform',
   repo: 'search-ui-cd',
