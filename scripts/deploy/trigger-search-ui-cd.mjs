489a0843cba1830ac24e5a400b772a4f533a8930
samiusss <sasab21@ulaval.ca>
2025-02-05 08:20:46 -0500

    fix: debuggin running the build action from private repo

--- scripts/deploy/trigger-search-ui-cd.mjs
+++ scripts/deploy/trigger-search-ui-cd.mjs
@@ -1,16 +1,12 @@
 import {context, getOctokit} from '@actions/github';
 
 const octokit = getOctokit(process.env.GH_TOKEN);
-const branchName = 'feat/DT-7426/configure-private-search-ui-deployment';
-
-console.log(`Triggering search-ui-cd for branch ${branchName}`);
 
 await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy',
   client_payload: {
     run_Id: context.runId,
     version: 'v3',
-    branch: branchName,
   },
   owner: 'coveo-platform',
   repo: 'search-ui-cd',
