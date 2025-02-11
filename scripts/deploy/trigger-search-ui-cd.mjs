34e43eb924f3248fb64e4cf7d0985f71fc0625af
samiusss <ssabir@coveo.com>
2025-02-11 11:43:35 -0500

    feat: Adding back docs, unit tests, validation tests [DT-7426] (#2003)
    * fix: add --yes option to apt-get command
    
    * fix: add steps about docs, tests and accessibility tests
    
    * fix: add the tests in the build action and remove them form release.yml
    
    * fix: allow checkout on correct release branch instead of master in private repo
    
    * fix: make build run before release.yml

--- scripts/deploy/trigger-search-ui-cd.mjs
+++ scripts/deploy/trigger-search-ui-cd.mjs
@@ -1,13 +1,15 @@
-import {context, getOctokit} from '@actions/github';
+import { context, getOctokit } from '@actions/github';
 
 const octokit = getOctokit(process.env.GH_TOKEN);
+const branchName = process.env.RELEASE_BRANCH_NAME;
 
 await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy',
   client_payload: {
     run_Id: context.runId,
     version: 'v2',
+    release_branch_name: branchName
   },
   owner: 'coveo-platform',
-  repo: 'search-ui-cd',
+  repo: 'search-ui-cd'
 });
