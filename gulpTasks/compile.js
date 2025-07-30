da6c310729db2a507f87752bf8f5bae2b50ff06c
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-07-30 14:39:00 -0400

    chore: remove modal-box dependency and patch (#2020 / KIT-4654)
    https://coveord.atlassian.net/browse/KIT-4654

--- gulpTasks/compile.js
+++ gulpTasks/compile.js
@@ -27,45 +27,28 @@ const analyze = gulp.series(
 
 function pipeModalBoxFiles() {
   return event_stream
-    .merge(
-      gulp
-        .src('./node_modules/modal-box/bin/modalBox.css')
-        .pipe(rename('_ModalBox.scss'))
-        .pipe(gulp.dest('./bin/sass/'))
-    )
+    .merge(gulp.src('./lib/modal-box/modalBox.css').pipe(rename('_ModalBox.scss')).pipe(gulp.dest('./bin/sass/')))
     .pipe(event_stream.wait());
 }
 
 function deprecatedDependencies() {
-  return gulp
-    .src('./src/Dependencies.js')
-    .pipe(rename('CoveoJsSearch.Dependencies.js'))
-    .pipe(gulp.dest('./bin/js/'));
+  return gulp.src('./src/Dependencies.js').pipe(rename('CoveoJsSearch.Dependencies.js')).pipe(gulp.dest('./bin/js/'));
 }
 
 // We duplicate css file to help on upgrade (deployments using the "NewDesign" file)
 // This should help mitigate 404 on those files, and hopefully possible maintenance case(s).
 function duplicateCssFile() {
-  return gulp
-    .src('./bin/css/CoveoFullSearch.css')
-    .pipe(rename('CoveoFullSearchNewDesign.css'))
-    .pipe(gulp.dest('./bin/css'));
+  return gulp.src('./bin/css/CoveoFullSearch.css').pipe(rename('CoveoFullSearchNewDesign.css')).pipe(gulp.dest('./bin/css'));
 }
 
 function duplicateCssMapFile() {
-  return gulp
-    .src('./bin/css/CoveoFullSearch.css.map')
-    .pipe(rename('CoveulFullSearchNewDesign.css.map'))
-    .pipe(gulp.dest('./bin/css'));
+  return gulp.src('./bin/css/CoveoFullSearch.css.map').pipe(rename('CoveulFullSearchNewDesign.css.map')).pipe(gulp.dest('./bin/css'));
 }
 
 // This cause an issue when dep are bundled together : the lack of EOL makes it so
 // that part of the bundled code can be commented out or not valid
 function addEolDependencies() {
-  return gulp
-    .src('./node_modules/underscore/underscore-min.js')
-    .pipe(eol())
-    .pipe(gulp.dest('./node_modules/underscore/'));
+  return gulp.src('./node_modules/underscore/underscore-min.js').pipe(eol()).pipe(gulp.dest('./node_modules/underscore/'));
 }
 
 module.exports = { compile, compileTSOnly, minimize, analyze };
--- webpack.tsonly.config.js
+++ webpack.tsonly.config.js
@@ -10,14 +10,16 @@ let bail;
 let plugins = [];
 let additionalRules = [];
 
-plugins.push(new webpack.DefinePlugin({
-  DISABLE_LOGGER: minimize
-}))
+plugins.push(
+  new webpack.DefinePlugin({
+    DISABLE_LOGGER: minimize
+  })
+);
 
 module.exports = {
   entry: {
     'CoveoJsSearch.Lazy': ['./src/Lazy.ts'],
-    'CoveoJsSearch': ['./src/Eager.ts']
+    CoveoJsSearch: ['./src/Eager.ts']
   },
   output: {
     path: path.resolve('./bin/js'),
@@ -32,81 +34,106 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js', '.scss'],
     alias: {
-      'l10n': __dirname + '/lib/l10n/l10n.min.js',
-      'globalize': globalizePath,
-      'modal-box': __dirname + '/node_modules/modal-box/bin/ModalBox.min.js',
+      l10n: __dirname + '/lib/l10n/l10n.min.js',
+      globalize: globalizePath,
+      'modal-box': __dirname + '/lib/modal-box/ModalBox.min.js',
       'magic-box': __dirname + '/node_modules/coveomagicbox/bin/MagicBox.min.js',
       'default-language': __dirname + '/src/strings/DefaultLanguage.js',
-      'jQuery': __dirname + '/test/lib/jquery.js',
-      'styling': __dirname + '/sass'
+      jQuery: __dirname + '/test/lib/jquery.js',
+      styling: __dirname + '/sass'
     },
     modules: ['node_modules', path.resolve(__dirname, '/bin/image/css')]
   },
   devtool: 'source-map',
   module: {
-    rules: [{
-      test: /underscore-min.js/,
-      use: [{
-        loader: 'string-replace-loader',
-        options: {
-          search: '//# sourceMappingURL=underscore-min.map',
-          replace: ''
-        }
-      }]
-    }, {
-      test: require.resolve(globalizePath),
-      use: [{
-        loader: 'expose-loader?Globalize'
-      }]
-    }, {
-      test: /jquery.js/,
-      use: [{
-        loader: 'string-replace-loader',
-        options: {
-          search: '//@ sourceMappingURL=jquery.min.map',
-          replace: ''
-        }
-      }]
-    }, {
-      test: /promise|es6-promise/,
-      use: [{
-        loader: 'string-replace-loader',
-        options: {
-          search: '//# sourceMappingURL=es6-promise.map',
-          replace: ''
-        }
-      }]
-    }, {
-      test: /coveo\.analytics\/dist\/.*\.js/,
-      use: [{
-        loader: 'string-replace-loader',
-        options: {
-          search: '(?!\n).*\.map',
-          flags: 'g',
-          replace: ''
-        }
-      }]
-    }, {
-      test: /\.(gif|svg|png|jpe?g|ttf|woff2?|eot)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: production ? '../image/[name].[ext]' : 'http://localhost:8080/image/[name].[ext]',
-          emitFile: false,
-          publicPath: ' ',
-        }
-      }]
-    }, {
-      test: /\.ts$/,
-      use: [{
-        loader: 'ts-loader'
-      }]
-    }, {
-      test: /\.scss/,
-      use: [{
-        loader: 'null-loader'
-      }]
-    }]
+    rules: [
+      {
+        test: /underscore-min.js/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: '//# sourceMappingURL=underscore-min.map',
+              replace: ''
+            }
+          }
+        ]
+      },
+      {
+        test: require.resolve(globalizePath),
+        use: [
+          {
+            loader: 'expose-loader?Globalize'
+          }
+        ]
+      },
+      {
+        test: /jquery.js/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: '//@ sourceMappingURL=jquery.min.map',
+              replace: ''
+            }
+          }
+        ]
+      },
+      {
+        test: /promise|es6-promise/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: '//# sourceMappingURL=es6-promise.map',
+              replace: ''
+            }
+          }
+        ]
+      },
+      {
+        test: /coveo\.analytics\/dist\/.*\.js/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: '(?!\n).*.map',
+              flags: 'g',
+              replace: ''
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|svg|png|jpe?g|ttf|woff2?|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: production ? '../image/[name].[ext]' : 'http://localhost:8080/image/[name].[ext]',
+              emitFile: false,
+              publicPath: ' '
+            }
+          }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: 'null-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: plugins,
   bail: bail
